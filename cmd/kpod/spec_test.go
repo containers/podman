69cecb049aaf37fcc9a086b3f3f84e6e63174b14
baude <bbaude@redhat.com>
2017-11-02 09:15:10 -0500

    spec_test.go: Unit tests for spec.go
    Unit tests for spec.go which includes testing
    parts of the oci spec.
    
    Signed-off-by: baude <bbaude@redhat.com>

---
+++ cmd/kpod/spec_test.go
@@ -0,0 +1,39 @@
+package main
+
+
+import (
+	"testing"
+	"reflect"
+
+	"github.com/stretchr/testify/assert"
+	spec "github.com/opencontainers/runtime-spec/specs-go"
+)
+
+func TestCreateConfig_GetVolumeMounts(t *testing.T) {
+	data := spec.Mount{
+		Destination: "/foobar",
+		Type: "bind",
+		Source: "foobar",
+		Options: []string{"ro", "rbind"},
+	}
+	config := createConfig{
+		volumes: []string{"foobar:/foobar:ro"},
+	}
+	specMount := config.GetVolumeMounts()
+	assert.True(t, reflect.DeepEqual(data, specMount[0]))
+}
+
+func TestCreateConfig_GetTmpfsMounts(t *testing.T) {
+	data := spec.Mount{
+		Destination: "/homer",
+		Type: "tmpfs",
+		Source: "tmpfs",
+		Options: []string{"rw", "size=787448k", "mode=1777"},
+	}
+	config:= createConfig{
+		tmpfs: []string{"/homer:rw,size=787448k,mode=1777"},
+	}
+	tmpfsMount := config.GetTmpfsMounts()
+	assert.True(t, reflect.DeepEqual(data, tmpfsMount[0]))
+
+}
