d086beb7abc5ed002fe699ab02792aa47d50d581
Urvashi Mohnani <umohnani@redhat.com>
2017-11-06 11:04:34 -0500

    Vendor in latest containers/image
    Adds the tarball transport, which is used by the kpod import command
    
    Signed-off-by: Urvashi Mohnani <umohnani@redhat.com>
    
    Closes: #12
    Approved by: rhatdan

---
+++ vendor/github.com/containers/image/transports/alltransports/storage_stub.go
@@ -0,0 +1,9 @@
+// +build containers_image_storage_stub
+
+package alltransports
+
+import "github.com/containers/image/transports"
+
+func init() {
+	transports.Register(transports.NewStubTransport("storage"))
+}
