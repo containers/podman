dd133a1ad25f75e5ddd53ed6cf59eedfb6838f54
baude <bbaude@redhat.com>
2018-01-24 08:45:55 -0600

    Initial gingko work
    This implements the ginkgo integration test framework for
    podman.  As tests are migrated from bats to ginkgo, we will
    still run both integration suites.  When a test is migrated,
    we remove the tests from bats at that time.  All new tests
    should be just for the ginkgo framework.
    
    One exception is that we only run the ginkgo suit in the
    travis/ubuntu environment.  The CentOS and Fedora PAPR nodes
    will more than cover those.
    
    Signed-off-by: baude <bbaude@redhat.com>
    
    Closes: #261
    Approved by: baude

---
+++ test/e2e/rmi_test.go
@@ -0,0 +1,70 @@
+package integration
+
+import (
+	"os"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Podman rmi", func() {
+	var (
+		tempdir    string
+		err        error
+		podmanTest PodmanTest
+		image1     = "docker.io/library/alpine:latest"
+		image3     = "docker.io/library/busybox:glibc"
+	)
+
+	BeforeEach(func() {
+		tempdir, err = CreateTempDirInTempDir()
+		if err != nil {
+			os.Exit(1)
+		}
+		podmanTest = PodmanCreate(tempdir)
+		podmanTest.RestoreAllArtifacts()
+	})
+
+	AfterEach(func() {
+		podmanTest.Cleanup()
+
+	})
+
+	It("podman rmi bogus image", func() {
+		session := podmanTest.Podman([]string{"rmi", "debian:6.0.10"})
+		session.Wait()
+		Expect(session.ExitCode()).To(Equal(125))
+
+	})
+
+	It("podman rmi with fq name", func() {
+		session := podmanTest.Podman([]string{"rmi", image1})
+		session.Wait()
+		Expect(session.ExitCode()).To(Equal(0))
+
+	})
+
+	It("podman rmi with short name", func() {
+		session := podmanTest.Podman([]string{"rmi", "alpine"})
+		session.Wait()
+		Expect(session.ExitCode()).To(Equal(0))
+
+	})
+
+	It("podman rmi all images", func() {
+		podmanTest.PullImages([]string{image3})
+		session := podmanTest.Podman([]string{"rmi", "-a"})
+		session.Wait()
+		Expect(session.ExitCode()).To(Equal(0))
+
+	})
+
+	It("podman rmi all images forceably with short options", func() {
+		podmanTest.PullImages([]string{image3})
+		session := podmanTest.Podman([]string{"rmi", "-fa"})
+		session.Wait()
+		Expect(session.ExitCode()).To(Equal(0))
+
+	})
+
+})
