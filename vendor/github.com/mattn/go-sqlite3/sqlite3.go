4bc9a6d63370b4bd9cfb29e52f4f0dbb0070f856
Matthew Heon <matthew.heon@gmail.com>
2018-02-06 15:38:51 -0500

    HACK: monkey-patch gosqlite3 to rollback unconditionally after tx error
    Signed-off-by: Matthew Heon <matthew.heon@gmail.com>
    
    Closes: #268
    Approved by: rhatdan

--- vendor/github.com/mattn/go-sqlite3/sqlite3.go
+++ vendor/github.com/mattn/go-sqlite3/sqlite3.go
@@ -313,7 +313,7 @@ func (ai *aggInfo) Done(ctx *C.sqlite3_context) {
 // Commit transaction.
 func (tx *SQLiteTx) Commit() error {
 	_, err := tx.c.exec(context.Background(), "COMMIT", nil)
-	if err != nil && err.(Error).Code == C.SQLITE_BUSY {
+	if err != nil { // && err.(Error).Code == C.SQLITE_BUSY {
 		// sqlite3 will leave the transaction open in this scenario.
 		// However, database/sql considers the transaction complete once we
 		// return from Commit() - we must clean up to honour its semantics.
