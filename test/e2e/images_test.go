62b93d172399c20753914d7d91d02ee105a77f6d
Valentin Rothberg <vrothberg@suse.com>
2018-03-14 07:46:14 +0100

    test/e2e/images_test.go: test with multiple tags
    As podman-images(1) had some issues correctly reporting all RepoTags of
    an image (in the default format), extend the e2e tests to avoid running
    into similar in the future.
    
    Signed-off-by: Valentin Rothberg <vrothberg@suse.com>
    
    Closes: #477
    Approved by: rhatdan

--- test/e2e/images_test.go
+++ test/e2e/images_test.go
@@ -37,6 +37,28 @@ var _ = Describe("Podman images", func() {
 		Expect(session.LineInOuputStartsWith("docker.io/library/busybox")).To(BeTrue())
 	})
 
+	It("podman images with multiple tags", func() {
+		// tag "docker.io/library/alpine:latest" to "foo:{a,b,c}"
+		session := podmanTest.Podman([]string{"tag", ALPINE, "foo:a", "foo:b", "foo:c"})
+		session.WaitWithDefaultTimeout()
+		Expect(session.ExitCode()).To(Equal(0))
+		// tag "foo:c" to "bar:{a,b}"
+		session = podmanTest.Podman([]string{"tag", "foo:c", "bar:a", "bar:b"})
+		session.WaitWithDefaultTimeout()
+		Expect(session.ExitCode()).To(Equal(0))
+		// check all previous and the newly tagged images
+		session = podmanTest.Podman([]string{"images"})
+		session.WaitWithDefaultTimeout()
+		Expect(session.ExitCode()).To(Equal(0))
+		session.LineInOutputContainsTag("docker.io/library/alpine", "latest")
+		session.LineInOutputContainsTag("docker.io/library/busybox", "glibc")
+		session.LineInOutputContainsTag("foo", "a")
+		session.LineInOutputContainsTag("foo", "b")
+		session.LineInOutputContainsTag("foo", "c")
+		session.LineInOutputContainsTag("bar", "a")
+		session.LineInOutputContainsTag("bar", "b")
+	})
+
 	It("podman images with digests", func() {
 		session := podmanTest.Podman([]string{"images", "--digests"})
 		session.WaitWithDefaultTimeout()
