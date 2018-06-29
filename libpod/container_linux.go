baa42fd4bd295be9232cde8e528841d3e8fd7d35
W. Trevor King <wking@tremily.us>
2018-06-29 14:18:30 -0700

    libpod/container: Replace containerState* with containerPlatformState
    This way we don't need to stub in structures for other OSes (e.g. the
    Darwin stub in a Linux-only file).  Matthew was concerned about errors
    unmarshalling, say, a Linux state object on a Windows box [1], but we
    can address that in checks when loading the database [2].
    
    [1]: https://github.com/projectatomic/libpod/pull/1015#discussion_r198649043
    [2]: https://github.com/projectatomic/libpod/pull/1015#discussion_r198802956
    
    Signed-off-by: W. Trevor King <wking@tremily.us>
    
    Closes: #1033
    Approved by: mheon

--- libpod/container_linux.go
+++ libpod/container_linux.go
@@ -6,18 +6,10 @@ import (
 	"github.com/containernetworking/plugins/pkg/ns"
 )
 
-type containerStateLinux struct {
+type containerPlatformState struct {
 
 	// NetNSPath is the path of the container's network namespace
 	// Will only be set if config.CreateNetNS is true, or the container was
 	// told to join another container's network namespace
 	NetNS ns.NetNS `json:"-"`
-}
-
-// containerStateWindows is intentionally left as a blank stub
-type containerStateWindows struct {
-}
-
-// containerStateDarwin is intentionally left as a blank stub
-type containerStateDarwin struct {
 }
