49b07389dd25fcecc12302b9d97d46266dfd92e2
Daniel J Walsh <dwalsh@redhat.com>
2018-07-25 17:51:31 -0400

    Vendor latest container/storage to fix overlay mountopt
    Signed-off-by: Daniel J Walsh <dwalsh@redhat.com>

--- vendor/github.com/containers/storage/pkg/mount/mount.go
+++ vendor/github.com/containers/storage/pkg/mount/mount.go
@@ -39,7 +39,7 @@ func Mounted(mountpoint string) (bool, error) {
 // specified like the mount or fstab unix commands: "opt1=val1,opt2=val2". See
 // flags.go for supported option flags.
 func Mount(device, target, mType, options string) error {
-	flag, _ := parseOptions(options)
+	flag, _ := ParseOptions(options)
 	if flag&REMOUNT != REMOUNT {
 		if mounted, err := Mounted(target); err != nil || mounted {
 			return err
@@ -53,7 +53,7 @@ func Mount(device, target, mType, options string) error {
 // specified like the mount or fstab unix commands: "opt1=val1,opt2=val2". See
 // flags.go for supported option flags.
 func ForceMount(device, target, mType, options string) error {
-	flag, data := parseOptions(options)
+	flag, data := ParseOptions(options)
 	return mount(device, target, mType, uintptr(flag), data)
 }
 
