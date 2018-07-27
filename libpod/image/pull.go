d4dbe6677439b91e55ee48c23edfd8f80d61c05d
Miloslav Trmač <mitr@redhat.com>
2018-07-27 03:34:04 +0200

    Reorganize the tag loading in DockerArchive case
    This should not change behavior, only to make future edits
    for an early exit easier to review.
    
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>
    
    Closes: #1176
    Approved by: rhatdan

--- libpod/image/pull.go
+++ libpod/image/pull.go
@@ -110,17 +110,17 @@ func refNamesFromImageReference(ctx context.Context, srcRef types.ImageReference
 		}
 
 		var dest []string
-		if len(manifest[0].RepoTags) > 0 {
-			dest = append(dest, manifest[0].RepoTags...)
-		} else {
+		if len(manifest[0].RepoTags) == 0 {
 			// If the input image has no repotags, we need to feed it a dest anyways
 			digest, err := getImageDigest(ctx, srcRef, sc)
 			if err != nil {
 				return nil, err
 			}
 			dest = append(dest, digest)
+		} else {
+			// Need to load in all the repo tags from the manifest
+			dest = append(dest, manifest[0].RepoTags...)
 		}
-		// Need to load in all the repo tags from the manifest
 		res := []*pullRefName{}
 		for _, dst := range dest {
 			pullInfo := getPullRefName(srcRef, dst)
