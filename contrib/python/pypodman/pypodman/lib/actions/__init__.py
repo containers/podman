3bfe07b4f583b9526b616ec7e49332994ca5bc2e
Jhon Honce <jhonce@redhat.com>
2018-08-16 15:51:26 -0700

    Support Attach subcommand in pypodman
    * Fix some random error handling
    
    Signed-off-by: Jhon Honce <jhonce@redhat.com>
    
    Closes: #1296
    Approved by: rhatdan

--- contrib/python/pypodman/pypodman/lib/actions/__init__.py
+++ contrib/python/pypodman/pypodman/lib/actions/__init__.py
@@ -1,4 +1,5 @@
 """Module to export all the podman subcommands."""
+from pypodman.lib.actions.attach_action import Attach
 from pypodman.lib.actions.create_action import Create
 from pypodman.lib.actions.images_action import Images
 from pypodman.lib.actions.ps_action import Ps
@@ -7,6 +8,7 @@ from pypodman.lib.actions.rm_action import Rm
 from pypodman.lib.actions.rmi_action import Rmi
 
 __all__ = [
+    'Attach',
     'Create',
     'Images',
     'Ps',
