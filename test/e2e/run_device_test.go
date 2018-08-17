1025f6aa0b36476c9bc77e1bc501b55e1e1d0997
Daniel J Walsh <dwalsh@redhat.com>
2018-08-17 18:17:44 -0400

    Fix handling of devices
    Devices are supposed to be able to be passed in via the form of
    
    --device /dev/foo
    --device /dev/foo:/dev/bar
    --device /dev/foo:rwm
    --device /dev/foo:/dev/bar:rwm
    
    Signed-off-by: Daniel J Walsh <dwalsh@redhat.com>

--- test/e2e/run_device_test.go
+++ test/e2e/run_device_test.go
@@ -8,7 +8,7 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Podman kill", func() {
+var _ = Describe("Podman run device", func() {
 	var (
 		tempdir    string
 		err        error
@@ -42,5 +42,31 @@ var _ = Describe("Podman kill", func() {
 		session.WaitWithDefaultTimeout()
 		Expect(session.ExitCode()).To(Equal(0))
 		Expect(session.OutputToString()).To(Equal("/dev/kmsg"))
+	})
+
+	It("podman run device rename test", func() {
+		session := podmanTest.Podman([]string{"run", "-q", "--device", "/dev/kmsg:/dev/kmsg1", ALPINE, "ls", "--color=never", "/dev/kmsg1"})
+		session.WaitWithDefaultTimeout()
+		Expect(session.ExitCode()).To(Equal(0))
+		Expect(session.OutputToString()).To(Equal("/dev/kmsg1"))
+	})
+
+	It("podman run device permission test", func() {
+		session := podmanTest.Podman([]string{"run", "-q", "--device", "/dev/kmsg:r", ALPINE, "ls", "--color=never", "/dev/kmsg"})
+		session.WaitWithDefaultTimeout()
+		Expect(session.ExitCode()).To(Equal(0))
+		Expect(session.OutputToString()).To(Equal("/dev/kmsg"))
+	})
+
+	It("podman run device rename and permission test", func() {
+		session := podmanTest.Podman([]string{"run", "-q", "--device", "/dev/kmsg:/dev/kmsg1:r", ALPINE, "ls", "--color=never", "/dev/kmsg1"})
+		session.WaitWithDefaultTimeout()
+		Expect(session.ExitCode()).To(Equal(0))
+		Expect(session.OutputToString()).To(Equal("/dev/kmsg1"))
+	})
+	It("podman run device rename and bad permission test", func() {
+		session := podmanTest.Podman([]string{"run", "-q", "--device", "/dev/kmsg:/dev/kmsg1:rd", ALPINE, "ls", "--color=never", "/dev/kmsg1"})
+		session.WaitWithDefaultTimeout()
+		Expect(session.ExitCode()).To(Not(Equal(0)))
 	})
 })
