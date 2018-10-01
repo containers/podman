c6a6c06435516f6438a1dfe739845ba6490e0513
baude <bbaude@redhat.com>
2018-10-01 10:15:43 -0500

    re-add BR for golang compiler to contrib/spec/podman.spec.in
    Signed-off-by: baude <bbaude@redhat.com>

--- .copr/prepare.sh
+++ .copr/prepare.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -euf
 set -x
+OS_TEST=${OS_TEST:=0}
 
 if [ ! -e /usr/bin/git ]; then
     dnf -y install git-core
@@ -11,12 +12,19 @@ COMMIT=$(git rev-parse HEAD)
 COMMIT_SHORT=$(git rev-parse --short=8 HEAD)
 COMMIT_NUM=$(git rev-list HEAD --count)
 COMMIT_DATE=$(date +%s)
+BR="#BuildRequires: golang-bin"
+NEWBR="BuildRequires: golang-bin"
+
 
 sed "s,#COMMIT#,${COMMIT},;
      s,#SHORTCOMMIT#,${COMMIT_SHORT},;
      s,#COMMITNUM#,${COMMIT_NUM},;
      s,#COMMITDATE#,${COMMIT_DATE}," \
          contrib/spec/podman.spec.in > contrib/spec/podman.spec
+
+if [ ${OS_TEST} -eq 0 ]; then
+    sed -i "s/${BR}/${NEWBR}/g" contrib/spec/podman.spec
+fi
 
 mkdir build/
 git archive --prefix "libpod-${COMMIT_SHORT}/" --format "tar.gz" HEAD -o "build/libpod-${COMMIT_SHORT}.tar.gz"
