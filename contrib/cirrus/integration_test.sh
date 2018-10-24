e73fed8f67e68abbe3828fb058282ea0c8b72789
Chris Evich <cevich@redhat.com>
2018-10-24 10:30:14 -0400

    DO NOT MERGE: integration test in a tight loop
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- contrib/cirrus/integration_test.sh
+++ contrib/cirrus/integration_test.sh
@@ -9,20 +9,63 @@ OS_RELEASE_ID $OS_RELEASE_ID
 OS_RELEASE_VER $OS_RELEASE_VER
 "
 
-show_env_vars
+make install.tools "BUILDTAGS=$BUILDTAGS"
+make "BUILDTAGS=$BUILDTAGS"
+make install PREFIX=/usr ETCDIR=/etc "BUILDTAGS=$BUILDTAGS"
 
-set -x
-cd "$GOSRC"
-case "${OS_RELEASE_ID}-${OS_RELEASE_VER}" in
-    ubuntu-18)
-        make install PREFIX=/usr ETCDIR=/etc "BUILDTAGS=$BUILDTAGS"
-        make test-binaries "BUILDTAGS=$BUILDTAGS"
-        SKIP_USERNS=1 make localintegration "BUILDTAGS=$BUILDTAGS"
-        ;;
-    fedora-28) ;&  # Continue to the next item
-    centos-7) ;&
-    rhel-7)
-        stub 'integration testing not working on $OS_RELEASE_ID'
-        ;;
-    *) bad_os_id_ver ;;
-esac
+cd $GOSRC/contrib/python/podman
+# Run one test, over and over forever, recording runs, passes, and failures.
+TESTNAME="test.test_containers"
+CMPLTD=0
+PASSED=0
+FAILED=0
+
+echo "."
+echo ">>>>> Beginning continuous testing, only non-zero exits will be shown."
+echo "."
+
+BIGSTART=$(date +%s)
+
+while true
+do
+    START=$(date +%s)
+    set +e  # let was designed by a prehistoric moron
+        # ooe.sh ./test/test_runner.sh -v $TESTNAME &> /tmp/test.log
+        if ooe.sh ./test/test_runner.sh -v &> /tmp/test.log
+        then
+            let PASSED++
+            rm -f /tmp/test.log
+            NOW=$(date +%s)
+            DIFF=$[NOW-START]
+        else
+            let FAILED++
+            mv /tmp/test.log /tmp/failure_${CMPLTD}_test.log
+            NOW=$(date +%s)
+            DIFF=$[NOW-START]
+            echo "-----systemd journal-----" >> /tmp/failure_${CMPLTD}_test.log
+            journalctl -l --since "-$DIFF seconds" >> /tmp/failure_${CMPLTD}_test.log
+        fi
+        let CMPLTD++
+        sync
+        # Wipe out pagecache, dentries and inodes
+        echo 3 > /proc/sys/vm/drop_caches
+    set -e
+    echo "$CMPLTD" > /tmp/test_runs_completed.txt
+    echo "$FAILED" > /tmp/test_runs_failed.txt
+    echo "$PASSED" > /tmp/test_runs_passed.txt
+
+    MSG=" $CMPLTD runs. Of these, $PASSED have passed and $FAILED have failed"
+    echo "."
+    echo ">>>>> Completed $MSG. Time Delta $DIFF."
+    echo "."
+
+    # Give some status so we know it's running
+    BIGDIFF=$[NOW-BIGSTART]
+    if ((BIGDIFF >= 600))
+    then
+        BIGSTART=$(date +%s)
+        ircmsg "cevich/jhonce, sir your cirrus-task-$CIRRUS_TASK_ID completed $MSG"
+    fi
+done
+
+echo "somehow code got here but should not have"
