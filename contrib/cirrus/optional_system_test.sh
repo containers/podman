aa742e9e681d91cf23a7e4e496358cab4d0ececa
Chris Evich <cevich@redhat.com>
2018-11-08 09:28:48 -0500

    Cirrus: Reveal magic, parallel system-testing
    Previously, several magic strings were in place to affect cirrus-ci
    operations.  Two were buried within scripts.  One to optionally
    execute system-tests within a PR. Another to avoid re-building
    cache-images upon every merge.
    
    Move these magic strings out into the open, buy locating their
    logic up-front in the ``.cirrus.yml`` file.  This improves
    readability and reduces surprise/astonishment at runtime.
    
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- contrib/cirrus/optional_system_test.sh
+++ contrib/cirrus/optional_system_test.sh
@@ -3,14 +3,6 @@
 set -e
 source $(dirname $0)/lib.sh
 
-MAGIC_RE='\*\*\*\s*CIRRUS:\s*SYSTEM\s*TEST\s*\*\*\*'
-if ! echo "$CIRRUS_CHANGE_MESSAGE" | egrep -q "$MAGIC_RE"
-then
-    echo "Skipping system-testing because PR title or description"
-    echo "does not match regular expression: $MAGIC_RE"
-    exit 0
-fi
-
 req_env_var "
 GOSRC $GOSRC
 OS_RELEASE_ID $OS_RELEASE_ID
