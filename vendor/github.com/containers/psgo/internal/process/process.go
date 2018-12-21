60a74088fa041adc374e2f7227bfbabaeffeb56d
Daniel J Walsh <dwalsh@redhat.com>
2018-12-21 08:00:49 -0500

    Vendor in latest psgo code to fix race conditions
    When running podman top, the processes collected can exit befor
    psgo gets information on the processes.  This can cause some weird
    errors and creates some CI flakes.
    
    psgo now properly ignores this situation rather then returning errors
    
    Signed-off-by: Daniel J Walsh <dwalsh@redhat.com>

--- vendor/github.com/containers/psgo/internal/process/process.go
+++ vendor/github.com/containers/psgo/internal/process/process.go
@@ -1,3 +1,17 @@
+// Copyright 2018 psgo authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package process
 
 import (
@@ -94,7 +108,7 @@ func FromPIDs(ctx *types.PsContext, pids []string) ([]*Process, error) {
 	for _, pid := range pids {
 		p, err := New(ctx, pid)
 		if err != nil {
-			if os.IsNotExist(err) {
+			if os.IsNotExist(errors.Cause(err)) {
 				// proc parsing is racy
 				// Let's ignore "does not exist" errors
 				continue
