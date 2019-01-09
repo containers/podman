45fb935fe44a383ae14f16ab533281687d642c5e
Jhon Honce <jhonce@redhat.com>
2019-01-09 15:05:58 -0700

    Move python code from contrib to it's own repo python-podman
    Signed-off-by: Jhon Honce <jhonce@redhat.com>

--- .papr_prepare.sh
+++ .papr_prepare.sh
@@ -4,10 +4,8 @@ set -xeuo pipefail
 DIST=${DIST:=Fedora}
 CONTAINER_RUNTIME=${CONTAINER_RUNTIME:=docker}
 IMAGE=fedorapodmanbuild
-PYTHON=python3
 if [[ ${DIST} != "Fedora" ]]; then
     IMAGE=centospodmanbuild
-    PYTHON=python
 fi
 
 # Since CRIU 3.11 has been pushed to Fedora 28 the checkpoint/restore
@@ -21,4 +19,4 @@ modprobe iptable_nat || :
 ${CONTAINER_RUNTIME} build -t ${IMAGE} -f Dockerfile.${DIST} . 2>build.log
 
 # Run the tests
-${CONTAINER_RUNTIME} run --rm --privileged --net=host -v $PWD:/go/src/github.com/containers/libpod:Z --workdir /go/src/github.com/containers/libpod -e CGROUP_MANAGER=cgroupfs -e PYTHON=$PYTHON -e STORAGE_OPTIONS="--storage-driver=vfs" -e CRIO_ROOT="/go/src/github.com/containers/libpod" -e PODMAN_BINARY="/usr/bin/podman" -e CONMON_BINARY="/usr/libexec/podman/conmon" -e DIST=$DIST -e CONTAINER_RUNTIME=$CONTAINER_RUNTIME $IMAGE sh ./.papr.sh -b -i -t
+${CONTAINER_RUNTIME} run --rm --privileged --net=host -v $PWD:/go/src/github.com/containers/libpod:Z --workdir /go/src/github.com/containers/libpod -e CGROUP_MANAGER=cgroupfs -e STORAGE_OPTIONS="--storage-driver=vfs" -e CRIO_ROOT="/go/src/github.com/containers/libpod" -e PODMAN_BINARY="/usr/bin/podman" -e CONMON_BINARY="/usr/libexec/podman/conmon" -e DIST=$DIST -e CONTAINER_RUNTIME=$CONTAINER_RUNTIME $IMAGE sh ./.papr.sh -b -i -t
