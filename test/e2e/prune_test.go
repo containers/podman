6226e24d40310d451a4c357a6bcc3ab81f27e876
Matthew Heon <matthew.heon@pm.me>
2019-02-08 14:38:41 -0500

    Fix tests after cherry-picking
    Signed-off-by: Matthew Heon <matthew.heon@pm.me>

--- test/e2e/prune_test.go
+++ test/e2e/prune_test.go
@@ -39,7 +39,6 @@ var _ = Describe("Podman rm", func() {
 	})
 
 	It("podman container prune containers", func() {
-		SkipIfRemote()
 		top := podmanTest.RunTopContainer("")
 		top.WaitWithDefaultTimeout()
 		Expect(top.ExitCode()).To(Equal(0))
@@ -56,7 +55,6 @@ var _ = Describe("Podman rm", func() {
 	})
 
 	It("podman image prune none images", func() {
-		SkipIfRemote()
 		podmanTest.BuildImage(pruneImage, "alpine_bash:latest", "true")
 
 		none := podmanTest.Podman([]string{"images", "-a"})
