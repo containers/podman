4b40640c81bb488c5bf48478159b34475545528f
baude <bbaude@redhat.com>
2019-02-08 16:43:32 -0600

    Add volume mounts to PS output
    When listing containers, you should be able to list the mounts
    with something like --format "table{{".Mounts}}".
    
    Resolves: #2238
    
    Signed-off-by: baude <bbaude@redhat.com>

--- cmd/podman/shared/container.go
+++ cmd/podman/shared/container.go
@@ -88,6 +88,7 @@ type PsContainerOutput struct {
 	PIDNS     string
 	User      string
 	UTS       string
+	Mounts    string
 }
 
 // Namespace describes output for ps namespace
@@ -228,6 +229,7 @@ func NewBatchContainer(ctr *libpod.Container, opts PsOptions) (PsContainerOutput
 	pso.CreatedAt = ctr.CreatedTime()
 	pso.StartedAt = startedAt
 	pso.Labels = ctr.Labels()
+	pso.Mounts = strings.Join(ctr.UserVolumes(), " ")
 
 	if opts.Namespace {
 		pso.Cgroup = ns.Cgroup
