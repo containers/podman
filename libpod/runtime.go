containers/podman

commit 650cf122e1b33f4d8f4426ee1cc1a4bf00c14798

libpod: allow multiple oci runtimes
This deprecates the libpod.conf variable of `runtime_path=`, and now has
`runtimes=`, like a map for naming the runtime, preparing for a
`--runtime` flag to `podman run` (i.e. runc, kata, etc.)

Reference: https://github.com/containers/libpod/issues/1750

Signed-off-by: Vincent Batts <vbatts@hashbangbash.com>

libpod/runtime.go
@@ -86,7 +86,7 @@ type Runtime struct {
 	imageContext    *types.SystemContext
 	ociRuntime      *OCIRuntime
 	netPlugin       ocicni.CNIPlugin
-	ociRuntimePath  string
+	ociRuntimePath  OCIRuntimePath
 	conmonPath      string
 	valid           bool
 	lock            sync.RWMutex
@@ -94,6 +94,14 @@ type Runtime struct {
 	firewallBackend firewall.FirewallBackend
 	lockManager     lock.Manager
 	configuredFrom  *runtimeConfiguredFrom
+}
+
+// OCIRuntimePath contains information about an OCI runtime.
+type OCIRuntimePath struct {
+	// Name of the runtime to refer to by the --runtime flag
+	Name string
+	// Paths to check for this executable
+	Paths []string
 }
 
 // RuntimeConfig contains configuration options used to set up the runtime
@@ -118,10 +126,8 @@ type RuntimeConfig struct {
 	// cause conflicts in containers/storage
 	// As such this is not exposed via the config file
 	StateType RuntimeStateStore `toml:"-"`
-	// RuntimePath is the path to OCI runtime binary for launching
-	// containers
-	// The first path pointing to a valid file will be used
-	RuntimePath []string `toml:"runtime_path"`
+	// OCIRuntimes are the set of configured OCI runtimes (default is runc)
+	OCIRuntimes []OCIRuntimePath `toml:"runtimes"`
 	// ConmonPath is the path to the Conmon binary used for managing
 	// containers
 	// The first path pointing to a valid file will be used
@@ -213,14 +219,19 @@ var (
 		StorageConfig:         storage.StoreOptions{},
 		ImageDefaultTransport: DefaultTransport,
 		StateType:             BoltDBStateStore,
-		RuntimePath: []string{
-			"/usr/bin/runc",
-			"/usr/sbin/runc",
-			"/usr/local/bin/runc",
-			"/usr/local/sbin/runc",
-			"/sbin/runc",
-			"/bin/runc",
-			"/usr/lib/cri-o-runc/sbin/runc",
+		OCIRuntimes: []OCIRuntimePath{
+			{
+				Name: "runc",
+				Paths: []string{
+					"/usr/bin/runc",
+					"/usr/sbin/runc",
+					"/usr/local/bin/runc",
+					"/usr/local/sbin/runc",
+					"/sbin/runc",
+					"/bin/runc",
+					"/usr/lib/cri-o-runc/sbin/runc",
+				},
+			},
 		},
 		ConmonPath: []string{
 			"/usr/libexec/podman/conmon",
@@ -453,22 +464,25 @@ func NewRuntimeFromConfig(configPath string, options ...RuntimeOption) (runtime 
 func makeRuntime(runtime *Runtime) (err error) {
 	// Find a working OCI runtime binary
 	foundRuntime := false
-	for _, path := range runtime.config.RuntimePath {
-		stat, err := os.Stat(path)
-		if err != nil {
-			continue
+outer:
+	for _, oruntime := range runtime.config.OCIRuntimes {
+		for _, path := range oruntime.Paths {
+			stat, err := os.Stat(path)
+			if err != nil {
+				continue
+			}
+			if stat.IsDir() {
+				continue
+			}
+			foundRuntime = true
+			runtime.ociRuntimePath = OCIRuntimePath{Name: oruntime.Name, Paths: []string{path}}
+			break outer
 		}
-		if stat.IsDir() {
-			continue
-		}
-		foundRuntime = true
-		runtime.ociRuntimePath = path
-		break
 	}
 	if !foundRuntime {
 		return errors.Wrapf(ErrInvalidArg,
 			"could not find a working binary (configured options: %v)",
-			runtime.config.RuntimePath)
+			runtime.config.OCIRuntimes)
 	}
 
 	// Find a working conmon binary
@@ -619,7 +633,7 @@ func makeRuntime(runtime *Runtime) (err error) {
 	}
 
 	// Make an OCI runtime to perform container operations
-	ociRuntime, err := newOCIRuntime("runc", runtime.ociRuntimePath,
+	ociRuntime, err := newOCIRuntime(runtime.ociRuntimePath,
 		runtime.conmonPath, runtime.config.ConmonEnvVars,
 		runtime.config.CgroupManager, runtime.config.TmpDir,
 		runtime.config.MaxLogSize, runtime.config.NoPivotRoot,

libpod/info.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"bytes"
 	"fmt"
-	"github.com/containers/buildah"
 	"io/ioutil"
 	"os"
 	"runtime"
@@ -12,6 +11,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/containers/buildah"
 	"github.com/containers/libpod/pkg/rootless"
 	"github.com/containers/libpod/pkg/util"
 	"github.com/containers/libpod/utils"
@@ -184,12 +184,12 @@ func (r *Runtime) GetConmonVersion() (string, error) {
 
 // GetOCIRuntimePath returns the path to the OCI Runtime Path the runtime is using
 func (r *Runtime) GetOCIRuntimePath() string {
-	return r.ociRuntimePath
+	return r.ociRuntimePath.Paths[0]
 }
 
 // GetOCIRuntimeVersion returns a string representation of the oci runtimes version
 func (r *Runtime) GetOCIRuntimeVersion() (string, error) {
-	output, err := utils.ExecCmd(r.ociRuntimePath, "--version")
+	output, err := utils.ExecCmd(r.ociRuntimePath.Paths[0], "--version")
 	if err != nil {
 		return "", err
 	}

libpod/oci.go
@@ -75,10 +75,10 @@ type syncInfo struct {
 }
 
 // Make a new OCI runtime with provided options
-func newOCIRuntime(name string, path string, conmonPath string, conmonEnv []string, cgroupManager string, tmpDir string, logSizeMax int64, noPivotRoot bool, reservePorts bool) (*OCIRuntime, error) {
+func newOCIRuntime(oruntime OCIRuntimePath, conmonPath string, conmonEnv []string, cgroupManager string, tmpDir string, logSizeMax int64, noPivotRoot bool, reservePorts bool) (*OCIRuntime, error) {
 	runtime := new(OCIRuntime)
-	runtime.name = name
-	runtime.path = path
+	runtime.name = oruntime.Name
+	runtime.path = oruntime.Paths[0]
 	runtime.conmonPath = conmonPath
 	runtime.conmonEnv = conmonEnv
 	runtime.cgroupManager = cgroupManager

libpod/options.go
@@ -147,7 +147,11 @@ func WithOCIRuntime(runtimePath string) RuntimeOption {
 			return errors.Wrapf(ErrInvalidArg, "must provide a valid path")
 		}
 
-		rt.config.RuntimePath = []string{runtimePath}
+		rt.config.OCIRuntimes = []OCIRuntimePath{
+			{
+				Paths: []string{runtimePath},
+			},
+		}
 
 		return nil
 	}

commit 8aa9b85fa7b5abd79826ec1229902eb8e19b4193

libpod: honor runtime_path from libpod.conf
Add backward compatibility for `runtime_path` that was used by older
versions of Podman.

The issue was introduced with: 650cf122e1b33f4d8f4426ee1cc1a4bf00c14798

If `runtime_path` is specified, it overrides any other configuration
and a warning is printed.

It should be considered deprecated and will be removed in future.

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

libpod/runtime.go
@@ -130,6 +130,12 @@ type RuntimeConfig struct {
 	OCIRuntime string `toml:"runtime"`
 	// OCIRuntimes are the set of configured OCI runtimes (default is runc)
 	OCIRuntimes map[string][]string `toml:"runtimes"`
+	// RuntimePath is the path to OCI runtime binary for launching
+	// containers.
+	// The first path pointing to a valid file will be used
+	// This is used only when there are no OCIRuntime/OCIRuntimes defined.  It
+	// is used only to be backward compatible with older versions of Podman.
+	RuntimePath []string `toml:"runtime_path"`
 	// ConmonPath is the path to the Conmon binary used for managing
 	// containers
 	// The first path pointing to a valid file will be used
@@ -403,6 +409,7 @@ func NewRuntime(options ...RuntimeOption) (runtime *Runtime, err error) {
 			runtime.config.ConmonPath = tmpConfig.ConmonPath
 			runtime.config.ConmonEnvVars = tmpConfig.ConmonEnvVars
 			runtime.config.OCIRuntimes = tmpConfig.OCIRuntimes
+			runtime.config.RuntimePath = tmpConfig.RuntimePath
 			runtime.config.CNIPluginDir = tmpConfig.CNIPluginDir
 			runtime.config.NoPivotRoot = tmpConfig.NoPivotRoot
 			break
@@ -485,10 +492,25 @@ func NewRuntimeFromConfig(configPath string, options ...RuntimeOption) (runtime 
 // Make a new runtime based on the given configuration
 // Sets up containers/storage, state store, OCI runtime
 func makeRuntime(runtime *Runtime) (err error) {
+
+	// Backward compatibility for `runtime_path`
+	if runtime.config.RuntimePath != nil {
+		// Don't print twice in rootless mode.
+		if os.Geteuid() == 0 {
+			logrus.Warningf("The configuration is using `runtime_path`, which is deprecated and will be removed in future.  Please use `runtimes` and `runtime`")
+			logrus.Warningf("If you are using both `runtime_path` and `runtime`, the configuration from `runtime_path` is used")
+		}
+
+		// Transform `runtime_path` into `runtimes` and `runtime`.
+		name := filepath.Base(runtime.config.RuntimePath[0])
+		runtime.config.OCIRuntime = name
+		runtime.config.OCIRuntimes = map[string][]string{name: runtime.config.RuntimePath}
+	}
+
 	// Find a working OCI runtime binary
 	foundRuntime := false
 	// If runtime is an absolute path, then use it as it is.
-	if runtime.config.OCIRuntime[0] == '/' {
+	if runtime.config.OCIRuntime != "" && runtime.config.OCIRuntime[0] == '/' {
 		foundRuntime = true
 		runtime.ociRuntimePath = OCIRuntimePath{Name: filepath.Base(runtime.config.OCIRuntime), Paths: []string{runtime.config.OCIRuntime}}
 	} else {