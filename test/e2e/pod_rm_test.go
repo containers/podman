61b41c0875cd49e3cbb770a2406f1372bd8f9757
Chris Evich <cevich@redhat.com>
2019-03-21 15:01:59 -0400

    fix podman pod rm -a race
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- test/e2e/pod_rm_test.go
+++ test/e2e/pod_rm_test.go
@@ -3,6 +3,7 @@
 package integration
 
 import (
+	"fmt"
 	"os"
 
 	. "github.com/containers/libpod/test/utils"
@@ -116,6 +117,7 @@ var _ = Describe("Podman pod rm", func() {
 		// one pod should have been deleted
 		result = podmanTest.Podman([]string{"pod", "ps", "-q"})
 		result.WaitWithDefaultTimeout()
+		fmt.Println("podman pod ps -q returned: \n%s\n", result.OutputToString())
 		Expect(len(result.OutputToStringArray())).To(Equal(1))
 	})
 
