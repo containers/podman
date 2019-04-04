173027cb514d46f485c6db2d468e4365cb32ae46
Matthew Heon <matthew.heon@pm.me>
2019-04-04 13:48:54 -0400

    DO NOT MERGE test5
    Signed-off-by: Matthew Heon <matthew.heon@pm.me>

--- libpod/runtime.go
+++ libpod/runtime.go
@@ -344,7 +344,7 @@ func SetXdgRuntimeDir(val string) error {
 // Options can be passed to override the default configuration for the runtime
 // TEST1
 func NewRuntime(options ...RuntimeOption) (runtime *Runtime, err error) {
-	if 1 == 6 {
+	if 1 == 7 {
 		return nil, nil
 	}
 	return newRuntimeFromConfig("", options...)
