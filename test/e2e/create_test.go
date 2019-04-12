containers/podman

commit ba65301c955454e47c3893ca548f18a845a4c4a9

podman-remote create|run
add the ability to create and run containers via the podman-remote
client.

we now create an intermediate layer from the the create/run cli flags.
the intermediate layer can be converted into a createconfig or into a
varlink struct.  Once transported, the varlink struct can be converted
back to an intermediate layer and then to a createconfig.

remote terminals are not supported yet.

Signed-off-by: baude <bbaude@redhat.com>

pkg/adapter/containers.go
@@ -5,12 +5,17 @@ package adapter
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"strconv"
+	"strings"
 	"sync"
 	"syscall"
 	"time"
 
 	"github.com/containers/libpod/cmd/podman/cliconfig"
+	"github.com/containers/libpod/cmd/podman/shared"
 	"github.com/containers/libpod/libpod"
 	"github.com/containers/libpod/pkg/adapter/shortcuts"
 	"github.com/pkg/errors"
@@ -153,4 +158,149 @@ func (r *LocalRuntime) Log(c *cliconfig.LogsValues, options *libpod.LogOptions) 
 		fmt.Println(line.String(options))
 	}
 	return nil
+}
+
+// CreateContainer creates a libpod container
+func (r *LocalRuntime) CreateContainer(ctx context.Context, c *cliconfig.CreateValues) (string, error) {
+	results := shared.NewIntermediateLayer(&c.PodmanCommand)
+	ctr, _, err := shared.CreateContainer(ctx, &results, r.Runtime)
+	return ctr.ID(), err
+}
+
+// Run a libpod container
+func (r *LocalRuntime) Run(ctx context.Context, c *cliconfig.RunValues, exitCode int) (int, error) {
+	results := shared.NewIntermediateLayer(&c.PodmanCommand)
+
+	ctr, createConfig, err := shared.CreateContainer(ctx, &results, r.Runtime)
+	if err != nil {
+		return exitCode, err
+	}
+
+	if logrus.GetLevel() == logrus.DebugLevel {
+		cgroupPath, err := ctr.CGroupPath()
+		if err == nil {
+			logrus.Debugf("container %q has CgroupParent %q", ctr.ID(), cgroupPath)
+		}
+	}
+
+	// Handle detached start
+	if createConfig.Detach {
+		// if the container was created as part of a pod, also start its dependencies, if any.
+		if err := ctr.Start(ctx, c.IsSet("pod")); err != nil {
+			// This means the command did not exist
+			exitCode = 127
+			if strings.Index(err.Error(), "permission denied") > -1 {
+				exitCode = 126
+			}
+			return exitCode, err
+		}
+
+		fmt.Printf("%s\n", ctr.ID())
+		exitCode = 0
+		return exitCode, nil
+	}
+
+	outputStream := os.Stdout
+	errorStream := os.Stderr
+	inputStream := os.Stdin
+
+	// If -i is not set, clear stdin
+	if !c.Bool("interactive") {
+		inputStream = nil
+	}
+
+	// If attach is set, clear stdin/stdout/stderr and only attach requested
+	if c.IsSet("attach") || c.IsSet("a") {
+		outputStream = nil
+		errorStream = nil
+		if !c.Bool("interactive") {
+			inputStream = nil
+		}
+
+		attachTo := c.StringSlice("attach")
+		for _, stream := range attachTo {
+			switch strings.ToLower(stream) {
+			case "stdout":
+				outputStream = os.Stdout
+			case "stderr":
+				errorStream = os.Stderr
+			case "stdin":
+				inputStream = os.Stdin
+			default:
+				return exitCode, errors.Wrapf(libpod.ErrInvalidArg, "invalid stream %q for --attach - must be one of stdin, stdout, or stderr", stream)
+			}
+		}
+	}
+	// if the container was created as part of a pod, also start its dependencies, if any.
+	if err := StartAttachCtr(ctx, ctr, outputStream, errorStream, inputStream, c.String("detach-keys"), c.Bool("sig-proxy"), true, c.IsSet("pod")); err != nil {
+		// We've manually detached from the container
+		// Do not perform cleanup, or wait for container exit code
+		// Just exit immediately
+		if errors.Cause(err) == libpod.ErrDetach {
+			exitCode = 0
+			return exitCode, nil
+		}
+		// This means the command did not exist
+		exitCode = 127
+		if strings.Index(err.Error(), "permission denied") > -1 {
+			exitCode = 126
+		}
+		if c.IsSet("rm") {
+			if deleteError := r.Runtime.RemoveContainer(ctx, ctr, true, false); deleteError != nil {
+				logrus.Errorf("unable to remove container %s after failing to start and attach to it", ctr.ID())
+			}
+		}
+		return exitCode, err
+	}
+
+	if ecode, err := ctr.Wait(); err != nil {
+		if errors.Cause(err) == libpod.ErrNoSuchCtr {
+			// The container may have been removed
+			// Go looking for an exit file
+			config, err := r.Runtime.GetConfig()
+			if err != nil {
+				return exitCode, err
+			}
+			ctrExitCode, err := ReadExitFile(config.TmpDir, ctr.ID())
+			if err != nil {
+				logrus.Errorf("Cannot get exit code: %v", err)
+				exitCode = 127
+			} else {
+				exitCode = ctrExitCode
+			}
+		}
+	} else {
+		exitCode = int(ecode)
+	}
+
+	if c.IsSet("rm") {
+		r.Runtime.RemoveContainer(ctx, ctr, false, true)
+	}
+
+	return exitCode, nil
+}
+
+// ReadExitFile reads a container's exit file
+func ReadExitFile(runtimeTmp, ctrID string) (int, error) {
+	exitFile := filepath.Join(runtimeTmp, "exits", fmt.Sprintf("%s-old", ctrID))
+
+	logrus.Debugf("Attempting to read container %s exit code from file %s", ctrID, exitFile)
+
+	// Check if it exists
+	if _, err := os.Stat(exitFile); err != nil {
+		return 0, errors.Wrapf(err, "error getting exit file for container %s", ctrID)
+	}
+
+	// File exists, read it in and convert to int
+	statusStr, err := ioutil.ReadFile(exitFile)
+	if err != nil {
+		return 0, errors.Wrapf(err, "error reading exit file for container %s", ctrID)
+	}
+
+	exitCode, err := strconv.Atoi(string(statusStr))
+	if err != nil {
+		return 0, errors.Wrapf(err, "error parsing exit code for container %s", ctrID)
+	}
+
+	return exitCode, nil
 }

pkg/varlinkapi/containers.go
@@ -1,3 +1,5 @@
+// +build varlink
+
 package varlinkapi
 
 import (
@@ -581,27 +583,6 @@ func (i *LibpodAPI) GetContainerStatsWithHistory(call iopodman.VarlinkCall, prev
 		Pids:         int64(stats.PIDs),
 	}
 	return call.ReplyGetContainerStatsWithHistory(cStats)
-}
-
-// ContainerStatsToLibpodContainerStats converts the varlink containerstats to a libpod
-// container stats
-func ContainerStatsToLibpodContainerStats(stats iopodman.ContainerStats) libpod.ContainerStats {
-	cstats := libpod.ContainerStats{
-		ContainerID: stats.Id,
-		Name:        stats.Name,
-		CPU:         stats.Cpu,
-		CPUNano:     uint64(stats.Cpu_nano),
-		SystemNano:  uint64(stats.System_nano),
-		MemUsage:    uint64(stats.Mem_usage),
-		MemLimit:    uint64(stats.Mem_limit),
-		MemPerc:     stats.Mem_perc,
-		NetInput:    uint64(stats.Net_input),
-		NetOutput:   uint64(stats.Net_output),
-		BlockInput:  uint64(stats.Block_input),
-		BlockOutput: uint64(stats.Block_output),
-		PIDs:        uint64(stats.Pids),
-	}
-	return cstats
 }
 
 // GetContainersLogs is the varlink endpoint to obtain one or more container logs

cmd/podman/attach.go
@@ -6,6 +6,7 @@ import (
 	"github.com/containers/libpod/cmd/podman/cliconfig"
 	"github.com/containers/libpod/cmd/podman/libpodruntime"
 	"github.com/containers/libpod/libpod"
+	"github.com/containers/libpod/pkg/adapter"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
@@ -78,7 +79,7 @@ func attachCmd(c *cliconfig.AttachValues) error {
 	}
 
 	// If the container is in a pod, also set to recursively start dependencies
-	if err := startAttachCtr(ctr, os.Stdout, os.Stderr, inputStream, c.DetachKeys, c.SigProxy, false, ctr.PodID() != ""); err != nil && errors.Cause(err) != libpod.ErrDetach {
+	if err := adapter.StartAttachCtr(getContext(), ctr, os.Stdout, os.Stderr, inputStream, c.DetachKeys, c.SigProxy, false, ctr.PodID() != ""); err != nil && errors.Cause(err) != libpod.ErrDetach {
 		return errors.Wrapf(err, "error attaching to container %s", ctr.ID())
 	}
 

cmd/podman/cliconfig/defaults.go
@@ -1,0 +1,21 @@
+package cliconfig
+
+const (
+	// DefaultSystemD value
+	DefaultSystemD bool = true
+)
+
+var (
+	// DefaultHealthCheckInterval default value
+	DefaultHealthCheckInterval = "30s"
+	// DefaultHealthCheckRetries default value
+	DefaultHealthCheckRetries uint = 3
+	// DefaultHealthCheckStartPeriod default value
+	DefaultHealthCheckStartPeriod = "0s"
+	// DefaultHealthCheckTimeout default value
+	DefaultHealthCheckTimeout = "30s"
+	// DefaultImageVolume default value
+	DefaultImageVolume = "bind"
+	// DefaultShmSize default value
+	DefaultShmSize = "65536k"
+)

cmd/podman/commands.go
@@ -13,7 +13,6 @@ func getMainCommands() []*cobra.Command {
 	rootCommands := []*cobra.Command{
 		_attachCommand,
 		_commitCommand,
-		_createCommand,
 		_execCommand,
 		_generateCommand,
 		_playCommand,
@@ -26,7 +25,6 @@ func getMainCommands() []*cobra.Command {
 		_refreshCommand,
 		_restartCommand,
 		_rmCommand,
-		_runCommand,
 		_searchCommand,
 		_startCommand,
 		_statsCommand,
@@ -56,7 +54,6 @@ func getContainerSubCommands() []*cobra.Command {
 		_checkpointCommand,
 		_cleanupCommand,
 		_commitCommand,
-		_createCommand,
 		_execCommand,
 		_exportCommand,
 		_killCommand,
@@ -68,7 +65,6 @@ func getContainerSubCommands() []*cobra.Command {
 		_restartCommand,
 		_restoreCommand,
 		_rmCommand,
-		_runCommand,
 		_runlabelCommand,
 		_startCommand,
 		_statsCommand,

cmd/podman/common.go
@@ -294,19 +294,19 @@ func getCreateFlags(c *cliconfig.PodmanCommand) {
 		"set a healthcheck command for the container ('none' disables the existing healthcheck)",
 	)
 	createFlags.String(
-		"healthcheck-interval", "30s",
+		"healthcheck-interval", cliconfig.DefaultHealthCheckInterval,
 		"set an interval for the healthchecks (a value of disable results in no automatic timer setup)",
 	)
 	createFlags.Uint(
-		"healthcheck-retries", 3,
+		"healthcheck-retries", cliconfig.DefaultHealthCheckRetries,
 		"the number of retries allowed before a healthcheck is considered to be unhealthy",
 	)
 	createFlags.String(
-		"healthcheck-start-period", "0s",
+		"healthcheck-start-period", cliconfig.DefaultHealthCheckStartPeriod,
 		"the initialization time needed for a container to bootstrap",
 	)
 	createFlags.String(
-		"healthcheck-timeout", "30s",
+		"healthcheck-timeout", cliconfig.DefaultHealthCheckTimeout,
 		"the maximum time allowed to complete the healthcheck before an interval is considered failed",
 	)
 	createFlags.StringP(
@@ -314,7 +314,7 @@ func getCreateFlags(c *cliconfig.PodmanCommand) {
 		"Set container hostname",
 	)
 	createFlags.String(
-		"image-volume", "bind",
+		"image-volume", cliconfig.DefaultImageVolume,
 		"Tells podman how to handle the builtin image volumes. The options are: 'bind', 'tmpfs', or 'ignore'",
 	)
 	createFlags.Bool(
@@ -451,7 +451,7 @@ func getCreateFlags(c *cliconfig.PodmanCommand) {
 		"Security Options (default [])",
 	)
 	createFlags.String(
-		"shm-size", "65536k",
+		"shm-size", cliconfig.DefaultShmSize,
 		"Size of `/dev/shm`. The format is `<number><unit>`",
 	)
 	createFlags.String(
@@ -480,7 +480,7 @@ func getCreateFlags(c *cliconfig.PodmanCommand) {
 		"Sysctl options (default [])",
 	)
 	createFlags.Bool(
-		"systemd", true,
+		"systemd", cliconfig.DefaultSystemD,
 		"Run container in systemd mode if the command executable is systemd or init",
 	)
 	createFlags.StringSlice(

cmd/podman/container.go
@@ -53,8 +53,10 @@ var (
 		_containerExistsCommand,
 		_contInspectSubCommand,
 		_diffCommand,
+		_createCommand,
 		_listSubCommand,
 		_logsCommand,
+		_runCommand,
 	}
 )
 

cmd/podman/create.go
@@ -4,8 +4,7 @@ import (
 	"fmt"
 
 	"github.com/containers/libpod/cmd/podman/cliconfig"
-	"github.com/containers/libpod/cmd/podman/libpodruntime"
-	"github.com/containers/libpod/cmd/podman/shared"
+	"github.com/containers/libpod/pkg/adapter"
 	"github.com/opentracing/opentracing-go"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
@@ -52,18 +51,17 @@ func createCmd(c *cliconfig.CreateValues) error {
 		return err
 	}
 
-	runtime, err := libpodruntime.GetRuntime(&c.PodmanCommand)
+	runtime, err := adapter.GetRuntime(&c.PodmanCommand)
 	if err != nil {
 		return errors.Wrapf(err, "error creating libpod runtime")
 	}
 	defer runtime.Shutdown(false)
 
-	ctr, _, err := shared.CreateContainer(getContext(), &c.PodmanCommand, runtime)
+	cid, err := runtime.CreateContainer(getContext(), c)
 	if err != nil {
 		return err
 	}
-
-	fmt.Printf("%s\n", ctr.ID())
+	fmt.Printf("%s\n", cid)
 	return nil
 }
 

commit 0a7b5b4e086ee4583b8550d80934dfa5c356ff64

create: fix segfault if container name already exists
do not try to use ctr if there was an error.  It fixes a segfault when
there is already a container with the same name.

regression introduced by: ba65301c955454e47c3893ca548f18a845a4c4a9

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

pkg/adapter/containers.go
@@ -257,7 +257,10 @@ func (r *LocalRuntime) Log(c *cliconfig.LogsValues, options *libpod.LogOptions) 
 func (r *LocalRuntime) CreateContainer(ctx context.Context, c *cliconfig.CreateValues) (string, error) {
 	results := shared.NewIntermediateLayer(&c.PodmanCommand, false)
 	ctr, _, err := shared.CreateContainer(ctx, &results, r.Runtime)
-	return ctr.ID(), err
+	if err != nil {
+		return "", err
+	}
+	return ctr.ID(), nil
 }
 
 // Run a libpod container

test/e2e/create_test.go
@@ -70,6 +70,17 @@ var _ = Describe("Podman create", func() {
 		Expect(podmanTest.NumberOfContainers()).To(Equal(1))
 	})
 
+	It("podman create using existing name", func() {
+		session := podmanTest.Podman([]string{"create", "--name=foo", ALPINE, "ls"})
+		session.WaitWithDefaultTimeout()
+		Expect(session.ExitCode()).To(Equal(0))
+		Expect(podmanTest.NumberOfContainers()).To(Equal(1))
+
+		session = podmanTest.Podman([]string{"create", "--name=foo", ALPINE, "ls"})
+		session.WaitWithDefaultTimeout()
+		Expect(session.ExitCode()).To(Equal(125))
+	})
+
 	It("podman create adds annotation", func() {
 		session := podmanTest.Podman([]string{"create", "--annotation", "HELLO=WORLD", ALPINE, "ls"})
 		session.WaitWithDefaultTimeout()