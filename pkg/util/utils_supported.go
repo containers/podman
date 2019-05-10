8e88461511e81d2327e4c1a1315bb58fda1827ca
Giuseppe Scrivano <gscrivan@redhat.com>
2019-05-10 16:20:55 +0200

    rootless, spec: allow resources with cgroup v2
    We were always raising an error when the rootless user attempted to
    setup resources, but this is not the case anymore with cgroup v2.
    
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- pkg/util/utils_supported.go
+++ pkg/util/utils_supported.go
@@ -11,8 +11,32 @@ import (
 	"github.com/pkg/errors"
 	"os"
 	"path/filepath"
+	"sync"
 	"syscall"
 )
+
+const (
+	_cgroup2SuperMagic = 0x63677270
+)
+
+var (
+	isUnifiedOnce sync.Once
+	isUnified     bool
+	isUnifiedErr  error
+)
+
+// IsCgroup2UnifiedMode returns whether we are running in cgroup 2 unified mode.
+func IsCgroup2UnifiedMode() (bool, error) {
+	isUnifiedOnce.Do(func() {
+		var st syscall.Statfs_t
+		if err := syscall.Statfs("/sys/fs/cgroup", &st); err != nil {
+			isUnified, isUnifiedErr = false, err
+		} else {
+			isUnified, isUnifiedErr = st.Type == _cgroup2SuperMagic, nil
+		}
+	})
+	return isUnified, isUnifiedErr
+}
 
 // GetRootlessRuntimeDir returns the runtime directory when running as non root
 func GetRootlessRuntimeDir() (string, error) {
--- pkg/util/utils_windows.go
+++ pkg/util/utils_windows.go
@@ -10,3 +10,8 @@ import (
 func GetRootlessRuntimeDir() (string, error) {
 	return "", errors.New("this function is not implemented for windows")
 }
+
+// IsCgroup2UnifiedMode returns whether we are running in cgroup 2 unified mode.
+func IsCgroup2UnifiedMode() (bool, error) {
+	return false, errors.New("this function is not implemented for windows")
+}
