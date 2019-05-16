ee3381f8f2471513dd258dcb64568b57ad4e30d5
Divyansh Kamboj <kambojdivyansh2000@gmail.com>
2019-05-16 20:57:39 +0530

    Minor fix splitting env vars in podman-commit
    `string.Split()` splits into slice of size greater than 2
    which may result in loss of environment variables
    
    fixes #3132
    
    Signed-off-by: Divyansh Kamboj <kambojdivyansh2000@gmail.com>

--- test/e2e/commit_test.go
+++ test/e2e/commit_test.go
@@ -194,4 +194,24 @@ var _ = Describe("Podman commit", func() {
 		Expect(r.ExitCode()).To(Equal(0))
 	})
 
+	It("podman commit container check env variables", func() {
+		s := podmanTest.Podman([]string{"run", "--name", "test1", "-e", "TEST=1=1-01=9.01", "-it", "alpine", "true"})
+		s.WaitWithDefaultTimeout()
+		Expect(s.ExitCode()).To(Equal(0))
+
+		c := podmanTest.Podman([]string{"commit", "test1", "newimage"})
+		c.WaitWithDefaultTimeout()
+		Expect(c.ExitCode()).To(Equal(0))
+
+		inspect := podmanTest.Podman([]string{"inspect", "newimage"})
+		inspect.WaitWithDefaultTimeout()
+		Expect(inspect.ExitCode()).To(Equal(0))
+		image := inspect.InspectImageJSON()
+
+		envMap := make(map[string]bool)
+		for _, v := range image[0].Config.Env {
+			envMap[v] = true
+		}
+		Expect(envMap["TEST=1=1-01=9.01"]).To(BeTrue())
+	})
 })
