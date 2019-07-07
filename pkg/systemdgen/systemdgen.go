5ea1a4f6b3e89fce51feb16c6df3e7d3b1b894f4
Danila Kiver <danila.kiver@mail.ru>
2019-07-07 11:20:41 +0300

    Do not hardcode podman binary location in generate systemd.
    It is not correct to rely on specific location of the podman binary.
    In most cases it is /usr/bin/podman, but sometimes is not (e.g. in
    system tests). Use /proc/self/exe instead of hardcoded path.
    
    Signed-off-by: Danila Kiver <danila.kiver@mail.ru>

--- pkg/systemdgen/systemdgen.go
+++ pkg/systemdgen/systemdgen.go
@@ -2,16 +2,18 @@ package systemdgen
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 )
 
 var template = `[Unit]
 Description=%s Podman Container
 [Service]
 Restart=%s
-ExecStart=/usr/bin/podman start %s
-ExecStop=/usr/bin/podman stop -t %d %s
+ExecStart=%s start %s
+ExecStop=%s stop -t %d %s
 KillMode=none
 Type=forking
 PIDFile=%s
@@ -33,10 +35,25 @@ func ValidateRestartPolicy(restart string) error {
 // CreateSystemdUnitAsString takes variables to create a systemd unit file used to control
 // a libpod container
 func CreateSystemdUnitAsString(name, cid, restart, pidFile string, stopTimeout int) (string, error) {
+	podmanExe := getPodmanExecutable()
+	return createSystemdUnitAsString(podmanExe, name, cid, restart, pidFile, stopTimeout)
+}
+
+func createSystemdUnitAsString(exe, name, cid, restart, pidFile string, stopTimeout int) (string, error) {
 	if err := ValidateRestartPolicy(restart); err != nil {
 		return "", err
 	}
 
-	unit := fmt.Sprintf(template, name, restart, name, stopTimeout, name, pidFile)
+	unit := fmt.Sprintf(template, name, restart, exe, name, exe, stopTimeout, name, pidFile)
 	return unit, nil
+}
+
+func getPodmanExecutable() string {
+	podmanExe, err := os.Executable()
+	if err != nil {
+		podmanExe = "/usr/bin/podman"
+		logrus.Warnf("Could not obtain podman executable location, using default %s", podmanExe)
+	}
+
+	return podmanExe
 }
