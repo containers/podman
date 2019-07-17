6972617be4081b2b5709ff1b841f53bbc7f94993
Matthew Heon <matthew.heon@pm.me>
2019-07-17 15:17:26 -0400

    Expose Null eventer and allow its use in the Podman CLI
    We need this specifically for tests, but others may find it
    useful if they don't explicitly need events and don't want the
    performance implications of using them.
    
    Signed-off-by: Matthew Heon <matthew.heon@pm.me>

--- libpod/events/events.go
+++ libpod/events/events.go
@@ -16,11 +16,16 @@ var ErrNoJournaldLogging = errors.New("No support for journald logging")
 
 // String returns a string representation of EventerType
 func (et EventerType) String() string {
-	if et == LogFile {
+	switch et {
+	case LogFile:
 		return "file"
-
+	case Journald:
+		return "journald"
+	case Null:
+		return "null"
+	default:
+		return "invalid"
 	}
-	return "journald"
 }
 
 // IsValidEventer checks if the given string is a valid eventer type.
@@ -29,6 +34,8 @@ func IsValidEventer(eventer string) bool {
 	case LogFile.String():
 		return true
 	case Journald.String():
+		return true
+	case Null.String():
 		return true
 	default:
 		return false
