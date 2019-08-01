da8ff6479dd2f380caede8b48cb0c944914b3fb6
Divyansh Kamboj <kambojdivyansh2000@gmail.com>
2019-08-02 02:49:00 +0530

    Integration tests for generate-seccmop
    Signed-off-by: Divyansh Kamboj <kambojdivyansh2000@gmail.com>

--- test/e2e/run_generate_seccomp_test.go
+++ test/e2e/run_generate_seccomp_test.go
@@ -1,9 +1,13 @@
 package integration
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"os"
+	"reflect"
 	"time"
+
+	"github.com/docker/docker/api/types"
 
 	. "github.com/containers/libpod/test/utils"
 	. "github.com/onsi/ginkgo"
@@ -36,27 +40,69 @@ var _ = Describe("Podman generate profile", func() {
 	FIt("podman generate-seccomp generates profile that works ", func() {
 		tmpfile, _ := ioutil.TempFile("", "generate-seccomp.*.json")
 		fileName := tmpfile.Name()
-		tmpfile.Close()
+
+		defer tmpfile.Close()
 		defer os.Remove(fileName)
-		time.Sleep(time.Second * 2)
-		session := podmanTest.Podman([]string{"run", "--generate-seccomp=" + fileName, ALPINE, "true"})
+
+		session := podmanTest.Podman([]string{"run", "--annotation", "io.podman.trace-syscall=" + fileName, ALPINE, "ls"})
 		session.WaitWithDefaultTimeout()
 		Expect(session.ExitCode()).To(Equal(0))
-		result := podmanTest.Podman([]string{"run", "--security-opt", "seccomp=" + fileName, ALPINE, "true"})
+
+		time.Sleep(time.Second * 2)
+
+		result := podmanTest.Podman([]string{"run", "--security-opt", "seccomp=" + fileName, ALPINE, "ls"})
 		result.WaitWithDefaultTimeout()
 		Expect(session.ExitCode()).To(Equal(0))
 	})
-	FIt("podman generate-seccomp should fail to run command barred in logfile", func() {
+	FIt("podman generate-seccomp should fail to run syscalls not present in the profile", func() {
 		tmpfile, _ := ioutil.TempFile("", "generate-seccomp.*.json")
 		fileName := tmpfile.Name()
-		tmpfile.Close()
+
+		defer tmpfile.Close()
 		defer os.Remove(fileName)
-		time.Sleep(time.Second * 2)
-		session := podmanTest.Podman([]string{"run", "--generate-seccomp=" + fileName, ALPINE, "true"})
+
+		session := podmanTest.Podman([]string{"run", "--annotation", "io.podman.trace-syscall=" + fileName, ALPINE, "true"})
 		session.WaitWithDefaultTimeout()
 		Expect(session.ExitCode()).To(Equal(0))
-		result := podmanTest.Podman([]string{"run", "--security-opt", "seccomp=" + fileName, ALPINE, "sync"})
+
+		time.Sleep(time.Second * 2)
+
+		result := podmanTest.Podman([]string{"run", "--security-opt", "seccomp=" + fileName, ALPINE, "ls"})
 		result.WaitWithDefaultTimeout()
+		Expect(result.ExitCode()).ToNot(Equal(0))
+	})
+	FIt("podman generate-seccomp should generate the same seccomp profile for identical containers", func() {
+		tmpfile, _ := ioutil.TempFile("", "generate-seccomp.*.json")
+		profilePath1 := tmpfile.Name()
+
+		tmpfile.Close()
+		defer os.Remove(profilePath1)
+
+		session := podmanTest.Podman([]string{"run", "--annotation", "io.podman.trace-syscall=" + profilePath1, ALPINE, "ls"})
+		session.WaitWithDefaultTimeout()
 		Expect(session.ExitCode()).To(Equal(0))
+
+		tmpfile, _ = ioutil.TempFile("", "generate-seccomp.*.json")
+		profilePath2 := tmpfile.Name()
+
+		tmpfile.Close()
+		defer os.Remove(profilePath2)
+
+		result := podmanTest.Podman([]string{"run", "--annotation", "io.podman.trace-syscall=" + profilePath2, ALPINE, "ls"})
+		result.WaitWithDefaultTimeout()
+		Expect(result.ExitCode()).To(Equal(0))
+
+		time.Sleep(time.Second * 2)
+
+		profileJSON1, _ := ioutil.ReadFile(profilePath1)
+		profile1 := types.Seccomp{}
+		err := json.Unmarshal(profileJSON1, &profile1)
+		Expect(err).To(BeNil())
+
+		profileJSON2, _ := ioutil.ReadFile(profilePath2)
+		profile2 := types.Seccomp{}
+		err = json.Unmarshal(profileJSON2, &profile2)
+		Expect(err).To(BeNil())
+		Expect(reflect.DeepEqual(profile1.Syscalls, profile1.Syscalls)).To(Equal(true))
 	})
 })
