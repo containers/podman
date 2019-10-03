f8deaf9f9d90c7edd858171691db878de3d04d86
Chris Evich <cevich@redhat.com>
2019-10-03 14:12:18 -0400

    Cirrus: Fix log URIs & add optional $ALSO_FILENAME
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- contrib/upldrel/entrypoint.sh
+++ contrib/upldrel/entrypoint.sh
@@ -7,7 +7,7 @@ source /usr/local/bin/lib_entrypoint.sh
 req_env_var GCPJSON_FILEPATH GCPNAME GCPPROJECT BUCKET FROM_FILEPATH TO_FILENAME ALSO_FILENAME
 
 [[ -r "$FROM_FILEPATH" ]] || \
-    die 2 ERROR Cannot read release archive file: "$REL_ARC_FILEPATH"
+    die 2 ERROR Cannot read release archive file: "$FROM_FILEPATH"
 
 [[ -r "$GCPJSON_FILEPATH" ]] || \
     die 3 ERROR Cannot read GCP credentials file: "$GCPJSON_FILEPATH"
@@ -17,9 +17,11 @@ gcloud_init "$GCPJSON_FILEPATH"
 
 echo "Uploading archive as $TO_FILENAME"
 gsutil cp "$FROM_FILEPATH" "gs://$BUCKET/$TO_FILENAME"
-gsutil cp "$FROM_FILEPATH" "gs://$BUCKET/$ALSO_FILENAME"
+[[ -z "$ALSO_FILENAME" ]] || \
+    gsutil cp "$FROM_FILEPATH" "gs://$BUCKET/$ALSO_FILENAME"
 
 echo "."
 echo "Release now available for download at:"
-echo "    https://storage.cloud.google.com/$BUCKET/$TO_FILENAME"
-echo "    https://storage.cloud.google.com/$BUCKET/$ALSO_FILENAME"
+echo "    https://storage.googleapis.com/$BUCKET/$TO_FILENAME"
+[[ -z "$ALSO_FILENAME" ]] || \
+    echo "    https://storage.googleapis.com/$BUCKET/$ALSO_FILENAME"
