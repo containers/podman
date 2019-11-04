26481ce3dc6b5f7f658574b9835a5dbdbea9ec52
Jhon Honce <jhonce@redhat.com>
2019-11-04 16:11:04 -0700

    Cleaned up code, implemented info content
    * Move Content-Type check into serviceHandler
    * Custom 404 handler showing the url, mostly for debugging
    * Refactored images: better method names and explicit http codes
    * Added content to /info
    * Added podman fields to Info struct
    * Added Container struct
    
    Signed-off-by: Jhon Honce <jhonce@redhat.com>

--- pkg/serviceapi/handler.go
+++ pkg/serviceapi/handler.go
@@ -1,6 +1,8 @@
 package serviceapi
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/containers/libpod/libpod"
@@ -9,5 +11,24 @@ import (
 type serviceHandler func(w http.ResponseWriter, r *http.Request, runtime *libpod.Runtime)
 
 func (h serviceHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	contentType := r.Header.Get("Content-Type")
+	if contentType != "" && contentType != "application/json" {
+		apiError(w,
+			fmt.Sprintf("%s is not a supported Content-Type", r.Header.Get("Content-Type")),
+			http.StatusUnsupportedMediaType)
+		return
+	}
+
 	h(w, r, libpodRuntime)
+}
+
+func apiError(w http.ResponseWriter, error string, code int) {
+	msg := struct {
+		message string
+	}{
+		error,
+	}
+
+	w.WriteHeader(code)
+	fmt.Fprintln(w, json.Marshal(msg))
 }
