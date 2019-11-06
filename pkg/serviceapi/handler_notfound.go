7298cd5b5621c5eb5d568c0a1a6e8f4548d7aabf
Jhon Honce <jhonce@redhat.com>
2019-11-06 15:22:50 -0700

    Add components to Version, fix Error body
    Signed-off-by: Jhon Honce <jhonce@redhat.com>

--- pkg/serviceapi/handler_notfound.go
+++ pkg/serviceapi/handler_notfound.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
 )
 
 func registerNotFoundHandlers(r *mux.Router) error {
@@ -13,7 +14,7 @@ func registerNotFoundHandlers(r *mux.Router) error {
 }
 
 func notFound(w http.ResponseWriter, r *http.Request) {
-	http.Error(w,
-		fmt.Sprintf("%d %s for '%s'", http.StatusNotFound, http.StatusText(http.StatusNotFound), r.URL.String()),
-		http.StatusNotFound)
+	msg := fmt.Sprintf("%d %s for '%s'", http.StatusNotFound, http.StatusText(http.StatusNotFound), r.URL.String())
+	log.Debugf(msg)
+	http.Error(w, msg, http.StatusNotFound)
 }
