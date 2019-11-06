b435e0b5ede7aad4cbae6e4ce62276c8a7fe6c01
Jhon Honce <jhonce@redhat.com>
2019-11-06 15:22:50 -0700

    Add components to Version, fix Error body
    Signed-off-by: Jhon Honce <jhonce@redhat.com>

--- pkg/serviceapi/handler_version.go
+++ pkg/serviceapi/handler_version.go
@@ -15,6 +15,7 @@ import (
 
 func registerVersionHandlers(r *mux.Router) error {
 	r.Handle(unversionedPath("/version"), serviceHandler(versionHandler))
+	r.Handle("/version", serviceHandler(versionHandler))
 	return nil
 }
 
@@ -32,22 +33,38 @@ func versionHandler(w http.ResponseWriter, r *http.Request, runtime *libpod.Runt
 	}
 	hostInfo := infoData[0].Data
 
+	components := []docker.ComponentVersion{{
+		Name:    "Engine",
+		Version: versionInfo.Version,
+		Details: map[string]string{
+			"APIVersion":    DefaultApiVersion,
+			"Arch":          goRuntime.GOARCH,
+			"BuildTime":     time.Unix(versionInfo.Built, 0).Format(time.RFC3339),
+			"Experimental":  "true",
+			"GitCommit":     versionInfo.GitCommit,
+			"GoVersion":     versionInfo.GoVersion,
+			"KernelVersion": hostInfo["kernel"].(string),
+			"MinAPIVersion": MinimalApiVersion,
+			"Os":            goRuntime.GOOS,
+		},
+	}}
+
 	w.(ServiceWriter).WriteJSON(http.StatusOK, Version{docker.Version{
 		Platform: struct {
 			Name string
 		}{
 			Name: fmt.Sprintf("%s/%s/%s", goRuntime.GOOS, goRuntime.GOARCH, hostInfo["Distribution"].(map[string]interface{})["distribution"].(string)),
 		},
-		Components:    nil,
-		Version:       versionInfo.Version,
-		APIVersion:    DefaultApiVersion,
-		MinAPIVersion: MinimalApiVersion,
-		GitCommit:     versionInfo.GitCommit,
-		GoVersion:     versionInfo.GoVersion,
-		Os:            goRuntime.GOOS,
-		Arch:          goRuntime.GOARCH,
-		KernelVersion: hostInfo["kernel"].(string),
+		APIVersion:    components[0].Details["APIVersion"],
+		Arch:          components[0].Details["Arch"],
+		BuildTime:     components[0].Details["BuildTime"],
+		Components:    components,
 		Experimental:  true,
-		BuildTime:     time.Unix(versionInfo.Built, 0).Format(time.RFC3339),
+		GitCommit:     components[0].Details["GitCommit"],
+		GoVersion:     components[0].Details["GoVersion"],
+		KernelVersion: components[0].Details["KernelVersion"],
+		MinAPIVersion: components[0].Details["MinAPIVersion"],
+		Os:            components[0].Details["Os"],
+		Version:       components[0].Version,
 	}})
 }
