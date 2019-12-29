210baecea22f5c78860af8ab498bc28d7766293e
baude <bbaude@redhat.com>
2019-12-29 11:39:02 -0600

    [CI:DOCS]apiv2 healthcheck enablement
    wire up container healthchecks for the api.
    
    Signed-off-by: baude <bbaude@redhat.com>

--- pkg/bindings/healthcheck.go
+++ pkg/bindings/healthcheck.go
@@ -1,3 +1,30 @@
 package bindings
 
-func (c Connection) RunHealthCheck() {}
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/containers/libpod/libpod"
+)
+
+/*
+	All methods still need error handling defined based on the http response codes.
+*/
+
+func (c Connection) RunHealthCheck(nameOrID string) (*libpod.HealthCheckStatus, error) {
+	var (
+		status libpod.HealthCheckStatus
+	)
+	response, err := http.Get(c.makeEndpoint(fmt.Sprintf("/containers/%s/runhealthcheck", nameOrID)))
+	if err != nil {
+		return nil, err
+	}
+	data, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(data, &status)
+	return &status, err
+}
