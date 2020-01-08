583ff5320f879ad714d3482ed0ae23c8eecfdf05
Valentin Rothberg <rothberg@redhat.com>
2020-01-08 14:03:37 +0100

    fix lint - ignore image.ImageDeleteResponse definition
    Golint claims that image.Image stutters but renaming the type would be a
    breaking change which isn't worth the consequences.
    
    Signed-off-by: Valentin Rothberg <rothberg@redhat.com>

--- libpod/image/config.go
+++ libpod/image/config.go
@@ -2,7 +2,7 @@ package image
 
 // ImageDeleteResponse is the response for removing an image from storage and containers
 // what was untagged vs actually removed
-type ImageDeleteResponse struct {
+type ImageDeleteResponse struct { //nolint
 	Untagged []string `json:"untagged"`
 	Deleted  string   `json:"deleted"`
 }
