containers/podman

commit a824186ac9803ef5f7548df790988a4ebd2d9c07

Use buildah commit and bud in podman
Vendor in buildah and use as much of commit and bug as possible for podman
build and commit.

Resolves #586
Signed-off-by: baude <bbaude@redhat.com>

Closes: #681
Approved by: mheon

Makefile
@@ -29,6 +29,7 @@ COMMIT_NO := $(shell git rev-parse HEAD 2> /dev/null || true)
 GIT_COMMIT := $(if $(shell git status --porcelain --untracked-files=no),"${COMMIT_NO}-dirty","${COMMIT_NO}")
 BUILD_INFO := $(shell date +%s)
 ISODATE := $(shell date --iso-8601)
+LIBSECCOMP_COMMIT := release-2.3
 
 # If GOPATH not specified, use one in the local directory
 ifeq ($(GOPATH),)
@@ -249,6 +250,13 @@ install.tools: .install.gitvalidation .install.gometalinter .install.md2man
 
 varlink_generate: .gopathok cmd/podman/varlink/ioprojectatomicpodman.go
 
+.PHONY: install.libseccomp
+install.libseccomp:
+	rm -rf ../../seccomp/libseccomp
+	git clone https://github.com/seccomp/libseccomp ../../seccomp/libseccomp
+	cd ../../seccomp/libseccomp && git checkout $(LIBSECCOMP_COMMIT) && ./autogen.sh && ./configure --prefix=/usr && make all && make install
+
+
 cmd/podman/varlink/ioprojectatomicpodman.go: cmd/podman/varlink/io.projectatomic.podman.varlink
 	$(GO) generate ./cmd/podman/varlink/...
 
@@ -267,4 +275,5 @@ validate: gofmt .gitvalidation
 	uninstall \
 	shell \
 	changelog \
-	validate
+	validate \
+	install.libseccomp

commit eb0f79c3373d41356afd9a6603eeda4794750b1e

Fix Makefile ref libseccomp branch as a commit
Commit a824186ac9 mistakenly updated LIBSECCOMP_COMMIT with a branch
name instead of a commit reference.  This breaks on Ubuntu's git with
the `--detach` option, causing it to throw this error:

```
fatal: '--detach' cannot be used with '-b/-B/--orphan'
```

Fix this by using the tag `v2.3.3` which at the time of this commit, is
the current HEAD of the release-2.3 branch.

Thanks to Ed Santiago <santiago@redhat.com> for helping figure
out the error and the fix.

Signed-off-by: Chris Evich <cevich@redhat.com>

Makefile
@@ -81,7 +81,7 @@ LDFLAGS_PODMAN ?= \
 	  -X $(LIBPOD)/config._etcDir=$(ETCDIR) \
 	  -extldflags "$(LDFLAGS)"
 #Update to LIBSECCOMP_COMMIT should reflect in Dockerfile too.
-LIBSECCOMP_COMMIT := release-2.3
+LIBSECCOMP_COMMIT := v2.3.3
 # Rarely if ever should integration tests take more than 50min,
 # caller may override in special circumstances if needed.
 GINKGOTIMEOUT ?= -timeout=90m