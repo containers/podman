d4ca3197b62f6411827eb164edc4e8111aa9733f
Valentin Rothberg <rothberg@redhat.com>
2020-01-16 10:52:12 +0100

    hack/install_golangci.sh: check env vars
    Also make it executable and `set -eo pipefail`.
    
    Signed-off-by: Valentin Rothberg <rothberg@redhat.com>

--- hack/install_golangci.sh
+++ hack/install_golangci.sh
@@ -1,5 +1,17 @@
-#!/bin/bash -e
+#!/bin/bash
+
+if [ -z "$VERSION" ]; then
+	echo \$VERSION is empty
+	exit 1
+fi
+
+if [ -z "$GOBIN" ]; then
+	echo \$GOBIN is empty
+	exit 1
+fi
+
 $GOBIN/golangci-lint --version | grep $VERSION
 if [ $?  -ne 0 ]; then
+	set -e
 	curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sh -s -- -b $GOBIN v$VERSION
 fi
