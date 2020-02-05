02f214ce8200dd1e6a1b4a21762a7d4a422802af
Valentin Rothberg <rothberg@redhat.com>
2020-02-05 14:45:06 +0100

    [v1.4.2-stable] update containers/image
    Note that this includes fixes for
    https://access.redhat.com/security/cve/CVE-2020-1702.
    
    Signed-off-by: Valentin Rothberg <rothberg@redhat.com>

--- vendor/github.com/containers/image/docker/tarfile/src.go
+++ vendor/github.com/containers/image/docker/tarfile/src.go
@@ -12,6 +12,7 @@ import (
 	"sync"
 
 	"github.com/containers/image/internal/tmpdir"
+	"github.com/containers/image/internal/iolimits"
 	"github.com/containers/image/manifest"
 	"github.com/containers/image/pkg/compression"
 	"github.com/containers/image/types"
@@ -187,13 +188,13 @@ func findTarComponent(inputFile io.Reader, path string) (*tar.Reader, *tar.Heade
 }
 
 // readTarComponent returns full contents of componentPath.
-func (s *Source) readTarComponent(path string) ([]byte, error) {
+func (s *Source) readTarComponent(path string, limit int) ([]byte, error) {
 	file, err := s.openTarComponent(path)
 	if err != nil {
 		return nil, errors.Wrapf(err, "Error loading tar component %s", path)
 	}
 	defer file.Close()
-	bytes, err := ioutil.ReadAll(file)
+	bytes, err := iolimits.ReadAtMost(file, limit)
 	if err != nil {
 		return nil, err
 	}
@@ -217,7 +218,7 @@ func (s *Source) ensureCachedDataIsPresent() error {
 		}
 
 		// Read and parse config.
-		configBytes, err := s.readTarComponent(tarManifest[0].Config)
+		configBytes, err := s.readTarComponent(tarManifest[0].Config, iolimits.MaxConfigBodySize)
 		if err != nil {
 			s.cacheDataResult = err
 			return
@@ -247,7 +248,7 @@ func (s *Source) ensureCachedDataIsPresent() error {
 // loadTarManifest loads and decodes the manifest.json.
 func (s *Source) loadTarManifest() ([]ManifestItem, error) {
 	// FIXME? Do we need to deal with the legacy format?
-	bytes, err := s.readTarComponent(manifestFileName)
+	bytes, err := s.readTarComponent(manifestFileName, iolimits.MaxTarFileManifestSize)
 	if err != nil {
 		return nil, err
 	}
