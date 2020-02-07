f611fd11ceea57415b3c51dea753c9b2c7420fde
Miloslav Trmač <mitr@redhat.com>
2020-02-07 09:06:15 +0100

    BEHAVIOR CHANGE: Don't ignore error sending local image over Varlink
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- pkg/adapter/runtime_remote.go
+++ pkg/adapter/runtime_remote.go
@@ -867,7 +867,7 @@ func (r *LocalRuntime) LoadImage(ctx context.Context, name string, cli *cliconfi
 	var names string
 	remoteTempFile, err := r.SendFileOverVarlink(cli.Input)
 	if err != nil {
-		return "", nil
+		return "", err
 	}
 	more := varlink.More
 	if cli.Quiet {
