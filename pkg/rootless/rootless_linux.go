containers/podman

commit e9dc2120925d9bc32b87ed3c4122aa40f7413db5

rootless: check if the conmon process is valid
if the pause process doesn't exist and  we try to join a conmon
namespace, make sure the process still exists.  Otherwise re-create
the user namespace.

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

pkg/rootless/rootless_linux.go
@@ -452,6 +452,7 @@ func TryJoinFromFilePaths(pausePidPath string, needNewNamespace bool, paths []st
 
 	var lastErr error
 	var pausePid int
+	foundProcess := false
 
 	for _, path := range paths {
 		if !needNewNamespace {
@@ -502,11 +503,15 @@ func TryJoinFromFilePaths(pausePidPath string, needNewNamespace bool, paths []st
 			}
 
 			pausePid, err = strconv.Atoi(string(b[:n]))
-			if err == nil {
+			if err == nil && unix.Kill(pausePid, 0) == nil {
+				foundProcess = true
 				lastErr = nil
 				break
 			}
 		}
+	}
+	if !foundProcess {
+		return BecomeRootInUserNS(pausePidPath)
 	}
 	if lastErr != nil {
 		return false, 0, lastErr

commit 6dbb89e56c476d41ef63ad828ebdc549ce276c85

rootless: become root only if the pause file is specified
we need to store the pause process PID file so that it can be re-used
later.

commit e9dc2120925d9bc32b87ed3c4122aa40f7413db5 introduced this
regression.

Closes: https://github.com/containers/libpod/issues/5246

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

pkg/rootless/rootless_linux.go
@@ -510,7 +510,7 @@ func TryJoinFromFilePaths(pausePidPath string, needNewNamespace bool, paths []st
 			}
 		}
 	}
-	if !foundProcess {
+	if !foundProcess && pausePidPath != "" {
 		return BecomeRootInUserNS(pausePidPath)
 	}
 	if lastErr != nil {