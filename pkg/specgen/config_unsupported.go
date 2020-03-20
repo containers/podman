036531c0064df9b21414fbad4056d90edc54533b
Miloslav Trmač <mitr@redhat.com>
2020-03-20 23:25:15 +0100

    Fix the pkg/specgen/SpecGenerator.getSeccompConfig stub
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- pkg/specgen/config_unsupported.go
+++ pkg/specgen/config_unsupported.go
@@ -3,10 +3,11 @@
 package specgen
 
 import (
+	"github.com/containers/libpod/libpod/image"
 	spec "github.com/opencontainers/runtime-spec/specs-go"
 	"github.com/pkg/errors"
 )
 
-func (s *SpecGenerator) getSeccompConfig(configSpec *spec.Spec) (*spec.LinuxSeccomp, error) {
+func (s *SpecGenerator) getSeccompConfig(configSpec *spec.Spec, img *image.Image) (*spec.LinuxSeccomp, error) {
 	return nil, errors.New("function not supported on non-linux OS's")
 }
