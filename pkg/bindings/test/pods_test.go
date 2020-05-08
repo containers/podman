containers/podman

commit a2ad5bb9e1541707af39a19ec18997f6f0d77a0e

Add functions to return image informations
Signed-off-by: Daniel J Walsh <dwalsh@redhat.com>

pkg/specgen/generate/container.go
@@ -20,17 +20,27 @@ func CompleteSpec(ctx context.Context, r *libpod.Runtime, s *specgen.SpecGenerat
 	}
 
 	// Image stop signal
-	if s.StopSignal == nil && newImage.Config != nil {
-		sig, err := signal.ParseSignalNameOrNumber(newImage.Config.StopSignal)
+	if s.StopSignal == nil {
+		stopSignal, err := newImage.StopSignal(ctx)
+		if err != nil {
+			return err
+		}
+		sig, err := signal.ParseSignalNameOrNumber(stopSignal)
 		if err != nil {
 			return err
 		}
 		s.StopSignal = &sig
 	}
+
 	// Image envs from the image if they don't exist
 	// already
-	if newImage.Config != nil && len(newImage.Config.Env) > 0 {
-		envs, err := envLib.ParseSlice(newImage.Config.Env)
+	env, err := newImage.Env(ctx)
+	if err != nil {
+		return err
+	}
+
+	if len(env) > 0 {
+		envs, err := envLib.ParseSlice(env)
 		if err != nil {
 			return err
 		}
@@ -41,12 +51,15 @@ func CompleteSpec(ctx context.Context, r *libpod.Runtime, s *specgen.SpecGenerat
 		}
 	}
 
+	labels, err := newImage.Labels(ctx)
+	if err != nil {
+		return err
+	}
+
 	// labels from the image that dont exist already
-	if config := newImage.Config; config != nil {
-		for k, v := range config.Labels {
-			if _, exists := s.Labels[k]; !exists {
-				s.Labels[k] = v
-			}
+	for k, v := range labels {
+		if _, exists := s.Labels[k]; !exists {
+			s.Labels[k] = v
 		}
 	}
 
@@ -75,20 +88,30 @@ func CompleteSpec(ctx context.Context, r *libpod.Runtime, s *specgen.SpecGenerat
 	}
 
 	// entrypoint
-	if config := newImage.Config; config != nil {
-		if len(s.Entrypoint) < 1 && len(config.Entrypoint) > 0 {
-			s.Entrypoint = config.Entrypoint
-		}
-		if len(s.Command) < 1 && len(config.Cmd) > 0 {
-			s.Command = config.Cmd
-		}
-		if len(s.Command) < 1 && len(s.Entrypoint) < 1 {
-			return errors.Errorf("No command provided or as CMD or ENTRYPOINT in this image")
-		}
-		// workdir
-		if len(s.WorkDir) < 1 && len(config.WorkingDir) > 1 {
-			s.WorkDir = config.WorkingDir
-		}
+	entrypoint, err := newImage.Entrypoint(ctx)
+	if err != nil {
+		return err
+	}
+	if len(s.Entrypoint) < 1 && len(entrypoint) > 0 {
+		s.Entrypoint = entrypoint
+	}
+	command, err := newImage.Cmd(ctx)
+	if err != nil {
+		return err
+	}
+	if len(s.Command) < 1 && len(command) > 0 {
+		s.Command = command
+	}
+	if len(s.Command) < 1 && len(s.Entrypoint) < 1 {
+		return errors.Errorf("No command provided or as CMD or ENTRYPOINT in this image")
+	}
+	// workdir
+	workingDir, err := newImage.WorkingDir(ctx)
+	if err != nil {
+		return err
+	}
+	if len(s.WorkDir) < 1 && len(workingDir) > 1 {
+		s.WorkDir = workingDir
 	}
 
 	if len(s.SeccompProfilePath) < 1 {
@@ -99,15 +122,17 @@ func CompleteSpec(ctx context.Context, r *libpod.Runtime, s *specgen.SpecGenerat
 		s.SeccompProfilePath = p
 	}
 
-	if user := s.User; len(user) == 0 {
-		switch {
+	if len(s.User) == 0 {
+		s.User, err = newImage.User(ctx)
+		if err != nil {
+			return err
+		}
+
 		// TODO This should be enabled when namespaces actually work
 		//case usernsMode.IsKeepID():
 		//	user = fmt.Sprintf("%d:%d", rootless.GetRootlessUID(), rootless.GetRootlessGID())
-		case newImage.Config == nil || (newImage.Config != nil && len(newImage.Config.User) == 0):
+		if len(s.User) == 0 {
 			s.User = "0"
-		default:
-			s.User = newImage.Config.User
 		}
 	}
 	if err := finishThrottleDevices(s); err != nil {

libpod/image/image.go
@@ -867,22 +867,77 @@ func (i *Image) Intermediate(ctx context.Context) (bool, error) {
 	return false, nil
 }
 
+// User returns the image's user
+func (i *Image) User(ctx context.Context) (string, error) {
+	imgInspect, err := i.inspect(ctx, false)
+	if err != nil {
+		return "", err
+	}
+	return imgInspect.Config.User, nil
+}
+
+// StopSignal returns the image's StopSignal
+func (i *Image) StopSignal(ctx context.Context) (string, error) {
+	imgInspect, err := i.inspect(ctx, false)
+	if err != nil {
+		return "", err
+	}
+	return imgInspect.Config.StopSignal, nil
+}
+
+// WorkingDir returns the image's WorkingDir
+func (i *Image) WorkingDir(ctx context.Context) (string, error) {
+	imgInspect, err := i.inspect(ctx, false)
+	if err != nil {
+		return "", err
+	}
+	return imgInspect.Config.WorkingDir, nil
+}
+
+// Cmd returns the image's cmd
+func (i *Image) Cmd(ctx context.Context) ([]string, error) {
+	imgInspect, err := i.inspect(ctx, false)
+	if err != nil {
+		return nil, err
+	}
+	return imgInspect.Config.Cmd, nil
+}
+
+// Entrypoint returns the image's entrypoint
+func (i *Image) Entrypoint(ctx context.Context) ([]string, error) {
+	imgInspect, err := i.inspect(ctx, false)
+	if err != nil {
+		return nil, err
+	}
+	return imgInspect.Config.Entrypoint, nil
+}
+
+// Env returns the image's env
+func (i *Image) Env(ctx context.Context) ([]string, error) {
+	imgInspect, err := i.imageInspectInfo(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return imgInspect.Env, nil
+}
+
 // Labels returns the image's labels
 func (i *Image) Labels(ctx context.Context) (map[string]string, error) {
 	imgInspect, err := i.imageInspectInfo(ctx)
 	if err != nil {
-		return nil, nil
+		return nil, err
 	}
 	return imgInspect.Labels, nil
 }
 
 // GetLabel Returns a case-insensitive match of a given label
 func (i *Image) GetLabel(ctx context.Context, label string) (string, error) {
-	imageLabels, err := i.Labels(ctx)
+	labels, err := i.Labels(ctx)
 	if err != nil {
 		return "", err
 	}
-	for k, v := range imageLabels {
+
+	for k, v := range labels {
 		if strings.ToLower(k) == strings.ToLower(label) {
 			return v, nil
 		}

commit a4c607cc71ea5c3a4d4898a84b19cdd236ae90bd

set binding tests to required
some small fix ups for binding tests and then make them required.

update containers-common

V2 bindings tests were failing because of changes introduced in commit
a2ad5bb.

Fix some typos.

Signed-off-by: Lokesh Mandvekar <lsm5@fedoraproject.org>

in the case where the specgen attribute for Env and Labels are nil, we should should then make the map IF we have labels and envs that need to be added.

Signed-off-by: Brent Baude <bbaude@redhat.com>

.cirrus.yml
@@ -644,7 +644,6 @@ special_testing_cross_task:
 
 special_testing_bindings_task:
 
-    skip: $CI == 'true'
     depends_on:
         - "gating"
         - "varlink_api"

cmd/podman/manifest/annotate.go
@@ -24,7 +24,7 @@ var (
 
 func init() {
 	registry.Commands = append(registry.Commands, registry.CliCommand{
-		Mode:    []entities.EngineMode{entities.ABIMode, entities.TunnelMode},
+		Mode:    []entities.EngineMode{entities.ABIMode},
 		Command: annotateCmd,
 		Parent:  manifestCmd,
 	})

contrib/cirrus/setup_environment.sh
@@ -87,7 +87,7 @@ case "$CG_FS_TYPE" in
             # Normally not something to do for stable testing
             # but crun is new, and late-breaking fixes may be required
             # on short notice
-            dnf update -y crun
+            dnf update -y crun containers-common
         fi
         ;;
     *)

pkg/bindings/manifests/manifests.go
@@ -125,23 +125,24 @@ func Push(ctx context.Context, name string, destination *string, all *bool) (str
 	return idr.ID, response.Process(&idr)
 }
 
+// There is NO annotate endpoint.  this binding could never work
 // Annotate updates the image configuration of a given manifest list
-func Annotate(ctx context.Context, name, digest string, options image.ManifestAnnotateOpts) (string, error) {
-	var idr handlers.IDResponse
-	conn, err := bindings.GetClient(ctx)
-	if err != nil {
-		return "", err
-	}
-	params := url.Values{}
-	params.Set("digest", digest)
-	optionsString, err := jsoniter.MarshalToString(options)
-	if err != nil {
-		return "", err
-	}
-	stringReader := strings.NewReader(optionsString)
-	response, err := conn.DoRequest(stringReader, http.MethodPost, "/manifests/%s/annotate", params, name)
-	if err != nil {
-		return "", err
-	}
-	return idr.ID, response.Process(&idr)
-}
+//func Annotate(ctx context.Context, name, digest string, options image.ManifestAnnotateOpts) (string, error) {
+//	var idr handlers.IDResponse
+//	conn, err := bindings.GetClient(ctx)
+//	if err != nil {
+//		return "", err
+//	}
+//	params := url.Values{}
+//	params.Set("digest", digest)
+//	optionsString, err := jsoniter.MarshalToString(options)
+//	if err != nil {
+//		return "", err
+//	}
+//	stringReader := strings.NewReader(optionsString)
+//	response, err := conn.DoRequest(stringReader, http.MethodPost, "/manifests/%s/annotate", params, name)
+//	if err != nil {
+//		return "", err
+//	}
+//	return idr.ID, response.Process(&idr)
+//}

pkg/domain/infra/tunnel/manifest.go
@@ -64,32 +64,34 @@ func (ir *ImageEngine) ManifestAdd(ctx context.Context, opts entities.ManifestAd
 	return listID, nil
 }
 
+// FIXME There is no endpoint for annotate and therefor this code is currently invalid
 // ManifestAnnotate updates an entry of the manifest list
 func (ir *ImageEngine) ManifestAnnotate(ctx context.Context, names []string, opts entities.ManifestAnnotateOptions) (string, error) {
-	manifestAnnotateOpts := image.ManifestAnnotateOpts{
-		Arch:       opts.Arch,
-		Features:   opts.Features,
-		OS:         opts.OS,
-		OSFeatures: opts.OSFeatures,
-		OSVersion:  opts.OSVersion,
-		Variant:    opts.Variant,
-	}
-	if len(opts.Annotation) > 0 {
-		annotations := make(map[string]string)
-		for _, annotationSpec := range opts.Annotation {
-			spec := strings.SplitN(annotationSpec, "=", 2)
-			if len(spec) != 2 {
-				return "", errors.Errorf("no value given for annotation %q", spec[0])
-			}
-			annotations[spec[0]] = spec[1]
-		}
-		manifestAnnotateOpts.Annotation = annotations
-	}
-	updatedListID, err := manifests.Annotate(ctx, names[0], names[1], manifestAnnotateOpts)
-	if err != nil {
-		return updatedListID, errors.Wrapf(err, "error annotating %s of manifest list %s", names[1], names[0])
-	}
-	return fmt.Sprintf("%s :%s", updatedListID, names[1]), nil
+	return "", errors.New("not implemented")
+	//	manifestAnnotateOpts := image.ManifestAnnotateOpts{
+	//		Arch:       opts.Arch,
+	//		Features:   opts.Features,
+	//		OS:         opts.OS,
+	//		OSFeatures: opts.OSFeatures,
+	//		OSVersion:  opts.OSVersion,
+	//		Variant:    opts.Variant,
+	//	}
+	//	if len(opts.Annotation) > 0 {
+	//		annotations := make(map[string]string)
+	//		for _, annotationSpec := range opts.Annotation {
+	//			spec := strings.SplitN(annotationSpec, "=", 2)
+	//			if len(spec) != 2 {
+	//				return "", errors.Errorf("no value given for annotation %q", spec[0])
+	//			}
+	//			annotations[spec[0]] = spec[1]
+	//		}
+	//		manifestAnnotateOpts.Annotation = annotations
+	//	}
+	//	updatedListID, err := manifests.Annotate(ctx, names[0], names[1], manifestAnnotateOpts)
+	//	if err != nil {
+	//		return updatedListID, errors.Wrapf(err, "error annotating %s of manifest list %s", names[1], names[0])
+	//	}
+	//	return fmt.Sprintf("%s :%s", updatedListID, names[1]), nil
 }
 
 // ManifestRemove removes the digest from manifest list

pkg/specgen/generate/container.go
@@ -42,11 +42,13 @@ func CompleteSpec(ctx context.Context, r *libpod.Runtime, s *specgen.SpecGenerat
 		if err != nil {
 			return err
 		}
-		sig, err := signal.ParseSignalNameOrNumber(stopSignal)
-		if err != nil {
-			return err
+		if stopSignal != "" {
+			sig, err := signal.ParseSignalNameOrNumber(stopSignal)
+			if err != nil {
+				return err
+			}
+			s.StopSignal = &sig
 		}
-		s.StopSignal = &sig
 	}
 
 	rtc, err := r.GetConfig()
@@ -78,6 +80,9 @@ func CompleteSpec(ctx context.Context, r *libpod.Runtime, s *specgen.SpecGenerat
 	}
 
 	// labels from the image that dont exist already
+	if len(labels) > 0 && s.Labels == nil {
+		s.Labels = make(map[string]string)
+	}
 	for k, v := range labels {
 		if _, exists := s.Labels[k]; !exists {
 			s.Labels[k] = v

pkg/bindings/test/containers_test.go
@@ -531,7 +531,7 @@ var _ = Describe("Podman containers ", func() {
 		Expect(err).ToNot(BeNil())
 	})
 
-	It("podman prune stoped containers", func() {
+	It("podman prune stopped containers", func() {
 		// Start and stop a container to enter in exited state.
 		var name = "top"
 		_, err := bt.RunTopContainer(&name, &bindings.PFalse, nil)
@@ -546,7 +546,7 @@ var _ = Describe("Podman containers ", func() {
 		Expect(len(pruneResponse.ID)).To(Equal(1))
 	})
 
-	It("podman prune stoped containers with filters", func() {
+	It("podman prune stopped containers with filters", func() {
 		// Start and stop a container to enter in exited state.
 		var name = "top"
 		_, err := bt.RunTopContainer(&name, &bindings.PFalse, nil)

pkg/bindings/test/manifests_test.go
@@ -118,25 +118,27 @@ var _ = Describe("Podman containers ", func() {
 		Expect(len(data.Manifests)).To(BeZero())
 	})
 
-	It("annotate manifest", func() {
-		id, err := manifests.Create(bt.conn, []string{"quay.io/libpod/foobar:latest"}, []string{}, nil)
-		Expect(err).To(BeNil())
-		opts := image.ManifestAddOpts{Images: []string{"docker.io/library/alpine:latest"}}
+	// There is NO annotate endpoint, this could never work.:w
 
-		_, err = manifests.Add(bt.conn, id, opts)
-		Expect(err).To(BeNil())
-		data, err := manifests.Inspect(bt.conn, id)
-		Expect(err).To(BeNil())
-		Expect(len(data.Manifests)).To(BeNumerically("==", 1))
-		digest := data.Manifests[0].Digest.String()
-		annoOpts := image.ManifestAnnotateOpts{OS: "foo"}
-		_, err = manifests.Annotate(bt.conn, id, digest, annoOpts)
-		Expect(err).To(BeNil())
-		list, err := manifests.Inspect(bt.conn, id)
-		Expect(err).To(BeNil())
-		Expect(len(list.Manifests)).To(BeNumerically("==", 1))
-		Expect(list.Manifests[0].Platform.OS).To(Equal("foo"))
-	})
+	//It("annotate manifest", func() {
+	//	id, err := manifests.Create(bt.conn, []string{"quay.io/libpod/foobar:latest"}, []string{}, nil)
+	//	Expect(err).To(BeNil())
+	//	opts := image.ManifestAddOpts{Images: []string{"docker.io/library/alpine:latest"}}
+	//
+	//	_, err = manifests.Add(bt.conn, id, opts)
+	//	Expect(err).To(BeNil())
+	//	data, err := manifests.Inspect(bt.conn, id)
+	//	Expect(err).To(BeNil())
+	//	Expect(len(data.Manifests)).To(BeNumerically("==", 1))
+	//	digest := data.Manifests[0].Digest.String()
+	//	annoOpts := image.ManifestAnnotateOpts{OS: "foo"}
+	//	_, err = manifests.Annotate(bt.conn, id, digest, annoOpts)
+	//	Expect(err).To(BeNil())
+	//	list, err := manifests.Inspect(bt.conn, id)
+	//	Expect(err).To(BeNil())
+	//	Expect(len(list.Manifests)).To(BeNumerically("==", 1))
+	//	Expect(list.Manifests[0].Platform.OS).To(Equal("foo"))
+	//})
 
 	It("push manifest", func() {
 		Skip("TODO")

pkg/bindings/test/pods_test.go
@@ -57,6 +57,11 @@ var _ = Describe("Podman pods", func() {
 		podSummary, err := pods.List(bt.conn, nil)
 		Expect(err).To(BeNil())
 		Expect(len(podSummary)).To(Equal(1))
+
+		// Start the pod
+		_, err = pods.Start(bt.conn, newpod)
+		Expect(err).To(BeNil())
+
 		// Adding an alpine container to the existing pod
 		_, err = bt.RunTopContainer(nil, &bindings.PTrue, &newpod)
 		Expect(err).To(BeNil())
@@ -83,6 +88,11 @@ var _ = Describe("Podman pods", func() {
 	It("List pods with filters", func() {
 		newpod2 := "newpod2"
 		bt.Podcreate(&newpod2)
+
+		// Start the pod
+		_, err = pods.Start(bt.conn, newpod)
+		Expect(err).To(BeNil())
+
 		_, err = bt.RunTopContainer(nil, &bindings.PTrue, &newpod)
 		Expect(err).To(BeNil())
 

pkg/bindings/test/system_test.go
@@ -3,7 +3,6 @@ package test_bindings
 import (
 	"time"
 
-	"github.com/containers/libpod/pkg/api/handlers"
 	"github.com/containers/libpod/pkg/bindings"
 	"github.com/containers/libpod/pkg/bindings/containers"
 	"github.com/containers/libpod/pkg/bindings/pods"
@@ -39,8 +38,8 @@ var _ = Describe("Podman system", func() {
 	})
 
 	It("podman events", func() {
-		eChan := make(chan handlers.Event, 1)
-		var messages []handlers.Event
+		eChan := make(chan entities.Event, 1)
+		var messages []entities.Event
 		cancelChan := make(chan bool, 1)
 		go func() {
 			for e := range eChan {