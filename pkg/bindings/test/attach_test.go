d34e5a142a57a8341fdc928b9f653392a27800e9
Jhon Honce <jhonce@redhat.com>
2020-05-13 14:53:53 -0700

    V2 Update attach bindings to use Readers/Writers vs chan
    * Change function call to use readers/writers in place channels
    * Support stdin for pushing data from client to container
    * Add bindings test
    
    Signed-off-by: Jhon Honce <jhonce@redhat.com>

--- pkg/bindings/test/attach_test.go
+++ pkg/bindings/test/attach_test.go
@@ -2,10 +2,13 @@ package test_bindings
 
 import (
 	"bytes"
+	"fmt"
 	"time"
 
+	"github.com/containers/libpod/libpod/define"
 	"github.com/containers/libpod/pkg/bindings"
 	"github.com/containers/libpod/pkg/bindings/containers"
+	"github.com/containers/libpod/pkg/specgen"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gexec"
@@ -31,7 +34,7 @@ var _ = Describe("Podman containers attach", func() {
 		bt.cleanup()
 	})
 
-	It("attach", func() {
+	It("can run top in container", func() {
 		name := "TopAttachTest"
 		id, err := bt.RunTopContainer(&name, nil, nil)
 		Expect(err).ShouldNot(HaveOccurred())
@@ -51,13 +54,56 @@ var _ = Describe("Podman containers attach", func() {
 		go func() {
 			defer GinkgoRecover()
 
-			err := containers.Attach(bt.conn, id, nil, &bindings.PTrue, &bindings.PTrue, &bindings.PTrue, stdout, stderr)
+			err := containers.Attach(bt.conn, id, nil, &bindings.PTrue, &bindings.PTrue, nil, stdout, stderr)
 			Expect(err).ShouldNot(HaveOccurred())
 		}()
 
 		time.Sleep(5 * time.Second)
-
 		// First character/First line of top output
 		Expect(stdout.String()).Should(ContainSubstring("Mem: "))
+	})
+
+	It("can echo data via cat in container", func() {
+		s := specgen.NewSpecGenerator(alpine.name, false)
+		s.Name = "CatAttachTest"
+		s.Terminal = true
+		s.Command = []string{"/bin/cat"}
+		ctnr, err := containers.CreateWithSpec(bt.conn, s)
+		Expect(err).ShouldNot(HaveOccurred())
+
+		err = containers.Start(bt.conn, ctnr.ID, nil)
+		Expect(err).ShouldNot(HaveOccurred())
+
+		wait := define.ContainerStateRunning
+		_, err = containers.Wait(bt.conn, ctnr.ID, &wait)
+		Expect(err).ShouldNot(HaveOccurred())
+
+		tickTock := time.NewTimer(2 * time.Second)
+		go func() {
+			<-tickTock.C
+			timeout := uint(5)
+			err := containers.Stop(bt.conn, ctnr.ID, &timeout)
+			if err != nil {
+				GinkgoWriter.Write([]byte(err.Error()))
+			}
+		}()
+
+		msg := "Hello, World"
+		stdin := &bytes.Buffer{}
+		stdin.WriteString(msg + "\n")
+
+		stdout := &bytes.Buffer{}
+		stderr := &bytes.Buffer{}
+		go func() {
+			defer GinkgoRecover()
+
+			err := containers.Attach(bt.conn, ctnr.ID, nil, &bindings.PFalse, &bindings.PTrue, stdin, stdout, stderr)
+			Expect(err).ShouldNot(HaveOccurred())
+		}()
+
+		time.Sleep(5 * time.Second)
+		// Tty==true so we get echo'ed stdin + expected output
+		Expect(stdout.String()).Should(Equal(fmt.Sprintf("%[1]s\r\n%[1]s\r\n", msg)))
+		Expect(stderr.String()).Should(BeEmpty())
 	})
 })
