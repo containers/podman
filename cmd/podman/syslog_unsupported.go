66cae3209ec362cc6fb663dec742cf0514aa9af8
Matthew Heon <matthew.heon@pm.me>
2020-05-20 18:19:06 -0400

    Fix build on OS X
    We disabled the OS X and Windows cross-building tests. This,
    predictably, led us to regress a bit in our ability to build for
    both of these.
    
    This fixes the build on OS X and fixes one obvious Windows bug.
    Unfortunately, we're dragging in all of `pkg/spec` somewhere on
    Windows, and things are blowing up spectacularly because of it
    (plus a few uses of the `syscall` package in the bindings).
    
    I've giving up for the day. This fixes OS X, but does not fully
    enable the cross-build CI (need Windows fixes for that).
    
    Signed-off-by: Matthew Heon <matthew.heon@pm.me>

---
+++ cmd/podman/syslog_unsupported.go
@@ -0,0 +1,17 @@
+// +build !linux
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func syslogHook() {
+	if !useSyslog {
+		return
+	}
+
+	fmt.Fprintf(os.Stderr, "Logging to Syslog is not supported on Windows")
+	os.Exit(1)
+}
