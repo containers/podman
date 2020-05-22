09b430a55844735fb934375df4f3386f2e1ac8d3
Daniel J Walsh <dwalsh@redhat.com>
2020-05-22 05:57:31 -0400

    Attempt to turn on build_without_cgo tests
    Signed-off-by: Daniel J Walsh <dwalsh@redhat.com>

--- pkg/specgen/generate/config_linux_nocgo.go
+++ pkg/specgen/generate/config_linux_nocgo.go
@@ -5,10 +5,11 @@ package generate
 import (
 	"errors"
 
+	"github.com/containers/libpod/libpod/image"
 	"github.com/containers/libpod/pkg/specgen"
 	spec "github.com/opencontainers/runtime-spec/specs-go"
 )
 
-func (s *specgen.SpecGenerator) getSeccompConfig(configSpec *spec.Spec) (*spec.LinuxSeccomp, error) {
+func getSeccompConfig(s *specgen.SpecGenerator, configSpec *spec.Spec, img *image.Image) (*spec.LinuxSeccomp, error) {
 	return nil, errors.New("not implemented")
 }
