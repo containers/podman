d7770df5ef9f6085a5e31e3260f7dc1f0056f162
Jhon Honce <jhonce@redhat.com>
2020-06-24 12:40:55 -0700

    Fix python dockerpy tests
    * Refactor packaging so unittest discovery works
    * Refactor tests to use python3-docker.rpm that ships with Fedora32
    * Flush image cache between tests suites
    * Update documentation to reflect changes
    
    Outstanding issue:
    * client.get_image() does not fail if image does not exist
    
    Signed-off-by: Jhon Honce <jhonce@redhat.com>

--- test/python/dockerpy/tests/test_containers.py
+++ test/python/dockerpy/tests/test_containers.py
@@ -1,18 +1,15 @@
-
-import unittest
-import docker
-import requests
 import os
-from docker import Client
-from . import constant
-from . import common
 import time
+import unittest
+
+import requests
+
+from . import common, constant
 
 client = common.get_client()
 
-class TestContainers(unittest.TestCase):
 
-    podman = None
+class TestContainers(unittest.TestCase):
     topContainerId = ""
 
     def setUp(self):
@@ -33,6 +30,7 @@ class TestContainers(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         common.terminate_connection(cls)
+        common.flush_image_cache(cls)
         return super().tearDownClass()
 
     def test_inspect_container(self):
@@ -42,16 +40,15 @@ class TestContainers(unittest.TestCase):
         self.assertEqual(error.exception.response.status_code, 404)
         # Inspect valid container by name
         container = client.inspect_container(constant.TOP)
-        self.assertIn(TestContainers.topContainerId , container["Id"])
+        self.assertIn(TestContainers.topContainerId, container["Id"])
         # Inspect valid container by Id
         container = client.inspect_container(TestContainers.topContainerId)
-        self.assertIn(constant.TOP , container["Name"])
+        self.assertIn(constant.TOP, container["Name"])
 
     def test_create_container(self):
         # Run a container with detach mode
         container = client.create_container(image="alpine", detach=True)
-        self.assertEqual(len(container),2)
-
+        self.assertEqual(len(container), 2)
 
     def test_start_container(self):
         # Start bogus container
@@ -65,9 +62,9 @@ class TestContainers(unittest.TestCase):
         # self.assertEqual(error.exception.response.status_code, 304)
 
         # Create a new container and validate the count
-        client.create_container(image=constant.ALPINE,name="container2")
-        containers = client.containers(quiet=True,all=True)
-        self.assertEqual(len(containers),2)
+        client.create_container(image=constant.ALPINE, name="container2")
+        containers = client.containers(quiet=True, all=True)
+        self.assertEqual(len(containers), 2)
 
     def test_stop_container(self):
         # Stop bogus container
@@ -82,7 +79,10 @@ class TestContainers(unittest.TestCase):
         # Stop a running container and validate the state
         client.stop(TestContainers.topContainerId)
         container = client.inspect_container(constant.TOP)
-        self.assertIn(container["State"]["Status"],"stopped exited",)
+        self.assertIn(
+            container["State"]["Status"],
+            "stopped exited",
+        )
 
     def test_restart_container(self):
         # Restart bogus container
@@ -109,12 +109,12 @@ class TestContainers(unittest.TestCase):
         # Remove container by ID with force
         client.remove_container(TestContainers.topContainerId, force=True)
         containers = client.containers()
-        self.assertEqual(len(containers),0)
+        self.assertEqual(len(containers), 0)
 
     def test_remove_container_without_force(self):
         # Validate current container count
         containers = client.containers()
-        self.assertTrue(len(containers),1)
+        self.assertTrue(len(containers), 1)
 
         # Remove running container should throw error
         with self.assertRaises(requests.HTTPError) as error:
@@ -125,7 +125,7 @@ class TestContainers(unittest.TestCase):
         client.stop(TestContainers.topContainerId)
         client.remove_container(TestContainers.topContainerId)
         containers = client.containers()
-        self.assertEqual(len(containers),0)
+        self.assertEqual(len(containers), 0)
 
     def test_pause_container(self):
         # Pause bogus container
@@ -151,7 +151,6 @@ class TestContainers(unittest.TestCase):
             client.pause(TestContainers.topContainerId)
         self.assertEqual(error.exception.response.status_code, 500)
 
-
     def test_unpause_container(self):
         # Unpause bogus container
         with self.assertRaises(requests.HTTPError) as error:
@@ -173,7 +172,7 @@ class TestContainers(unittest.TestCase):
         # Add container and validate the count
         client.create_container(image="alpine", detach=True)
         containers = client.containers(all=True)
-        self.assertEqual(len(containers),2)
+        self.assertEqual(len(containers), 2)
 
         # Not working for now......checking
         # # List container with filter by id
--- test/python/dockerpy/tests/test_images.py
+++ test/python/dockerpy/tests/test_images.py
@@ -1,17 +1,18 @@
-
+import os
+import stat
 import unittest
+from os import remove
+from stat import ST_SIZE
+
 import docker
 import requests
-import os
-from docker import Client
-from . import constant
-from . import common
+
+from . import common, constant
 
 client = common.get_client()
 
-class TestImages(unittest.TestCase):
 
-    podman = None
+class TestImages(unittest.TestCase):
     def setUp(self):
         super().setUp()
         common.restore_image_from_cache(self)
@@ -25,12 +26,11 @@ class TestImages(unittest.TestCase):
         super().setUpClass()
         common.enable_sock(cls)
 
-
     @classmethod
     def tearDownClass(cls):
         common.terminate_connection(cls)
+        common.flush_image_cache(cls)
         return super().tearDownClass()
-
 
 # Inspect Image
 
@@ -43,46 +43,46 @@ class TestImages(unittest.TestCase):
 
 # Tag Image
 
-    # Validates if invalid image name is given a bad response is encountered.
+# Validates if invalid image name is given a bad response is encountered.
+
     def test_tag_invalid_image(self):
         with self.assertRaises(requests.HTTPError):
-            client.tag("dummy","demo")
-
-
+            client.tag("dummy", "demo")
 
     #  Validates if the image is tagged successfully.
     def test_tag_valid_image(self):
-        client.tag(constant.ALPINE,"demo",constant.ALPINE_SHORTNAME)
+        client.tag(constant.ALPINE, "demo", constant.ALPINE_SHORTNAME)
         alpine_image = client.inspect_image(constant.ALPINE)
         for x in alpine_image["RepoTags"]:
-            if("demo:alpine" in x):
+            if ("demo:alpine" in x):
                 self.assertTrue
         self.assertFalse
 
     # Validates if name updates when the image is retagged.
     @unittest.skip("dosent work now")
     def test_retag_valid_image(self):
-        client.tag(constant.ALPINE_SHORTNAME, "demo","rename")
+        client.tag(constant.ALPINE_SHORTNAME, "demo", "rename")
         alpine_image = client.inspect_image(constant.ALPINE)
         self.assertNotIn("demo:test", alpine_image["RepoTags"])
 
 # List Image
-    # List All Images
+# List All Images
+
     def test_list_images(self):
         allImages = client.images()
         self.assertEqual(len(allImages), 1)
         # Add more images
         client.pull(constant.BB)
         allImages = client.images()
-        self.assertEqual(len(allImages) , 2)
+        self.assertEqual(len(allImages), 2)
 
-
-    # List images with filter
-        filters = {'reference':'alpine'}
-        allImages = client.images(filters = filters)
-        self.assertEqual(len(allImages) , 1)
+        # List images with filter
+        filters = {'reference': 'alpine'}
+        allImages = client.images(filters=filters)
+        self.assertEqual(len(allImages), 1)
 
 # Search Image
+
     def test_search_image(self):
         response = client.search("alpine")
         for i in response:
@@ -94,22 +94,25 @@ class TestImages(unittest.TestCase):
 # Image Exist (No docker-py support yet)
 
 # Remove Image
+
     def test_remove_image(self):
         # Check for error with wrong image name
         with self.assertRaises(requests.HTTPError):
             client.remove_image("dummy")
         allImages = client.images()
-        self.assertEqual(len(allImages) , 1)
+        self.assertEqual(len(allImages), 1)
         alpine_image = client.inspect_image(constant.ALPINE)
         client.remove_image(alpine_image)
         allImages = client.images()
-        self.assertEqual(len(allImages) , 0)
+        self.assertEqual(len(allImages), 0)
 
 # Image History
+
     def test_image_history(self):
         # Check for error with wrong image name
         with self.assertRaises(requests.HTTPError):
-            client.remove_image("dummy")
+            client.history("dummy")
+
         imageHistory = client.history(constant.ALPINE)
         alpine_image = client.inspect_image(constant.ALPINE)
         for h in imageHistory:
@@ -119,28 +122,37 @@ class TestImages(unittest.TestCase):
 
 # Prune Image (No docker-py support yet)
 
+    def test_get_image_dummy(self):
+        # FIXME: seems to be an error in the library
+        self.skipTest("Documentation and library do not match")
+        # Check for error with wrong image name
+        with self.assertRaises(docker.errors.ImageNotFound):
+            client.get_image("dummy")
+
 # Export Image
 
     def test_export_image(self):
         client.pull(constant.BB)
-        file = os.path.join(constant.ImageCacheDir , "busybox.tar")
         if not os.path.exists(constant.ImageCacheDir):
             os.makedirs(constant.ImageCacheDir)
-        # Check for error with wrong image name
-        with self.assertRaises(requests.HTTPError):
-            client.get_image("dummy")
-        response = client.get_image(constant.BB)
-        image_tar = open(file,mode="wb")
-        image_tar.write(response.data)
-        image_tar.close()
-        os.stat(file)
+
+        image = client.get_image(constant.BB)
+
+        file = os.path.join(constant.ImageCacheDir, "busybox.tar")
+        tarball = open(file, mode="wb")
+        for frame in image:
+            tarball.write(frame)
+        tarball.close()
+        sz = os.path.getsize(file)
+        self.assertGreater(sz, 0)
+
 
 # Import|Load Image
 
     def test_import_image(self):
         allImages = client.images()
         self.assertEqual(len(allImages), 1)
-        file = os.path.join(constant.ImageCacheDir , "busybox.tar")
+        file = os.path.join(constant.ImageCacheDir, "alpine.tar")
         client.import_image_from_file(filename=file)
         allImages = client.images()
         self.assertEqual(len(allImages), 2)
