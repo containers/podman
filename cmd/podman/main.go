ee956b04b05eed25d3aec967d8c68e7d1418fa09
Jhon Honce <jhonce@redhat.com>
2020-07-21 10:36:44 -0700

    [WIP] Refactor podman system connection
    * Add support to manage multiple connections
      * Add connection
      * Remove connection
      * Rename connection
      * Set connection as default
      * Add markdown/man pages
    * Fix recursion in hack/xref-helpmsgs-manpages
    
    Signed-off-by: Jhon Honce <jhonce@redhat.com>
    
    <MH: Fixed build after rebase>
    
    Signed-off-by: Matt Heon <matthew.heon@pm.me>

--- cmd/podman/main.go
+++ cmd/podman/main.go
@@ -14,6 +14,7 @@ import (
 	_ "github.com/containers/libpod/v2/cmd/podman/pods"
 	"github.com/containers/libpod/v2/cmd/podman/registry"
 	_ "github.com/containers/libpod/v2/cmd/podman/system"
+	_ "github.com/containers/libpod/v2/cmd/podman/system/connection"
 	_ "github.com/containers/libpod/v2/cmd/podman/volumes"
 	"github.com/containers/libpod/v2/pkg/rootless"
 	"github.com/containers/libpod/v2/pkg/terminal"
