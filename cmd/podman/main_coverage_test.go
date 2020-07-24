a9961b70d1f2952aac8d32c722281ec7f75164dc
Valentin Rothberg <rothberg@redhat.com>
2020-07-24 13:06:49 +0200

    cmd/podman_coverage_test.go
    We need a dummy main_test.go file which specifies a test that does
    nothing.  Otherwise, `ginkgo` will force executing the a test even
    when there's a build condition (e.g., coverage).
    
    Now, the unit tests will pass again.
    
    Signed-off-by: Valentin Rothberg <rothberg@redhat.com>

---
+++ cmd/podman/main_coverage_test.go
@@ -0,0 +1,39 @@
+// +build coverage
+
+package main
+
+import (
+	"os"
+	"strings"
+	"testing"
+)
+
+// NOTE: do not use this in production.  Binaries built with this file are
+// merely useful to collect coverage data.
+func TestCoverageMain(_ *testing.T) {
+	var (
+		args []string
+	)
+
+	for _, arg := range os.Args {
+		switch {
+		case strings.HasPrefix(arg, "COVERAGE"):
+			// Dummy argument to enable global flags for Podman.
+		case strings.HasPrefix(arg, "-test"):
+			// Make sure we don't pass `go test` specific flags to
+			// Podman.
+		default:
+			args = append(args, arg)
+		}
+	}
+
+	os.Args = args
+	main() // "run" Podman
+
+	// Make sure that std{err,out} write to /dev/null so we prevent the
+	// testing backend to print "PASS" along with the coverage.  We really
+	// want the coverage to be set via the `-test.coverprofile=$path` flag.
+	null, _ := os.Open(os.DevNull)
+	os.Stdout = null
+	os.Stderr = null
+}
