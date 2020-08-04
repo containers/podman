cd74f66bad464f03353321184466da61032b78a5
Brent Baude <bbaude@redhat.com>
2020-08-04 13:26:06 -0500

    remove --latest for all remote commands
    instead of hiding the latest options for podman-remote or catching an error if podman --remote <cmd> -l is used, we no longer add the latest option to any remote command.  podman will error with a "unknown flag" option.
    
    Fixes: #7127
    
    Signed-off-by: Brent Baude <bbaude@redhat.com>

--- cmd/podman/validate/latest.go
+++ cmd/podman/validate/latest.go
@@ -7,9 +7,8 @@ import (
 
 func AddLatestFlag(cmd *cobra.Command, b *bool) {
 	// Initialization flag verification
-	cmd.Flags().BoolVarP(b, "latest", "l", false,
-		"Act on the latest container podman is aware of\nNot supported with the \"--remote\" flag")
-	if registry.IsRemote() {
-		_ = cmd.Flags().MarkHidden("latest")
+	if !registry.IsRemote() {
+		cmd.Flags().BoolVarP(b, "latest", "l", false,
+			"Act on the latest container podman is aware of\nNot supported with the \"--remote\" flag")
 	}
 }
