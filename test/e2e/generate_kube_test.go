d6258eb6c234d60db068742308a8a3f9b002ea7d
Andy Librian <andylibrian@gmail.com>
2020-10-04 21:11:45 +0700

    Add support for resource memory limit to generate kube
    addresses #7855
    
    Signed-off-by: Andy Librian <andylibrian@gmail.com>

--- test/e2e/generate_kube_test.go
+++ test/e2e/generate_kube_test.go
@@ -235,6 +235,31 @@ var _ = Describe("Podman generate kube", func() {
 		}
 	})
 
+	It("podman generate kube on pod with memory limit", func() {
+		podName := "testMemoryLimit"
+		podSession := podmanTest.Podman([]string{"pod", "create", "--name", podName})
+		podSession.WaitWithDefaultTimeout()
+		Expect(podSession.ExitCode()).To(Equal(0))
+
+		ctr1Name := "ctr1"
+		ctr1Session := podmanTest.Podman([]string{"create", "--name", ctr1Name, "--pod", podName, "--memory", "10Mi", ALPINE, "top"})
+		ctr1Session.WaitWithDefaultTimeout()
+		Expect(ctr1Session.ExitCode()).To(Equal(0))
+
+		kube := podmanTest.Podman([]string{"generate", "kube", podName})
+		kube.WaitWithDefaultTimeout()
+		Expect(kube.ExitCode()).To(Equal(0))
+
+		pod := new(v1.Pod)
+		err := yaml.Unmarshal(kube.Out.Contents(), pod)
+		Expect(err).To(BeNil())
+
+		for _, ctr := range pod.Spec.Containers {
+			memoryLimit, _ := ctr.Resources.Limits.Memory().AsInt64()
+			Expect(memoryLimit).To(Equal(int64(10 * 1024 * 1024)))
+		}
+	})
+
 	It("podman generate kube on pod with ports", func() {
 		podName := "test"
 		podSession := podmanTest.Podman([]string{"pod", "create", "--name", podName, "-p", "4000:4000", "-p", "5000:5000"})
