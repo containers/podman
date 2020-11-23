5d9647a12094c953436b1f9af44f88e53db4af16
Valentin Rothberg <rothberg@redhat.com>
2020-11-23 10:35:52 +0100

    clarify ps(1) fallback of `podman top`
    Podman top falls back to executing ps(1) inside the container in the
    presence of ps-specific flags.  Clarify that a bit more to help users
    resolve issues when, for instance, ps(1) isn't installed in the
    container.
    
    Signed-off-by: Valentin Rothberg <rothberg@redhat.com>

--- cmd/podman/containers/top.go
+++ cmd/podman/containers/top.go
@@ -18,12 +18,10 @@ import (
 )
 
 var (
-	topDescription = `Similar to system "top" command.
+	topDescription = `Display the running processes of a container.
 
-  Specify format descriptors to alter the output.
-
-  Running "podman top -l pid pcpu seccomp" will print the process ID, the CPU percentage and the seccomp mode of each process of the latest container.`
-
+  The top command extends the ps(1) compatible AIX descriptors with container-specific ones as shown below.  In the presence of ps(1) specific flags (e.g, -eo), Podman will execute ps(1) inside the container.
+`
 	topOptions = entities.TopOptions{}
 
 	topCommand = &cobra.Command{
