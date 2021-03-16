21f229a3dd3c1348d8f882050203d96f261f78ea
Nalin Dahyabhai <nalin@redhat.com>
2021-03-16 16:01:26 -0400

    Downgrade github.com/coreos/go-systemd/v22
    Downgrade github.com/coreos/go-systemd/v22 to a version that will build
    against systemd headers that we have on CentOS 8.  This also pulls in
    github.com/varlink/go.
    
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>

--- vendor/github.com/coreos/go-systemd/v22/activation/files.go
+++ vendor/github.com/coreos/go-systemd/v22/activation/files.go
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build !windows
-
 // Package activation implements primitives for systemd socket activation.
 package activation
 
