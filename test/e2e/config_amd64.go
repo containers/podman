b847ba7e30e2713f8ff10082e801a6815a023ef6
baude <bbaude@redhat.com>
2021-04-07 13:45:07 -0500

    speed up CI handling of images
    now that ci uses cached images, putting the large toolbox image into
    cache should help speed up tests.
    
    Signed-off-by: baude <bbaude@redhat.com>

--- test/e2e/config_amd64.go
+++ test/e2e/config_amd64.go
@@ -5,7 +5,7 @@ var (
 	STORAGE_OPTIONS          = "--storage-driver vfs"
 	ROOTLESS_STORAGE_FS      = "vfs"
 	ROOTLESS_STORAGE_OPTIONS = "--storage-driver vfs"
-	CACHE_IMAGES             = []string{ALPINE, BB, fedoraMinimal, nginx, redis, registry, infra, labels, healthcheck, ubi_init, ubi_minimal}
+	CACHE_IMAGES             = []string{ALPINE, BB, fedoraMinimal, nginx, redis, registry, infra, labels, healthcheck, ubi_init, ubi_minimal, fedoraToolbox}
 	nginx                    = "quay.io/libpod/alpine_nginx:latest"
 	BB_GLIBC                 = "docker.io/library/busybox:glibc"
 	registry                 = "quay.io/libpod/registry:2.6"
