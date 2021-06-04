9c5048544068c37e70d4e9b135180bfbc70e4b12
Valentin Rothberg <rothberg@redhat.com>
2021-06-04 13:15:33 +0200

    remote events: support labels
    Certain event meta data was lost when converting the remote events to
    libpod events and vice versa.  Enable the skipped system tests for
    remote.
    
    Signed-off-by: Valentin Rothberg <rothberg@redhat.com>

--- pkg/domain/entities/events.go
+++ pkg/domain/entities/events.go
@@ -30,29 +30,41 @@ func ConvertToLibpodEvent(e Event) *libpodEvents.Event {
 	if err != nil {
 		return nil
 	}
+	image := e.Actor.Attributes["image"]
+	name := e.Actor.Attributes["name"]
+	details := e.Actor.Attributes
+	delete(details, "image")
+	delete(details, "name")
+	delete(details, "containerExitCode")
 	return &libpodEvents.Event{
 		ContainerExitCode: exitCode,
 		ID:                e.Actor.ID,
-		Image:             e.Actor.Attributes["image"],
-		Name:              e.Actor.Attributes["name"],
+		Image:             image,
+		Name:              name,
 		Status:            status,
 		Time:              time.Unix(e.Time, e.TimeNano),
 		Type:              t,
+		Details: libpodEvents.Details{
+			Attributes: details,
+		},
 	}
 }
 
 // ConvertToEntitiesEvent converts a libpod event to an entities one.
 func ConvertToEntitiesEvent(e libpodEvents.Event) *Event {
+	attributes := e.Details.Attributes
+	if attributes == nil {
+		attributes = make(map[string]string)
+	}
+	attributes["image"] = e.Image
+	attributes["name"] = e.Name
+	attributes["containerExitCode"] = strconv.Itoa(e.ContainerExitCode)
 	return &Event{dockerEvents.Message{
 		Type:   e.Type.String(),
 		Action: e.Status.String(),
 		Actor: dockerEvents.Actor{
-			ID: e.ID,
-			Attributes: map[string]string{
-				"image":             e.Image,
-				"name":              e.Name,
-				"containerExitCode": strconv.Itoa(e.ContainerExitCode),
-			},
+			ID:         e.ID,
+			Attributes: attributes,
 		},
 		Scope:    "local",
 		Time:     e.Time.Unix(),
