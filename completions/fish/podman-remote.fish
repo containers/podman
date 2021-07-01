84da70a0f36e7d6fc87919ca4a69b20bc9d43f72
Paul Holzinger <pholzing@redhat.com>
2021-07-01 20:01:44 +0200

    update shell completion scripts
    The new cobra v1.2.0 release brings a number of bug fixes for shell
    completion scripts. Regenerate the scripts with `make completions`
    to sync them with the upstream version, currently we have some custom
    ones to avoid some upstream bugs. Because the new cobra version has
    all fixes we should use the upstream scripts.
    Add a check to CI to ensure we always use the up to date scripts.
    
    [NO TESTS NEEDED]
    
    Signed-off-by: Paul Holzinger <pholzing@redhat.com>

--- completions/fish/podman-remote.fish
+++ completions/fish/podman-remote.fish
@@ -1,7 +1,7 @@
 # fish completion for podman-remote                        -*- shell-script -*-
 
 function __podman_remote_debug
-    set file "$BASH_COMP_DEBUG_FILE"
+    set -l file "$BASH_COMP_DEBUG_FILE"
     if test -n "$file"
         echo "$argv" >> $file
     end
@@ -10,32 +10,38 @@ end
 function __podman_remote_perform_completion
     __podman_remote_debug "Starting __podman_remote_perform_completion"
 
-    set args (string split -- " " (string trim -l (commandline -c)))
-    set lastArg "$args[-1]"
+    # Extract all args except the last one
+    set -l args (commandline -opc)
+    # Extract the last arg and escape it in case it is a space
+    set -l lastArg (string escape -- (commandline -ct))
 
     __podman_remote_debug "args: $args"
     __podman_remote_debug "last arg: $lastArg"
 
-    set emptyArg ""
-    if test -z "$lastArg"
-        __podman_remote_debug "Setting emptyArg"
-        set emptyArg \"\"
-    end
-    __podman_remote_debug "emptyArg: $emptyArg"
+    set -l requestComp "$args[1] __complete $args[2..-1] $lastArg"
 
-    set requestComp "$args[1] __complete $args[2..-1] $emptyArg"
     __podman_remote_debug "Calling $requestComp"
+    set -l results (eval $requestComp 2> /dev/null)
 
-    # Call the command as a sub-shell so that we can redirect any errors
-    # For example, if $requestComp has an unmatched quote
-    # https://github.com/spf13/cobra/issues/1214
-    set results (fish -c "$requestComp" 2> /dev/null)
-    set comps $results[1..-2]
-    set directiveLine $results[-1]
+    # Some programs may output extra empty lines after the directive.
+    # Let's ignore them or else it will break completion.
+    # Ref: https://github.com/spf13/cobra/issues/1279
+    for line in $results[-1..1]
+        if test (string trim -- $line) = ""
+            # Found an empty line, remove it
+            set results $results[1..-2]
+        else
+            # Found non-empty line, we have our proper output
+            break
+        end
+    end
+
+    set -l comps $results[1..-2]
+    set -l directiveLine $results[-1]
 
     # For Fish, when completing a flag with an = (e.g., <program> -n=<TAB>)
     # completions must be prefixed with the flag
-    set flagPrefix (string match -r -- '-.*=' "$lastArg")
+    set -l flagPrefix (string match -r -- '-.*=' "$lastArg")
 
     __podman_remote_debug "Comps: $comps"
     __podman_remote_debug "DirectiveLine: $directiveLine"
@@ -58,7 +64,7 @@ function __podman_remote_prepare_completions
     # Start fresh
     set --erase __podman_remote_comp_results
 
-    set results (__podman_remote_perform_completion)
+    set -l results (__podman_remote_perform_completion)
     __podman_remote_debug "Completion results: $results"
 
     if test -z "$results"
@@ -67,39 +73,39 @@ function __podman_remote_prepare_completions
         return 1
     end
 
-    set directive (string sub --start 2 $results[-1])
+    set -l directive (string sub --start 2 $results[-1])
     set --global __podman_remote_comp_results $results[1..-2]
 
     __podman_remote_debug "Completions are: $__podman_remote_comp_results"
     __podman_remote_debug "Directive is: $directive"
 
-    set shellCompDirectiveError 1
-    set shellCompDirectiveNoSpace 2
-    set shellCompDirectiveNoFileComp 4
-    set shellCompDirectiveFilterFileExt 8
-    set shellCompDirectiveFilterDirs 16
+    set -l shellCompDirectiveError 1
+    set -l shellCompDirectiveNoSpace 2
+    set -l shellCompDirectiveNoFileComp 4
+    set -l shellCompDirectiveFilterFileExt 8
+    set -l shellCompDirectiveFilterDirs 16
 
     if test -z "$directive"
         set directive 0
     end
 
-    set compErr (math (math --scale 0 $directive / $shellCompDirectiveError) % 2)
+    set -l compErr (math (math --scale 0 $directive / $shellCompDirectiveError) % 2)
     if test $compErr -eq 1
         __podman_remote_debug "Received error directive: aborting."
         # Might as well do file completion, in case it helps
         return 1
     end
 
-    set filefilter (math (math --scale 0 $directive / $shellCompDirectiveFilterFileExt) % 2)
-    set dirfilter (math (math --scale 0 $directive / $shellCompDirectiveFilterDirs) % 2)
+    set -l filefilter (math (math --scale 0 $directive / $shellCompDirectiveFilterFileExt) % 2)
+    set -l dirfilter (math (math --scale 0 $directive / $shellCompDirectiveFilterDirs) % 2)
     if test $filefilter -eq 1; or test $dirfilter -eq 1
         __podman_remote_debug "File extension filtering or directory filtering not supported"
         # Do full file completion instead
         return 1
     end
 
-    set nospace (math (math --scale 0 $directive / $shellCompDirectiveNoSpace) % 2)
-    set nofiles (math (math --scale 0 $directive / $shellCompDirectiveNoFileComp) % 2)
+    set -l nospace (math (math --scale 0 $directive / $shellCompDirectiveNoSpace) % 2)
+    set -l nofiles (math (math --scale 0 $directive / $shellCompDirectiveNoFileComp) % 2)
 
     __podman_remote_debug "nospace: $nospace, nofiles: $nofiles"
 
@@ -109,34 +115,34 @@ function __podman_remote_prepare_completions
     # may not already be filtered so as to allow fish to match on different
     # criteria than the prefix.
     if test $nospace -ne 0; or test $nofiles -eq 0
-        set prefix (commandline -t)
+        set -l prefix (commandline -t | string escape --style=regex)
         __podman_remote_debug "prefix: $prefix"
 
-        set completions
-        for comp in $__podman_remote_comp_results
-            if test (string match -e -r -- "^$prefix" "$comp")
-                set -a completions $comp
-            end
-        end
+        set -l completions (string match -r -- "^$prefix.*" $__podman_remote_comp_results)
         set --global __podman_remote_comp_results $completions
         __podman_remote_debug "Filtered completions are: $__podman_remote_comp_results"
 
         # Important not to quote the variable for count to work
-        set numComps (count $__podman_remote_comp_results)
+        set -l numComps (count $__podman_remote_comp_results)
         __podman_remote_debug "numComps: $numComps"
 
         if test $numComps -eq 1; and test $nospace -ne 0
-            # To support the "nospace" directive we trick the shell
-            # by outputting an extra, longer completion.
-            # We must first split on \t to get rid of the descriptions because
-            # the extra character we add to the fake second completion must be
-            # before the description.  We don't need descriptions anyway since
-            # there is only a single real completion which the shell will expand
-            # immediately.
-            __podman_remote_debug "Adding second completion to perform nospace directive"
-            set split (string split --max 1 \t $__podman_remote_comp_results[1])
-            set --global __podman_remote_comp_results $split[1] $split[1].
-            __podman_remote_debug "Completions are now: $__podman_remote_comp_results"
+            # We must first split on \t to get rid of the descriptions to be
+            # able to check what the actual completion will be.
+            # We don't need descriptions anyway since there is only a single
+            # real completion which the shell will expand immediately.
+            set -l split (string split --max 1 \t $__podman_remote_comp_results[1])
+
+            # Fish won't add a space if the completion ends with any
+            # of the following characters: @=/:.,
+            set -l lastChar (string sub -s -1 -- $split)
+            if not string match -r -q "[@=/:.,]" -- "$lastChar"
+                # In other cases, to support the "nospace" directive we trick the shell
+                # by outputting an extra, longer completion.
+                __podman_remote_debug "Adding second completion to perform nospace directive"
+                set --global __podman_remote_comp_results $split[1] $split[1].
+                __podman_remote_debug "Completions are now: $__podman_remote_comp_results"
+            end
         end
 
         if test $numComps -eq 0; and test $nofiles -eq 0
@@ -152,10 +158,14 @@ end
 
 # Since Fish completions are only loaded once the user triggers them, we trigger them ourselves
 # so we can properly delete any completions provided by another script.
-# The space after the program name is essential to trigger completion for the program
-# and not completion of the program name itself.
-complete --do-complete "podman-remote " > /dev/null 2>&1
-# Using '> /dev/null 2>&1' since '&>' is not supported in older versions of fish.
+# Only do this if the program can be found, or else fish may print some errors; besides,
+# the existing completions will only be loaded if the program can be found.
+if type -q "podman-remote"
+    # The space after the program name is essential to trigger completion for the program
+    # and not completion of the program name itself.
+    # Also, we use '> /dev/null 2>&1' since '&>' is not supported in older versions of fish.
+    complete --do-complete "podman-remote " > /dev/null 2>&1
+end
 
 # Remove any pre-existing completions for the program since we will be handling all of them.
 complete -c podman-remote -e
