e9b71e3c22f26191f3aa7f669b7763ea2f4d4520
Adrien Raffin-Caboisse <restanrm@users.noreply.github.com>
2021-08-27 18:42:43 +0200

    Add parsing to image manifest if result is not manifest list
    This modification aims to even the behavior between the cli and the REST
    API.
    
    When retrieving a manifest of an image via
    ```
    podman manifest inspect custom.registry.com/container:v0.1
    ```
    
    we obtain the following warning: `The manifest type
    application/vnd.docker.distribution.manifest.v2+json is not a manifest
    list but a single image.` and the content of the manifest.
    
    When using the REST API with the socket, we receive an empty manifest
    response like the following:
    ```
    {"schemaVersion":2,"mediaType":"application/vnd.docker.distribution.manifest.v2+json","manifests":null}
    ```
    
    Signed-off-by: Adrien Raffin-Caboisse <raffinadrien@gmail.com>

--- pkg/api/handlers/libpod/manifests.go
+++ pkg/api/handlers/libpod/manifests.go
@@ -90,6 +90,16 @@ func ManifestInspect(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if len(schema2List.Manifests) == 0 {
+		var schema2 manifest.Schema2
+		if err := json.Unmarshal(rawManifest, &schema2); err != nil {
+			utils.Error(w, "Something went wrong.", http.StatusInternalServerError, err)
+			return
+		}
+		utils.WriteResponse(w, http.StatusOK, schema2)
+		return
+	}
+
 	utils.WriteResponse(w, http.StatusOK, schema2List)
 }
 
