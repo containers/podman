6a4cd465feecd2945a8023c168f701b238556380
Adrien Raffin-Caboisse <adrien.raffin@sekoia.fr>
2021-08-30 14:32:52 +0200

    tests(apiv2): add test for inspect on single image
    Signed-off-by: Adrien Raffin-Caboisse <raffinadrien@gmail.com>

--- test/apiv2/python/rest_api/test_v2_0_0_manifest.py
+++ test/apiv2/python/rest_api/test_v2_0_0_manifest.py
@@ -9,6 +9,18 @@ class ManifestTestCase(APITestCase):
         r = requests.post(self.uri("/manifests/create"), params={"name": "ThisIsAnInvalidImage"})
         self.assertEqual(r.status_code, 400, r.text)
 
+    def test_manifest_inspect_multi_image(self):
+        r = requests.get(self.uri("/manifests/quay.io/libpod/busybox:latest/json"))
+        print(r.json())
+        j = r.json()
+        self.assertEqual(len(j.get("manifests", [])), 9)
+
+    def test_manifest_inspect_single_image(self):
+        r = requests.get(self.uri("/manifests/quay.io/libpod/busybox@sha256:c9249fdf56138f0d929e2080ae98ee9cb2946f71498fc1484288e6a935b5e5bc/json"))
+        print(r.json())
+        j = r.json()
+        self.assertEqual(j.get("config", {}).get("mediaType", ""), "application/vnd.docker.container.image.v1+json")
+
 
 if __name__ == "__main__":
     unittest.main()
