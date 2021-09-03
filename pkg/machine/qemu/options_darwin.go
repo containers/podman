c189c8679d02f0ffbd4bc5a174fc1bb22fd02516
Michael Anckaert <michael.anckaert@sinax.be>
2021-09-03 16:17:28 +0200

    Fix #11418 - Default TMPDIR to /tmp on OS X
    Signed-off-by: Michael Anckaert <michael.anckaert@sinax.be>

--- pkg/machine/qemu/options_darwin.go
+++ pkg/machine/qemu/options_darwin.go
@@ -2,14 +2,12 @@ package qemu
 
 import (
 	"os"
-
-	"github.com/pkg/errors"
 )
 
 func getRuntimeDir() (string, error) {
 	tmpDir, ok := os.LookupEnv("TMPDIR")
 	if !ok {
-		return "", errors.New("unable to resolve TMPDIR")
+		tmpDir = "/tmp"
 	}
 	return tmpDir, nil
 }
