a1199dbaf1e42de665a8fc40ba0dd17d21f05ab7
Valentin Rothberg <rothberg@redhat.com>
2021-09-28 14:51:51 +0200

    [CI:DOCS] cmd/podman: no dot for short descriptions
    Remove trailing dots in the short descriptions for the sake of
    consistency.  Noticed while parsing `podman help`.
    
    Signed-off-by: Valentin Rothberg <rothberg@redhat.com>

--- cmd/podman/generate/generate.go
+++ cmd/podman/generate/generate.go
@@ -11,7 +11,7 @@ var (
 	// Command: podman _generate_
 	generateCmd = &cobra.Command{
 		Use:   "generate",
-		Short: "Generate structured data based on containers, pods or volumes.",
+		Short: "Generate structured data based on containers, pods or volumes",
 		Long:  "Generate structured data (e.g., Kubernetes YAML or systemd units) based on containers, pods or volumes.",
 		RunE:  validate.SubCommandExists,
 	}
