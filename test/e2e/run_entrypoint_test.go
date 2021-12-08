b526a0ccdc289edaaff4ee1ab9ffab60310c4b53
Aditya Rajan <arajan@redhat.com>
2021-12-08 17:38:45 +0530

    specgen: honor empty args for entrypoint
    Users should be able to override containers entrypoint using
    `--entrypoint ""` following works fine for podman but not for podman
    remote.
    
    Specgen ignores empty argument for entrypoint so make specgen honor
    empty arguments.
    
    Signed-off-by: Aditya Rajan <arajan@redhat.com>

--- test/e2e/run_entrypoint_test.go
+++ test/e2e/run_entrypoint_test.go
@@ -103,7 +103,6 @@ ENTRYPOINT ["grep", "Alpine", "/etc/os-release"]
 	})
 
 	It("podman run user entrypoint overrides image entrypoint and image cmd", func() {
-		SkipIfRemote("#12521: podman-remote not handling passing empty --entrypoint")
 		dockerfile := `FROM quay.io/libpod/alpine:latest
 CMD ["-i"]
 ENTRYPOINT ["grep", "Alpine", "/etc/os-release"]
