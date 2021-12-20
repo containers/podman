48cb446514f1ae8089530d728337b7a91730afe2
Giuseppe Scrivano <gscrivan@redhat.com>
2021-12-20 09:06:49 +0100

    build: fix test for subid 4
    libsubid changes its ABI in version 4.  Account for the different name
    in the configure script.
    
    Closes: https://github.com/containers/podman/issues/12654
    
    [NO NEW TESTS NEEDED] it is a change in the build script
    
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- hack/libsubid_tag.sh
+++ hack/libsubid_tag.sh
@@ -7,9 +7,19 @@ mkdir -p "$tmpdir"
 trap 'rm -fr "$tmpdir"' EXIT
 cc -o "$tmpdir"/libsubid_tag -l subid -x c - > /dev/null 2> /dev/null << EOF
 #include <shadow/subid.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+const char *Prog = "test";
+FILE *shadow_logfd = NULL;
+
 int main() {
 	struct subid_range *ranges = NULL;
+#if SUBID_ABI_MAJOR >= 4
+	subid_get_uid_ranges("root", &ranges);
+#else
 	get_subuid_ranges("root", &ranges);
+#endif
 	free(ranges);
 	return 0;
 }
