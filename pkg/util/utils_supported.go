791c8c347c1bbd69763b7358acfa415c18764d62
Brent Baude <bbaude@redhat.com>
2022-03-01 13:07:24 -0600

    Revert "use GetRuntimeDir() from c/common"
    This reverts commit fc5cf812c81a10f8a021aae11df5f12ab2a6f6f6.
    
    [NO NEW TESTS NEEDED]
    
    Signed-off-by: Brent Baude <bbaude@redhat.com>

--- pkg/util/utils_supported.go
+++ pkg/util/utils_supported.go
@@ -6,21 +6,67 @@ package util
 //  should work to take darwin from this
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"syscall"
 
-	cutil "github.com/containers/common/pkg/util"
 	"github.com/containers/podman/v4/pkg/rootless"
 	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 )
 
 // GetRuntimeDir returns the runtime directory
 func GetRuntimeDir() (string, error) {
+	var rootlessRuntimeDirError error
+
 	if !rootless.IsRootless() {
 		return "", nil
 	}
-	return cutil.GetRuntimeDir()
+
+	rootlessRuntimeDirOnce.Do(func() {
+		runtimeDir := os.Getenv("XDG_RUNTIME_DIR")
+		uid := fmt.Sprintf("%d", rootless.GetRootlessUID())
+		if runtimeDir == "" {
+			tmpDir := filepath.Join("/run", "user", uid)
+			if err := os.MkdirAll(tmpDir, 0700); err != nil {
+				logrus.Debug(err)
+			}
+			st, err := os.Stat(tmpDir)
+			if err == nil && int(st.Sys().(*syscall.Stat_t).Uid) == os.Geteuid() && (st.Mode().Perm()&0700 == 0700) {
+				runtimeDir = tmpDir
+			}
+		}
+		if runtimeDir == "" {
+			tmpDir := filepath.Join(os.TempDir(), fmt.Sprintf("podman-run-%s", uid))
+			if err := os.MkdirAll(tmpDir, 0700); err != nil {
+				logrus.Debug(err)
+			}
+			st, err := os.Stat(tmpDir)
+			if err == nil && int(st.Sys().(*syscall.Stat_t).Uid) == os.Geteuid() && (st.Mode().Perm()&0700 == 0700) {
+				runtimeDir = tmpDir
+			}
+		}
+		if runtimeDir == "" {
+			home := os.Getenv("HOME")
+			if home == "" {
+				rootlessRuntimeDirError = fmt.Errorf("neither XDG_RUNTIME_DIR nor HOME was set non-empty")
+				return
+			}
+			resolvedHome, err := filepath.EvalSymlinks(home)
+			if err != nil {
+				rootlessRuntimeDirError = errors.Wrapf(err, "cannot resolve %s", home)
+				return
+			}
+			runtimeDir = filepath.Join(resolvedHome, "rundir")
+		}
+		rootlessRuntimeDir = runtimeDir
+	})
+
+	if rootlessRuntimeDirError != nil {
+		return "", rootlessRuntimeDirError
+	}
+	return rootlessRuntimeDir, nil
 }
 
 // GetRootlessConfigHomeDir returns the config home directory when running as non root
