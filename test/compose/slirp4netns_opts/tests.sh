c25213c8f8d23d82768b53d23909cdbf9f9235ea
Paul Holzinger <pholzing@redhat.com>
2022-03-22 14:50:39 +0100

    fix compose test error in retry logic
    We cannot use local var outside of a function. We have to use a global
    one.
    
    Log: https://storage.googleapis.com/cirrus-ci-6707778565701632-fcae48/artifacts/containers/podman/5970023511490560/html/compose_v2-podman-fedora-35-root-host.log.html
    
    Signed-off-by: Paul Holzinger <pholzing@redhat.com>

--- test/compose/slirp4netns_opts/tests.sh
+++ test/compose/slirp4netns_opts/tests.sh
@@ -1,20 +1,19 @@
 # -*- bash -*-
 
-output="$(cat $OUTFILE)"
 expected="teststring"
 
 # Reading from the nc socket is flaky because docker-compose only starts
 # the containers. We cannot know at this point if the container did already
 # send the message. Give the container 5 seconds time to send the message
 # to prevent flakes.
-local _timeout=5
-while [ $_timeout -gt 0 ]; do
+container_timeout=5
+while [ $container_timeout -gt 0 ]; do
+    output="$(< $OUTFILE)"
     if [ -n "$output" ]; then
         break
     fi
     sleep 1
-    _timeout=$(($_timeout - 1))
-    output="$(cat $OUTFILE)"
+    container_timeout=$(($container_timeout - 1))
 done
 
 is "$output" "$expected" "$testname : nc received teststring"
