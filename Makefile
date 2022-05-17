containers/podman

commit 0d1ba0a58fdb15af7ee3f3650f633156575d296d

Remove changelog.txt from the repository
The changelog.txt file hasn't been kept in sync with release tags,
especially on main, so remove it.

The release notes will be featured in RELEASE_NOTES.md.

Signed-off-by: jesperpedersen <jesper.pedersen@redhat.com>

[NO TESTS NEEDED]

Makefile
@@ -258,7 +258,7 @@ test/goecho/goecho: .gopathok $(wildcard test/goecho/*.go)
 
 .PHONY: codespell
 codespell:
-	codespell -S bin,vendor,.git,go.sum,changelog.txt,.cirrus.yml,"RELEASE_NOTES.md,*.xz,*.gz,*.ps1,*.tar,*.tgz,bin2img,*ico,*.png,*.1,*.5,copyimg,*.orig,apidoc.go" -L uint,iff,od,seeked,splitted,marge,ERRO,hist,ether -w
+	codespell -S bin,vendor,.git,go.sum,.cirrus.yml,"RELEASE_NOTES.md,*.xz,*.gz,*.ps1,*.tar,*.tgz,bin2img,*ico,*.png,*.1,*.5,copyimg,*.orig,apidoc.go" -L uint,iff,od,seeked,splitted,marge,ERRO,hist,ether -w
 
 .PHONY: validate
 validate: gofmt lint .gitvalidation validate.completions man-page-check swagger-check tests-included tests-expect-exit
@@ -468,17 +468,6 @@ swagger: pkg/api/swagger.yaml
 .PHONY: docker-docs
 docker-docs: docs
 	(cd docs; ./dckrman.sh ./build/man/*.1)
-
-.PHONY: changelog
-changelog: ## Generate updated changelog.txt from git logs
-	@echo "Creating changelog from $(CHANGELOG_BASE) to $(CHANGELOG_TARGET)"
-	$(eval TMPFILE := $(shell mktemp podman_tmp_XXXX))
-	$(shell cat changelog.txt > $(TMPFILE))
-	$(shell echo "- Changelog for $(CHANGELOG_TARGET) ($(ISODATE)):" > changelog.txt)
-	$(shell git log --no-merges --format="  * %s" $(CHANGELOG_BASE)..$(CHANGELOG_TARGET) >> changelog.txt)
-	$(shell echo "" >> changelog.txt)
-	$(shell cat $(TMPFILE) >> changelog.txt)
-	$(shell rm $(TMPFILE))
 
 # Workaround vim syntax highlighting bug: "
 

contrib/cirrus/pr-should-include-tests
@@ -35,7 +35,6 @@ filtered_changes=$(git diff --name-status $base $head |
                        fgrep -vx .cirrus.yml          |
                        fgrep -vx .gitignore           |
                        fgrep -vx Makefile             |
-                       fgrep -vx changelog.txt        |
                        fgrep -vx go.mod               |
                        fgrep -vx go.sum               |
                        egrep -v  '^[^/]+\.md$'        |

hack/release.sh
@@ -36,20 +36,10 @@ write_spec_version()
 	sed -i "s/^\(Version: *\).*/\1${LOCAL_VERSION}/" contrib/spec/podman.spec.in
 }
 
-write_changelog()
-{
-	echo "- Changelog for v${VERSION} (${DATE})" >.changelog.txt &&
-	git log --no-merges --format='  * %s' "${LAST_TAG}..HEAD" >>.changelog.txt &&
-	echo >>.changelog.txt &&
-	cat changelog.txt >>.changelog.txt &&
-	mv -f .changelog.txt changelog.txt
-}
-
 release_commit()
 {
 	write_go_version "${VERSION}" &&
 	write_spec_version "${VERSION}" &&
-	write_changelog &&
 	git commit -asm "Bump to v${VERSION}"
 }
 

commit 9bf81814a2a5fb4ecda9856d32e6cbd0f8f83644

Makefile: rm CHANGELOG_* and ISODATE vars
Those are not used since commit 0d1ba0a58fdb15af7e.

Fixes: 0d1ba0a58fdb15af7
Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

Makefile
@@ -27,8 +27,6 @@ COVERAGE_PATH ?= .coverage
 DESTDIR ?=
 EPOCH_TEST_COMMIT ?= $(shell git merge-base $${DEST_BRANCH:-main} HEAD)
 HEAD ?= HEAD
-CHANGELOG_BASE ?= HEAD~
-CHANGELOG_TARGET ?= HEAD
 PROJECT := github.com/containers/podman
 GIT_BASE_BRANCH ?= origin/main
 GIT_BRANCH ?= $(shell git rev-parse --abbrev-ref HEAD 2>/dev/null)
@@ -86,10 +84,8 @@ GIT_COMMIT ?= $(if $(shell git status --porcelain --untracked-files=no),${COMMIT
 DATE_FMT = %s
 ifdef SOURCE_DATE_EPOCH
 	BUILD_INFO ?= $(shell date -u -d "@$(SOURCE_DATE_EPOCH)" "+$(DATE_FMT)" 2>/dev/null || date -u -r "$(SOURCE_DATE_EPOCH)" "+$(DATE_FMT)" 2>/dev/null || date -u "+$(DATE_FMT)")
-	ISODATE ?= $(shell date -d "@$(SOURCE_DATE_EPOCH)" --iso-8601)
 else
 	BUILD_INFO ?= $(shell date "+$(DATE_FMT)")
-	ISODATE ?= $(shell date --iso-8601)
 endif
 LIBPOD := ${PROJECT}/v4/libpod
 GOFLAGS ?= -trimpath