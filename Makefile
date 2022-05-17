containers/podman

commit 3b9177995e0124beb064ef8615ba9a2ae7ca4f4b

vendor test tools in submodule
Instead of using the main module we should vendor the test tools in a
different directory. That way we do not add extra dependencies to the
main module which can be problemetic for packages or other users.

This is already done in buildah so this makes us more consitent.

Signed-off-by: Paul Holzinger <pholzing@redhat.com>

.gitignore
@@ -33,6 +33,7 @@ release.txt
 /test/goecho/goecho
 /test/version/version
 /test/testvol/testvol
+/test/tools/build
 .vscode*
 tags
 result

Makefile
@@ -136,7 +136,7 @@ endif
 
 export PATH := $(PATH):$(GOBIN):$(CURDIR)/hack
 
-GOMD2MAN ?= $(shell command -v go-md2man || echo '$(GOBIN)/go-md2man')
+GOMD2MAN ?= $(shell command -v go-md2man || echo './test/tools/build/go-md2man')
 
 CROSS_BUILD_TARGETS := \
 	bin/podman.cross.linux.amd64 \
@@ -232,7 +232,7 @@ endif
 .PHONY: .gitvalidation
 .gitvalidation: .gopathok
 	@echo "Validating vs commit '$(call err_if_empty,EPOCH_TEST_COMMIT)'"
-	GIT_CHECK_EXCLUDE="./vendor:docs/make.bat:test/buildah-bud/buildah-tests.diff" $(GOBIN)/git-validation -run DCO,short-subject,dangling-whitespace -range $(EPOCH_TEST_COMMIT)..$(HEAD)
+	GIT_CHECK_EXCLUDE="./vendor:docs/make.bat:test/buildah-bud/buildah-tests.diff" ./test/tools/build/git-validation -run DCO,short-subject,dangling-whitespace -range $(EPOCH_TEST_COMMIT)..$(HEAD)
 
 .PHONY: lint
 lint: golangci-lint
@@ -856,13 +856,8 @@ install.systemd:
 endif
 
 .PHONY: install.tools
-install.tools: .install.goimports .install.gitvalidation .install.md2man .install.ginkgo .install.golangci-lint .install.bats ## Install needed tools
-
-.install.goimports: .gopathok
-	if [ ! -x "$(GOBIN)/goimports" ]; then \
-		$(GO) install ./vendor/golang.org/x/tools/cmd/goimports ; \
-	fi
-	touch .install.goimports
+install.tools: .install.ginkgo .install.golangci-lint .install.bats ## Install needed tools
+	make -C test/tools
 
 .PHONY: .install.ginkgo
 .install.ginkgo: .gopathok
@@ -870,15 +865,15 @@ install.tools: .install.goimports .install.gitvalidation .install.md2man .instal
 		$(GO) install $(BUILDFLAGS) ./vendor/github.com/onsi/ginkgo/ginkgo ; \
 	fi
 
-.PHONY: .install.gitvalidation
-.install.gitvalidation: .gopathok
-	if [ ! -x "$(GOBIN)/git-validation" ]; then \
-		$(GO) install ./vendor/github.com/vbatts/git-validation ; \
-	fi
-
 .PHONY: .install.golangci-lint
 .install.golangci-lint: .gopathok
 	VERSION=1.45.2 GOBIN=$(GOBIN) ./hack/install_golangci.sh
+
+.PHONY: .install.md2man
+.install.md2man:
+	if [ ! -x "$(GOMD2MAN)" ]; then \
+		make -C test/tools build/go-md2man ; \
+	fi
 
 .PHONY: .install.bats
 .install.bats: .gopathok
@@ -888,12 +883,6 @@ install.tools: .install.goimports .install.gitvalidation .install.md2man .instal
 .install.pre-commit:
 	if [ -z "$(PRE_COMMIT)" ]; then \
 		python3 -m pip install --user pre-commit; \
-	fi
-
-.PHONY: .install.md2man
-.install.md2man: .gopathok
-	if [ ! -x "$(GOMD2MAN)" ]; then \
-		$(GO) install ./vendor/github.com/cpuguy83/go-md2man/v2 ; \
 	fi
 
 # $BUILD_TAGS variable is used in hack/golangci-lint.sh

test/tools/Makefile
@@ -1,0 +1,31 @@
+GO := go
+GO_BUILD=$(GO) build
+
+BUILDDIR := build
+
+all: $(BUILDDIR)
+
+.PHONY: vendor
+vendor:
+	$(GO) mod tidy
+	$(GO) mod vendor
+	$(GO) mod verify
+
+.PHONY: clean
+clean:
+	rm -rf $(BUILDDIR)
+
+.PHONY: $(BUILDDIR)
+$(BUILDDIR): \
+	$(BUILDDIR)/goimports \
+	$(BUILDDIR)/go-md2man \
+	$(BUILDDIR)/git-validation
+
+$(BUILDDIR)/goimports:
+	$(GO_BUILD) -o $@ ./vendor/golang.org/x/tools/cmd/goimports
+
+$(BUILDDIR)/go-md2man:
+	$(GO_BUILD) -o $@ ./vendor/github.com/cpuguy83/go-md2man/v2
+
+$(BUILDDIR)/git-validation:
+	$(GO_BUILD) -o $@ ./vendor/github.com/vbatts/git-validation

test/tools/vendor/github.com/sirupsen/logrus/.gitignore
@@ -1,0 +1,2 @@
+logrus
+vendor

commit c63bb6b9a8a9d18d661f10c1a2a6884258180fae

Makefile,.gitignore: rm .install.goimports
Recent commit 3b9177995e0124beb064ef8615ba9a2ae7ca4f4b removes
this target, but some artifacts remain. Remove those.

Fixes: 3b9177995e0124beb064ef8615ba9a2ae7ca4f4b
Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

.gitignore
@@ -39,5 +39,4 @@ tags
 result
 # Necessary to prevent hack/tree-status.sh false-positive
 /*runner_stats.log
-.install.goimports
 .generate-bindings

Makefile
@@ -923,7 +923,6 @@ clean: clean-binaries ## Clean all make artifacts
 		libpod/pod_ffjson.go \
 		libpod/container_easyjson.go \
 		libpod/pod_easyjson.go \
-		.install.goimports \
 		docs/build \
 		.venv
 	make -C docs clean