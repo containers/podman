956e2e1b842b5e753a5a2476465deb3ab25ca370
Valentin Rothberg <vrothberg@redhat.com>
2022-05-24 13:56:58 +0200

    pkg/domain/infra/abi/images_test.go: remove commented code
    Signed-off-by: Valentin Rothberg <vrothberg@redhat.com>

--- pkg/domain/infra/abi/images_test.go
+++ pkg/domain/infra/abi/images_test.go
@@ -16,39 +16,3 @@ func TestToDomainHistoryLayer(t *testing.T) {
 	newLayer := toDomainHistoryLayer(&layer)
 	assert.Equal(t, layer.Size, newLayer.Size)
 }
-
-//
-// import (
-// 	"context"
-// 	"testing"
-//
-// 	"github.com/stretchr/testify/mock"
-// )
-//
-// type MockImageRuntime struct {
-// 	mock.Mock
-// }
-//
-// func (m *MockImageRuntime) Delete(ctx context.Context, renderer func() interface{}, name string) error {
-// 	_ = m.Called(ctx, renderer, name)
-// 	return nil
-// }
-//
-// func TestImageSuccess(t *testing.T) {
-// 	actual := func() interface{} { return nil }
-//
-// 	m := new(MockImageRuntime)
-// 	m.On(
-// 		"Delete",
-// 		mock.AnythingOfType("*context.emptyCtx"),
-// 		mock.AnythingOfType("func() interface {}"),
-// 		"fedora").
-// 		Return(nil)
-//
-// 	r := DirectImageRuntime{m}
-// 	err := r.Delete(context.TODO(), actual, "fedora")
-// 	if err != nil {
-// 		t.Errorf("should be nil, got: %v", err)
-// 	}
-// 	m.AssertExpectations(t)
-// }
