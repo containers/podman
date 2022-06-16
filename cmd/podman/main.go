containers/podman

commit 44d037898ebc328432823d51ca2da298902a46e9

provide better error on invalid flag
Add a extra `See 'podman command --help'` to the error output.
With this patch you now get:
```
$ podman run -h
Error: flag needs an argument: 'h' in -h
See 'podman run --help'
```

Fixes #13082
Fixes #13002

Signed-off-by: Paul Holzinger <pholzing@redhat.com>

cmd/podman/main.go
@@ -72,6 +72,8 @@ func parseCommands() *cobra.Command {
 		}
 		parent.AddCommand(c.Command)
 
+		c.Command.SetFlagErrorFunc(flagErrorFuncfunc)
+
 		// - templates need to be set here, as PersistentPreRunE() is
 		// not called when --help is used.
 		// - rootCmd uses cobra default template not ours
@@ -84,5 +86,11 @@ func parseCommands() *cobra.Command {
 		os.Exit(1)
 	}
 
+	rootCmd.SetFlagErrorFunc(flagErrorFuncfunc)
 	return rootCmd
+}
+
+func flagErrorFuncfunc(c *cobra.Command, e error) error {
+	e = fmt.Errorf("%w\nSee '%s --help'", e, c.CommandPath())
+	return e
 }

test/system/001-basic.bats
@@ -88,7 +88,8 @@ function setup() {
 
     # ...but no matter what, --remote is never allowed after subcommand
     PODMAN="${podman_non_remote} ${podman_args[@]}" run_podman 125 version --remote
-    is "$output" "Error: unknown flag: --remote" "podman version --remote"
+    is "$output" "Error: unknown flag: --remote
+See 'podman version --help'" "podman version --remote"
 }
 
 @test "podman-remote: defaults" {

test/system/300-cli-parsing.bats
@@ -12,4 +12,18 @@ load helpers
     run_podman run --rm --label 'true="false"' $IMAGE true
 }
 
+@test "podman flag error" {
+    local name="podman"
+    if is_remote; then
+        name="podman-remote"
+    fi
+    run_podman 125 run -h
+    is "$output" "Error: flag needs an argument: 'h' in -h
+See '$name run --help'" "expected error output"
+
+    run_podman 125 bad --invalid
+    is "$output" "Error: unknown flag: --invalid
+See '$name --help'" "expected error output"
+}
+
 # vim: filetype=sh

commit 09c462d735082efa0489b66c66f0590460f4f00c

fix "podman -h" help output
`podman -h` currently returns an error:
`Error: pflag: help requested`

This bug was introduced in 44d037898ebc, the problem is that we wrap the
error and cobra lib checks with `==` for this one and not errors.Is().
I have a PR upstream to fix this but for now this also works.

Signed-off-by: Paul Holzinger <pholzing@redhat.com>

cmd/podman/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 
@@ -26,6 +27,7 @@ import (
 	"github.com/containers/storage/pkg/reexec"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 )
 
 func main() {
@@ -101,6 +103,13 @@ func parseCommands() *cobra.Command {
 }
 
 func flagErrorFuncfunc(c *cobra.Command, e error) error {
+	// cobra compares via == and not errors.Is so we cannot wrap that error.
+	// This is required to make podman -h work.
+	// This can be removed once https://github.com/spf13/cobra/pull/1730
+	// is merged and vendored into podman.
+	if errors.Is(e, pflag.ErrHelp) {
+		return e
+	}
 	e = fmt.Errorf("%w\nSee '%s --help'", e, c.CommandPath())
 	return e
 }

test/system/015-help.bats
@@ -199,7 +199,7 @@ function check_help() {
     check_help
 
     # Test for regression of #7273 (spurious "--remote" help on output)
-    for helpopt in help --help; do
+    for helpopt in help --help -h; do
         run_podman $helpopt
         is "${lines[0]}" "Manage pods, containers and images" \
            "podman $helpopt: first line of output"