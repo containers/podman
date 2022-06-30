c63101f27857c8012d1452de0fe6d1fead9a8108
Kir Kolyshkin <kolyshkin@gmail.com>
2022-06-30 13:47:46 -0700

    libpod/define/exec_codes: fix a comment
    Use complete sentences.
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- libpod/define/exec_codes.go
+++ libpod/define/exec_codes.go
@@ -11,8 +11,8 @@ const (
 	// ExecErrorCodeGeneric is the default error code to return from an exec session if libpod failed
 	// prior to calling the runtime
 	ExecErrorCodeGeneric = 125
-	// ExecErrorCodeCannotInvoke is the error code to return when the runtime fails to invoke a command
-	// an example of this can be found by trying to execute a directory:
+	// ExecErrorCodeCannotInvoke is the error code to return when the runtime fails to invoke a command.
+	// An example of this can be found by trying to execute a directory:
 	// `podman exec -l /etc`
 	ExecErrorCodeCannotInvoke = 126
 	// ExecErrorCodeNotFound is the error code to return when a command cannot be found
