8f797ab66166ec99d0eff0bb0aed997bb1876c5d
Doug Rabson <dfr@rabson.org>
2022-08-19 11:30:39 +0100

    libpod: Add definition of containerPlatformState for FreeBSD
    For FreeBSD, we need the name of the 'network jail' which is the parent
    of all containers in a pod. Having a separate jail for the network
    configuration also simplifies the implementation of CNI plugins so we
    use this pattern for solitary containers as well as pods.
    
    [NO NEW TESTS NEEDED]
    
    Signed-off-by: Doug Rabson <dfr@rabson.org>

--- libpod/boltdb_state_freebsd.go
+++ libpod/boltdb_state_freebsd.go
@@ -1,19 +1,17 @@
-//go:build !linux && !freebsd
-// +build !linux,!freebsd
+//go:build freebsd
+// +build freebsd
 
 package libpod
-
-import (
-	"errors"
-)
 
 // replaceNetNS handle network namespace transitions after updating a
 // container's state.
 func replaceNetNS(netNSPath string, ctr *Container, newState *ContainerState) error {
-	return errors.New("replaceNetNS not supported on this platform")
+	// On FreeBSD, we just record the network jail's name in our state.
+	newState.NetworkJail = netNSPath
+	return nil
 }
 
 // getNetNSPath retrieves the netns path to be stored in the database
 func getNetNSPath(ctr *Container) string {
-	return ""
+	return ctr.state.NetworkJail
 }
