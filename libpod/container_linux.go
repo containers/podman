8ade76e79b78bffd6eb055db35de0b3ad4475804
Doug Rabson <dfr@rabson.org>
2022-08-26 15:26:57 +0100

    libpod: Move networkDisable to container_linux.go
    This moves platform-specific details of the network implementation out
    of the generic file so that we can add the FreeBSD equivalent.
    
    [NO NEW TESTS NEEDED]
    
    Signed-off-by: Doug Rabson <dfr@rabson.org>

--- libpod/container_linux.go
+++ libpod/container_linux.go
@@ -5,6 +5,7 @@ package libpod
 
 import (
 	"github.com/containernetworking/plugins/pkg/ns"
+	spec "github.com/opencontainers/runtime-spec/specs-go"
 )
 
 type containerPlatformState struct {
@@ -12,4 +13,18 @@ type containerPlatformState struct {
 	// Will only be set if config.CreateNetNS is true, or the container was
 	// told to join another container's network namespace
 	NetNS ns.NetNS `json:"-"`
+}
+
+func networkDisabled(c *Container) (bool, error) {
+	if c.config.CreateNetNS {
+		return false, nil
+	}
+	if !c.config.PostConfigureNetNS {
+		for _, ns := range c.config.Spec.Linux.Namespaces {
+			if ns.Type == spec.NetworkNamespace {
+				return ns.Path == "", nil
+			}
+		}
+	}
+	return false, nil
 }
