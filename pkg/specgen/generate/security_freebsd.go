4781bc7fe7ef33c5d1f1892b2da274ed3d4bd6c8
Doug Rabson <dfr@rabson.org>
2022-08-26 11:17:32 +0100

    specgen/generate: Add support for FreeBSD
    [NO NEW TESTS NEEDED]
    
    Signed-off-by: Doug Rabson <dfr@rabson.org>

--- pkg/specgen/generate/security_freebsd.go
+++ pkg/specgen/generate/security_freebsd.go
@@ -1,11 +1,6 @@
-//go:build !linux && !freebsd
-// +build !linux,!freebsd
-
 package generate
 
 import (
-	"errors"
-
 	"github.com/containers/common/libimage"
 	"github.com/containers/common/pkg/config"
 	"github.com/containers/podman/v4/libpod"
@@ -16,9 +11,9 @@ import (
 // setLabelOpts sets the label options of the SecurityConfig according to the
 // input.
 func setLabelOpts(s *specgen.SpecGenerator, runtime *libpod.Runtime, pidConfig specgen.Namespace, ipcConfig specgen.Namespace) error {
-	return errors.New("unsupported setLabelOpts")
+	return nil
 }
 
 func securityConfigureGenerator(s *specgen.SpecGenerator, g *generate.Generator, newImage *libimage.Image, rtc *config.Config) error {
-	return errors.New("unsupported securityConfigureGenerator")
+	return nil
 }
