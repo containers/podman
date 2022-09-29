9850845a6255671b5e11fc4d742b73b6b74a73fb
Lokesh Mandvekar <lsm5@fedoraproject.org>
2022-09-29 15:44:56 -0400

    [v4.0-rhel] vendor buildah v1.24.6
    Includes a fix for CVE-2022-2990
    Ref: https://github.com/advisories/GHSA-fjm8-m7m6-2fjp
    
    Signed-off-by: Lokesh Mandvekar <lsm5@fedoraproject.org>

--- vendor/github.com/containers/buildah/define/types.go
+++ vendor/github.com/containers/buildah/define/types.go
@@ -29,7 +29,7 @@ const (
 	Package = "buildah"
 	// Version for the Package.  Bump version in contrib/rpm/buildah.spec
 	// too.
-	Version = "1.24.4"
+	Version = "1.24.6"
 
 	// DefaultRuntime if containers.conf fails.
 	DefaultRuntime = "runc"
