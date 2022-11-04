462ce32e66a103e0929eb2c58c8f50ee1d096860
Chris Evich <cevich@redhat.com>
2022-11-04 11:21:18 -0400

    GHA: Fix cirrus-cron scripts
    Lack of proper testing possibility for github actions and lack of
    script-testing by me, allowed several flaws through into 'main'.  Fix
    the problems and manually test the scripts to make sure they're working.
    
    Note: Also revert the stupid SHA-based action-pinning back to normal,
    human-readable version numbers.  The value of using SHAs in the name of
    improved "security" is real, but the value of human-readability and
    ease of maintenance is greater.
    
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- .github/actions/check_cirrus_cron/cron_failures.sh
+++ .github/actions/check_cirrus_cron/cron_failures.sh
@@ -32,14 +32,11 @@ EOF
 # https://cirrus-ci.com/explorer
 owner=$(cut -d '/' -f 1 <<<"$GITHUB_REPOSITORY")
 repo=$(cut -d '/' -f 2 <<<"$GITHUB_REPOSITORY")
-sed -i -r -e "s/@@OWNER@@/$owner/g" -e "s/@@REPO@@/$repo/g" ./artifacts/query_raw.json
-
-# Easier to debug in error-reply when query is compacted
-tr -d '\n' < ./artifacts/query_raw.json | tr -s ' ' | tee ./artifacts/query.json | \
-    jq --indent 4 --color-output .
+sed -r -e "s/@@OWNER@@/$owner/g" -e "s/@@REPO@@/$repo/g" \
+    ./artifacts/query_raw.json > ./artifacts/query.json
 
 if grep -q '@@' ./artifacts/query.json; then
-    err "Found unreplaced substitution token in raw query JSON"
+    err "Found unreplaced substitution token in query JSON"
 fi
 
 # The query should never ever return an empty-list, unless there are no cirrus-cron
@@ -47,7 +44,7 @@ fi
 # be running anyway.
 filt_head='.data.ownerRepository.cronSettings'
 
-gql $(./artifacts/query.json) "$filt_head" > ./artifacts/reply.json
+gql "$(<./artifacts/query.json)" "$filt_head" > ./artifacts/reply.json
 # e.x. reply.json
 # {
 #   "data": {
