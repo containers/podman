2bf94b764af9c70aa9af5046fbe8727ebe1c6034
Jason T. Greene <jason.greene@redhat.com>
2023-01-10 14:42:59 -0600

    Introduce pkg retry logic in win installer task
    Signed-off-by: Jason T. Greene <jason.greene@redhat.com>

--- contrib/cirrus/win-installer-install.ps1
+++ contrib/cirrus/win-installer-install.ps1
@@ -1,6 +1,25 @@
 # Update service is required for dotnet 3.5 (dep of wix)
 Set-Service -Name wuauserv -StartupType "Manual"
-choco install -y wixtoolset mingw golang archiver
-if ($LASTEXITCODE -ne 0) {
-    Exit 1
+function retryInstall {
+    param([Parameter(ValueFromRemainingArguments)] [string[]] $pkgs)
+
+    foreach ($pkg in $pkgs) {
+        for ($retries = 0; ; $retries++) {
+            if ($retries -gt 5) {
+                throw "Could not install package $pkg"
+            }
+
+            if ($pkg -match '(.[^\@]+)@(.+)') {
+                $pkg = @("--version", $Matches.2, $Matches.1)
+            }
+
+            choco install -y $pkg
+            if ($LASTEXITCODE -eq 0) {
+                break
+            }
+            Write-Output "Error installing, waiting before retry..."
+            Start-Sleep -Seconds 6
+        }
+    }
 }
+retryInstall wixtoolset mingw@11.2 golang archiver
