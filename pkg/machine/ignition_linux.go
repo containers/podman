12d70446f42f915ae16fba768c33922c8376ed7c
nabbisen <nabbisen@scqr.net>
2023-01-27 22:46:39 +0900

    fix #17244: use /etc/timezone where `timedatectl` is missing on Linux
    Signed-off-by: nabbisen <nabbisen@scqr.net>

--- pkg/machine/ignition_linux.go
+++ pkg/machine/ignition_linux.go
@@ -1,12 +1,17 @@
 package machine
 
 import (
+	"errors"
+	"os"
 	"os/exec"
 	"strings"
 )
 
 func getLocalTimeZone() (string, error) {
 	output, err := exec.Command("timedatectl", "show", "--property=Timezone").Output()
+	if errors.Is(err, exec.ErrNotFound) {
+		output, err = os.ReadFile("/etc/timezone")
+	}
 	if err != nil {
 		return "", err
 	}
