containers/podman

commit 68fbebfacc94bfc5a1358fb3bb4d8139a2c86f53

Kube Play: use passthrough as the default log-driver if service-container is set
Reasoning
---------
When the log-driver is passthrough, the journal socket is passed to the containers as-is which has two advantages:
1. journald can see who the actual sender of the log event is,
    rather than thinking everything comes from the conmon process
2. conmon will not have to copy all the log data

Code Changes
------------
If log-driver was not set by the user and service-container is set use
passthrough as the default log-driver

Update the system tests
- explicitly set logdriver in sdnotify and play tests
- podman-kube template test:  Verify the default log driver for service-container

Signed-off-by: Ygal Blum <ygal.blum@gmail.com>

cmd/podman/kube/play.go
@@ -73,6 +73,7 @@ var (
   podman play kube --creds user:password --seccomp-profile-root /custom/path apache.yml
   podman play kube https://example.com/nginx.yml`,
 	}
+	logDriverFlagName = "log-driver"
 )
 
 func init() {
@@ -116,7 +117,6 @@ func playFlags(cmd *cobra.Command) {
 	flags.IPSliceVar(&playOptions.StaticIPs, staticIPFlagName, nil, "Static IP addresses to assign to the pods")
 	_ = cmd.RegisterFlagCompletionFunc(staticIPFlagName, completion.AutocompleteNone)
 
-	logDriverFlagName := "log-driver"
 	flags.StringVar(&playOptions.LogDriver, logDriverFlagName, common.LogDriver(), "Logging driver for the container")
 	_ = cmd.RegisterFlagCompletionFunc(logDriverFlagName, common.AutocompleteLogDriver)
 
@@ -245,6 +245,15 @@ func play(cmd *cobra.Command, args []string) error {
 
 	if playOptions.Force && !playOptions.Down {
 		return errors.New("--force may be specified only with --down")
+	}
+
+	// When running under Systemd use passthrough as the default log-driver.
+	// When doing so, the journal socket is passed to the containers as-is which has two advantages:
+	// 1. journald can see who the actual sender of the log event is,
+	//    rather than thinking everything comes from the conmon process
+	// 2. conmon will not have to copy all the log data
+	if !cmd.Flags().Changed(logDriverFlagName) && playOptions.ServiceContainer {
+		playOptions.LogDriver = define.PassthroughLogging
 	}
 
 	reader, err := readerFromArg(args[0])

test/system/250-systemd.bats
@@ -414,6 +414,12 @@ EOF
     run_podman 125 container rm $service_container
     is "$output" "Error: container .* is the service container of pod(s) .* and cannot be removed without removing the pod(s)"
 
+    # Verify that the log-driver for the Pod's containers is passthrough
+    for name in "a" "b"; do
+        run_podman container inspect test_pod-${name} --format "{{.HostConfig.LogConfig.Type}}"
+        is $output "passthrough"
+    done
+
     # Add a simple `auto-update --dry-run` test here to avoid too much redundancy
     # with 255-auto-update.bats
     run_podman auto-update --dry-run --format "{{.Unit}},{{.Container}},{{.Image}},{{.Updated}},{{.Policy}}"

test/system/260-sdnotify.bats
@@ -225,7 +225,7 @@ EOF
     wait_for_file $_SOCAT_LOG
 
     # Will run until all containers have stopped.
-    run_podman play kube --service-container=true $yaml_source
+    run_podman play kube --service-container=true --log-driver journald $yaml_source
     run_podman container wait $service_container test_pod-test
 
     # Make sure the containers have the correct policy.
@@ -302,7 +302,7 @@ EOF
     # Run `play kube` in the background as it will wait for all containers to
     # send the READY=1 message.
     timeout --foreground -v --kill=10 60 \
-        $PODMAN play kube --service-container=true $yaml_source &>/dev/null &
+        $PODMAN play kube --service-container=true --log-driver journald $yaml_source &>/dev/null &
 
     # Wait for both containers to be running
     for i in $(seq 1 20); do

test/system/700-play.bats
@@ -126,7 +126,7 @@ EOF
     # Run `play kube` in the background as it will wait for the service
     # container to exit.
     timeout --foreground -v --kill=10 60 \
-        $PODMAN play kube --service-container=true $yaml_source &>/dev/null &
+        $PODMAN play kube --service-container=true --log-driver journald $yaml_source &>/dev/null &
 
     # Wait for the container to be running
     container_a=test_pod-test

commit c95978f600dc91071a2d0e832e2a680e9ff25e72

kube play: only enforce passthrough in Quadlet
Only enforce the passthrough log driver for Quadlet. Commit 68fbebf
introduced a regression on the `podman-kube@` template as `podman logs`
stopped working and settings from containers.conf were ignored.

Fixes: #17482
Signed-off-by: Valentin Rothberg <vrothberg@redhat.com>

cmd/podman/kube/play.go
@@ -251,15 +251,6 @@ func play(cmd *cobra.Command, args []string) error {
 		return errors.New("--force may be specified only with --down")
 	}
 
-	// When running under Systemd use passthrough as the default log-driver.
-	// When doing so, the journal socket is passed to the containers as-is which has two advantages:
-	// 1. journald can see who the actual sender of the log event is,
-	//    rather than thinking everything comes from the conmon process
-	// 2. conmon will not have to copy all the log data
-	if !cmd.Flags().Changed(logDriverFlagName) && playOptions.ServiceContainer {
-		playOptions.LogDriver = define.PassthroughLogging
-	}
-
 	reader, err := readerFromArg(args[0])
 	if err != nil {
 		return err

pkg/systemd/quadlet/quadlet.go
@@ -793,6 +793,9 @@ func ConvertKube(kube *parser.UnitFile, isUser bool) (*parser.UnitFile, error) {
 
 		// Use a service container
 		"--service-container=true",
+
+		// We want output to the journal, so use the log driver.
+		"--log-driver", "passthrough",
 	)
 
 	if err := handleUserRemap(kube, KubeGroup, execStart, isUser, false); err != nil {

test/e2e/quadlet/basic.kube
@@ -3,6 +3,7 @@
 ## assert-podman-final-args-regex .*/podman_test.*/quadlet/deployment.yml
 ## assert-podman-args "--replace"
 ## assert-podman-args "--service-container=true"
+## assert-podman-args "--log-driver" "passthrough"
 ## assert-podman-stop-args "kube"
 ## assert-podman-stop-args "down"
 ## assert-podman-stop-final-args-regex .*/podman_test.*/quadlet/deployment.yml

test/e2e/quadlet_test.go
@@ -393,6 +393,7 @@ var _ = Describe("quadlet system generator", func() {
 				"## assert-podman-final-args-regex .*/podman_test.*/quadlet/deployment.yml",
 				"## assert-podman-args \"--replace\"",
 				"## assert-podman-args \"--service-container=true\"",
+				"## assert-podman-args \"--log-driver\" \"passthrough\"",
 				"## assert-podman-stop-args \"kube\"",
 				"## assert-podman-stop-args \"down\"",
 				"## assert-podman-stop-final-args-regex .*/podman_test.*/quadlet/deployment.yml",
@@ -413,7 +414,7 @@ var _ = Describe("quadlet system generator", func() {
 				"Type=notify",
 				"NotifyAccess=all",
 				"SyslogIdentifier=%N",
-				fmt.Sprintf("ExecStart=/usr/local/bin/podman kube play --replace --service-container=true %s/deployment.yml", quadletDir),
+				fmt.Sprintf("ExecStart=/usr/local/bin/podman kube play --replace --service-container=true --log-driver passthrough %s/deployment.yml", quadletDir),
 				fmt.Sprintf("ExecStop=/usr/local/bin/podman kube down %s/deployment.yml", quadletDir),
 			}
 

test/system/250-systemd.bats
@@ -414,10 +414,10 @@ EOF
     run_podman 125 container rm $service_container
     is "$output" "Error: container .* is the service container of pod(s) .* and cannot be removed without removing the pod(s)"
 
-    # Verify that the log-driver for the Pod's containers is passthrough
+    # containers/podman/issues/17482: verify that the log-driver for the Pod's containers is NOT passthrough
     for name in "a" "b"; do
         run_podman container inspect test_pod-${name} --format "{{.HostConfig.LogConfig.Type}}"
-        is $output "passthrough"
+        assert $output != "passthrough"
     done
 
     # Add a simple `auto-update --dry-run` test here to avoid too much redundancy