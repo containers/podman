0498ce3a569900301591cf09dc84c86e0bc9fe81
Giuseppe Scrivano <gscrivan@redhat.com>
2023-03-02 22:18:25 +0100

    cmd: do not require userns for "version"
    Closes: https://github.com/containers/podman/issues/17657
    
    [NO NEW TESTS NEEDED]
    
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- pkg/rootless/rootless_linux.c
+++ pkg/rootless/rootless_linux.c
@@ -386,6 +386,7 @@ can_use_shortcut (char **argv)
 
       if (strcmp (argv[argc], "mount") == 0
           || strcmp (argv[argc], "machine") == 0
+          || strcmp (argv[argc], "version") == 0
           || strcmp (argv[argc], "context") == 0
           || strcmp (argv[argc], "search") == 0
           || (strcmp (argv[argc], "system") == 0 && argv[argc+1] && strcmp (argv[argc+1], "service") != 0))
