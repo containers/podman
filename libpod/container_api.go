34ff27b813bed9b0f9498a06bbf073c2b3292569
Doug Rabson <dfr@rabson.org>
2023-03-06 11:23:14 +0000

    libpod: avoid nil pointer dereference in (*Container).Cleanup
    On FreeBSD, c.config.Spec.Linux is not populated - in this case, we can
    assume that the container is not using a pid namespace.
    
    [NO NEW TESTS NEEDED]
    
    Signed-off-by: Doug Rabson <dfr@rabson.org>

--- libpod/container_api.go
+++ libpod/container_api.go
@@ -788,10 +788,12 @@ func (c *Container) Cleanup(ctx context.Context) error {
 
 	// make sure all the container processes are terminated if we are running without a pid namespace.
 	hasPidNs := false
-	for _, i := range c.config.Spec.Linux.Namespaces {
-		if i.Type == spec.PIDNamespace {
-			hasPidNs = true
-			break
+	if c.config.Spec.Linux != nil {
+		for _, i := range c.config.Spec.Linux.Namespaces {
+			if i.Type == spec.PIDNamespace {
+				hasPidNs = true
+				break
+			}
 		}
 	}
 	if !hasPidNs {
