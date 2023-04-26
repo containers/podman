e95e9be76ba45888c5a54d14d068d70a8b51aa91
huangyanfeng <huangyanfeng1992@gmail.com>
2023-04-26 09:46:55 +0800

    fix manifest annotate help
    [NO NEW TESTS NEEDED]
    Signed-off-by: huangyanfeng <huangyanfeng1992@gmail.com>

--- cmd/podman/manifest/manifest.go
+++ cmd/podman/manifest/manifest.go
@@ -16,7 +16,7 @@ var (
 		Example: `podman manifest add mylist:v1.11 image:v1.11-amd64
   podman manifest create localhost/list
   podman manifest inspect localhost/list
-  podman manifest annotate --annotation left=right mylist:v1.11 image:v1.11-amd64
+  podman manifest annotate --annotation left=right mylist:v1.11 sha256:15352d97781ffdf357bf3459c037be3efac4133dc9070c2dce7eca7c05c3e736
   podman manifest push mylist:v1.11 docker://quay.io/myuser/image:v1.11
   podman manifest remove mylist:v1.11 sha256:15352d97781ffdf357bf3459c037be3efac4133dc9070c2dce7eca7c05c3e736
   podman manifest rm mylist:v1.11`,
