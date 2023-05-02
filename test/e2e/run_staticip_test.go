502654580357a6b1d6577bc040784f562fd02a09
Paul Holzinger <pholzing@redhat.com>
2023-05-02 10:42:06 +0200

    test/e2e: do not remove CNI directory
    This is not safe at all when run in parallel, CNI needs that directory
    to detect duplicated ips and also stores other important network info in
    it. Removing it while container network is setup is not safe at all and
    could cause a lot of weird flakes.
    
    This "hack" was added in commit 55508c11 but provides zero context what
    this was supposed to fix so I don't know what the actual issue is or was.
    
    Fixes #18399
    
    Signed-off-by: Paul Holzinger <pholzing@redhat.com>

--- test/e2e/run_staticip_test.go
+++ test/e2e/run_staticip_test.go
@@ -28,8 +28,6 @@ var _ = Describe("Podman run with --ip flag", func() {
 		}
 		podmanTest = PodmanTestCreate(tempdir)
 		podmanTest.Setup()
-		// Clean up the CNI networks used by the tests
-		os.RemoveAll("/var/lib/cni/networks/podman")
 	})
 
 	AfterEach(func() {
