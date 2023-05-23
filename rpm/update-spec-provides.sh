6652633ce55e36cecb42a9569666571ed1eb5671
Lokesh Mandvekar <lsm5@fedoraproject.org>
2023-05-23 09:15:03 -0400

    [CI:BUILD] Packit: add jobs for downstream Fedora package builds
    Get rid of `podman.spec.rpkg` in favour of
    `rpm/podman.spec` which gets synced with fedora dist-git on every
    upstream release. The version in the new spec file is set to `0` by
    default and gets updated by packit automatically on every packit task.
    
    For local manual rpm builds using the spec, the helper script in the
    `rpm/` subdir will update the Version field with the latest version
    found in the upstream repo.
    
    Packit will automatically create a PR on fedora dist-git on every new
    upstream release. A sample PR will look like:
    https://src.fedoraproject.org/rpms/container-selinux/pull-request/10#
    
    A dry run for this can be triggered using:
    `$ packit propose-downstream --local-content`
    
    To run this command locally, you would need to have your packit
    user-configuration-file set.
    Ref: https://packit.dev/docs/configuration/#user-configuration-file
    
    along with a fedora api key created at:
    https://src.fedoraproject.org/settings#nav-api-tab with sufficient ACLs.
    
    Also includes a revised `package` Makefile target which will build rpms
    using `rpm/podman.spec`. Fixes: #18421.
    
    [NO NEW TESTS NEEDED]
    
    Signed-off-by: Lokesh Mandvekar <lsm5@fedoraproject.org>
    (cherry picked from commit 6003dca9c78f6dde90f269ff7cac78e14735e157)
    Signed-off-by: Lokesh Mandvekar <lsm5@fedoraproject.org>

---
+++ rpm/update-spec-provides.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# This script will update the goimports in the rpm spec for downstream fedora
+# packaging, via the `propose-downstream` packit action.
+# The goimports don't need to be present upstream.
+
+SPEC_FILE=$(pwd)/podman.spec
+
+sed -i '/Provides: bundled(golang.*/d' $SPEC_FILE
+
+GO_IMPORTS=$(golist --imported --package-path github.com/containers/podman --skip-self | sort -u | xargs -I{} echo "Provides: bundled(golang({}))")
+
+awk -v r="$GO_IMPORTS" '/^# vendored libraries/ {print; print r; next} 1' $SPEC_FILE > temp && mv temp $SPEC_FILE
