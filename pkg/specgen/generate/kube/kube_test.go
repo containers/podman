6817e0ae3813cbd3d26805b1477f7c7398fdae74
Hedayat Vatankhah <hedayat.fwd@gmail.com>
2023-07-09 15:19:56 +0330

    Add support for using port names in Kubernetes health probes
    Closes #18645
    
    Signed-off-by: Hedayat Vatankhah <hedayat.fwd@gmail.com>

--- pkg/specgen/generate/kube/kube_test.go
+++ pkg/specgen/generate/kube/kube_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	v1 "github.com/containers/podman/v4/pkg/k8s.io/api/core/v1"
+	"github.com/containers/podman/v4/pkg/k8s.io/apimachinery/pkg/util/intstr"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -38,4 +39,27 @@ func TestParseMountPathRO(t *testing.T) {
 	_, options, err = parseMountPath("/to", false, nil)
 	assert.NoError(t, err)
 	assert.NotContains(t, options, "ro")
+}
+
+func TestGetPortNumber(t *testing.T) {
+	portSpec := intstr.IntOrString{Type: intstr.Int, IntVal: 3000, StrVal: "myport"}
+	cp1 := v1.ContainerPort{Name: "myport", ContainerPort: 4000}
+	cp2 := v1.ContainerPort{Name: "myport2", ContainerPort: 5000}
+	i, e := getPortNumber(portSpec, []v1.ContainerPort{cp1, cp2})
+	assert.NoError(t, e)
+	assert.Equal(t, i, int(portSpec.IntVal))
+
+	portSpec.Type = intstr.String
+	i, e = getPortNumber(portSpec, []v1.ContainerPort{cp1, cp2})
+	assert.NoError(t, e)
+	assert.Equal(t, i, 4000)
+
+	portSpec.StrVal = "not_valid"
+	_, e = getPortNumber(portSpec, []v1.ContainerPort{cp1, cp2})
+	assert.Error(t, e)
+
+	portSpec.StrVal = "6000"
+	i, e = getPortNumber(portSpec, []v1.ContainerPort{cp1, cp2})
+	assert.NoError(t, e)
+	assert.Equal(t, i, 6000)
 }
