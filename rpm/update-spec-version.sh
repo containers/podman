145748d1f10990568a769282e23790bedfb0a2c8
Lokesh Mandvekar <lsm5@fedoraproject.org>
2023-07-13 08:45:02 -0400

    Makefile: `package` -> `rpm`
    package and package-install targets have been renamed
    to rpm and rpm-install respectively for clarity.
    
    `make rpm` will now build rpm using HEAD.
    
    Resolves: #18817
    
    [NO NEW TESTS NEEDED]
    
    Signed-off-by: Lokesh Mandvekar <lsm5@fedoraproject.org>

--- rpm/update-spec-version.sh
+++ rpm/update-spec-version.sh
@@ -4,12 +4,17 @@
 # default. Useful for local manual rpm builds where the Version needs to be set
 # correctly.
 
-set -e
+set -eox pipefail
 
-# Script is run from git root directory
-SPEC_FILE=rpm/podman.spec
+PACKAGE=podman
+SPEC_FILE=$PACKAGE.spec
+GIT_DESCRIBE=$(git describe)
+VERSION=$(echo $GIT_DESCRIBE | sed -e 's/^v//' -e 's/-/~/g')
 
-LATEST_TAG=$(git tag --sort=creatordate | tail -1)
-LATEST_VERSION=$(echo $LATEST_TAG | sed -e 's/^v//')
+# Update spec file to use local changes
+sed -i "s/^Version:.*/Version: $VERSION/" $SPEC_FILE
+sed -i "s/^Source0:.*/Source0: $PACKAGE-$GIT_DESCRIBE.tar.gz/" $SPEC_FILE
+sed -i "s/^%autosetup.*/%autosetup -Sgit -n %{name}-$GIT_DESCRIBE/" $SPEC_FILE
 
-sed -i "s/^Version:.*/Version: $LATEST_VERSION/" $SPEC_FILE
+# Generate Source0 archive from HEAD
+(cd .. && git archive --format=tar.gz --prefix=$PACKAGE-$GIT_DESCRIBE/ HEAD -o rpm/$PACKAGE-$GIT_DESCRIBE.tar.gz)
