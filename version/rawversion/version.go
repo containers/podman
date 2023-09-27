4d15c88a932d1854edd92f5ff34ba980376a800f
Lokesh Mandvekar <lsm5@fedoraproject.org>
2023-09-27 09:02:09 -0400

    switch version to 4.7.1-dev
    [NO NEW TESTS NEEDED]
    
    Signed-off-by: Lokesh Mandvekar <lsm5@fedoraproject.org>

--- version/rawversion/version.go
+++ version/rawversion/version.go
@@ -7,4 +7,4 @@ package rawversion
 //
 // NOTE: remember to bump the version at the top of the top-level README.md
 // file when this is bumped.
-const RawVersion = "4.7.0"
+const RawVersion = "4.7.1-dev"
