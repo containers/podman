1ebd507fbfd8bb64315a4632c36860291d253bf1
Brent Baude <bbaude@redhat.com>
2023-12-05 12:58:17 -0600

    Fix regression in e2e machine test suite
    A simple regression was introduced to the test suite that overrode the
    default image for hyperv testing.
    
    Signed-off-by: Brent Baude <bbaude@redhat.com>

--- contrib/cirrus/win-podman-machine-main.ps1
+++ contrib/cirrus/win-podman-machine-main.ps1
@@ -4,6 +4,13 @@
 
 Set-Location "$ENV:CIRRUS_WORKING_DIR\repo"
 
+$GvTargetDir = "C:\Program Files\Redhat\Podman\"
+
+#Expand-Archive -Path "podman-remote-release-windows_amd64.zip" -DestinationPath $GvTargetDir
+
+New-Item -Path $GvTargetDir -ItemType "directory"
+Copy-Item "bin/windows/gvproxy.exe" -Destination $GvTargetDir
+
 Write-Host "Saving selection of CI env. vars."
 # Env. vars will not pass through win-sess-launch.ps1
 Get-ChildItem -Path "Env:\*" -include @("PATH", "Chocolatey*", "CIRRUS*", "TEST_*", "CI_*") `
