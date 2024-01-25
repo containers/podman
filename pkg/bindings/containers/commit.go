2d8231efe8284cd1c2ed6c0c48ad25fb0237e368
Giuseppe Scrivano <gscrivan@redhat.com>
2024-01-25 12:53:24 +0100

    bindings: containers/commit uses directly Docker type
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- pkg/bindings/containers/commit.go
+++ pkg/bindings/containers/commit.go
@@ -11,26 +11,26 @@ import (
 
 	"github.com/containers/podman/v4/pkg/bindings"
 	"github.com/containers/podman/v4/pkg/bindings/images"
-	"github.com/containers/podman/v4/pkg/domain/entities"
 	"github.com/containers/storage/pkg/regexp"
+	dockerAPI "github.com/docker/docker/api/types"
 )
 
 var iidRegex = regexp.Delayed(`^[0-9a-f]{12}`)
 
 // Commit creates a container image from a container.  The container is defined by nameOrID.  Use
 // the CommitOptions for finer grain control on characteristics of the resulting image.
-func Commit(ctx context.Context, nameOrID string, options *CommitOptions) (entities.IDResponse, error) {
+func Commit(ctx context.Context, nameOrID string, options *CommitOptions) (dockerAPI.IDResponse, error) {
 	if options == nil {
 		options = new(CommitOptions)
 	}
-	id := entities.IDResponse{}
+	id := dockerAPI.IDResponse{}
 	conn, err := bindings.GetClient(ctx)
 	if err != nil {
 		return id, err
 	}
 	params, err := options.ToParams()
 	if err != nil {
-		return entities.IDResponse{}, err
+		return dockerAPI.IDResponse{}, err
 	}
 	params.Set("container", nameOrID)
 	var requestBody io.Reader
