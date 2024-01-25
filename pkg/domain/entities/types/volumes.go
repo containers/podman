668d517af98e1e01c3c54dccef8d406f5db242b4
Giuseppe Scrivano <gscrivan@redhat.com>
2024-01-25 15:37:55 +0100

    bindings: volumes uses entities/types
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- pkg/domain/entities/types/volumes.go
+++ pkg/domain/entities/types/volumes.go
@@ -4,6 +4,22 @@ import (
 	"github.com/containers/podman/v4/libpod/define"
 )
 
+// swagger:model
+type VolumeCreateOptions struct {
+	// New volume's name. Can be left blank
+	Name string `schema:"name"`
+	// Volume driver to use
+	Driver string `schema:"driver"`
+	// User-defined key/value metadata. Provided for compatibility
+	Label map[string]string `schema:"label"`
+	// User-defined key/value metadata. Preferred field, will override Label
+	Labels map[string]string `schema:"labels"`
+	// Mapping of driver options and values.
+	Options map[string]string `schema:"opts"`
+	// Ignore existing volumes
+	IgnoreIfExists bool `schema:"ignoreIfExist"`
+}
+
 type VolumeRmReport struct {
 	Err error
 	Id  string //nolint:revive,stylecheck
--- pkg/domain/entities/volumes.go
+++ pkg/domain/entities/volumes.go
@@ -8,20 +8,7 @@ import (
 
 // VolumeCreateOptions provides details for creating volumes
 // swagger:model
-type VolumeCreateOptions struct {
-	// New volume's name. Can be left blank
-	Name string `schema:"name"`
-	// Volume driver to use
-	Driver string `schema:"driver"`
-	// User-defined key/value metadata. Provided for compatibility
-	Label map[string]string `schema:"label"`
-	// User-defined key/value metadata. Preferred field, will override Label
-	Labels map[string]string `schema:"labels"`
-	// Mapping of driver options and values.
-	Options map[string]string `schema:"opts"`
-	// Ignore existing volumes
-	IgnoreIfExists bool `schema:"ignoreIfExist"`
-}
+type VolumeCreateOptions = types.VolumeCreateOptions
 
 type VolumeConfigResponse = types.VolumeConfigResponse
 
