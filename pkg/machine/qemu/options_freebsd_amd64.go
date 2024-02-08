9f38b22152891f3320d3cc118af4ffb60c02e6a2
Paul Holzinger <pholzing@redhat.com>
2024-02-08 13:35:58 +0100

    pkg/machine: make it build for freebsd
    This is completely untested as I do not have access to a freebsd system
    but it compiles and changes look simple enough to assume it works.
    
    [NO NEW TESTS NEEDED]
    
    Signed-off-by: Paul Holzinger <pholzing@redhat.com>

--- pkg/machine/qemu/options_freebsd_amd64.go
+++ pkg/machine/qemu/options_freebsd_amd64.go
@@ -4,15 +4,7 @@ var (
 	QemuCommand = "qemu-system-x86_64"
 )
 
-func (v *MachineVM) addArchOptions(_ *setNewMachineCMDOpts) []string {
+func (v *QEMUStubber) addArchOptions(_ *setNewMachineCMDOpts) []string {
 	opts := []string{"-machine", "q35,accel=hvf:tcg", "-cpu", "host"}
 	return opts
-}
-
-func (v *MachineVM) prepare() error {
-	return nil
-}
-
-func (v *MachineVM) archRemovalFiles() []string {
-	return []string{}
 }
