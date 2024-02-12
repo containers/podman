87947761ed81105de12b5c0a7e2c5ac249efdca3
Ashley Cui <acui@redhat.com>
2024-02-12 16:18:06 -0500

    [CI:BUILD] Add VFKit into pkginstaller, remove QEMU
    Do not build and install QEMU in the pkginstaller. Instead, build, sign, and install VFKit.
    
    Signed-off-by: Ashley Cui <acui@redhat.com>

--- contrib/pkginstaller/package.sh
+++ contrib/pkginstaller/package.sh
@@ -7,10 +7,9 @@ OUTPUT=$1
 CODESIGN_IDENTITY=${CODESIGN_IDENTITY:-mock}
 PRODUCTSIGN_IDENTITY=${PRODUCTSIGN_IDENTITY:-mock}
 NO_CODESIGN=${NO_CODESIGN:-0}
-HELPER_BINARIES_DIR="/opt/podman/qemu/bin"
+HELPER_BINARIES_DIR="/opt/podman/bin"
 
 binDir="${BASEDIR}/root/podman/bin"
-qemuBinDir="${BASEDIR}/root/podman/qemu/bin"
 
 version=$(cat "${BASEDIR}/VERSION")
 arch=$(cat "${BASEDIR}/ARCH")
@@ -36,32 +35,6 @@ function sign() {
   codesign --deep --sign "${CODESIGN_IDENTITY}" --options runtime --timestamp --force ${opts} "$1"
 }
 
-function signQemu() {
-  if [ "${NO_CODESIGN}" -eq "1" ]; then
-    return
-  fi
-
-  local qemuArch="${arch}"
-  if [ "${qemuArch}" = amd64 ]; then
-      qemuArch=x86_64
-  fi
-
-  # sign the files inside /opt/podman/qemu/lib
-  libs=$(find "${BASEDIR}"/root/podman/qemu/lib -depth -name "*.dylib" -or -type f -perm +111)
-  echo "${libs}" | xargs -t -I % codesign --deep --sign "${CODESIGN_IDENTITY}" --options runtime --timestamp --force % || true
-
-  # sign the files inside /opt/podman/qemu/bin except qemu-system-*
-  bins=$(find "${BASEDIR}"/root/podman/qemu/bin -depth -type f -perm +111 ! -name "qemu-system-${qemuArch}")
-  echo "${bins}" | xargs -t -I % codesign --deep --sign "${CODESIGN_IDENTITY}" --options runtime --timestamp --force  % || true
-
-  # sign the qemu-system-* binary
-  # need to remove any extended attributes, otherwise codesign complains:
-  # qemu-system-aarch64: resource fork, Finder information, or similar detritus not allowed
-  xattr -cr "${qemuBinDir}/qemu-system-${qemuArch}"
-  codesign --deep --sign "${CODESIGN_IDENTITY}" --options runtime --timestamp --force \
-    --entitlements "${BASEDIR}/hvf.entitlements" "${qemuBinDir}/qemu-system-${qemuArch}"
-}
-
 goArch="${arch}"
 if [ "${goArch}" = aarch64 ]; then
   goArch=arm64
@@ -70,8 +43,8 @@ fi
 build_podman "../../../../"
 sign "${binDir}/podman"
 sign "${binDir}/gvproxy"
+sign "${binDir}/vfkit"
 sign "${binDir}/podman-mac-helper"
-signQemu
 
 pkgbuild --identifier com.redhat.podman --version "${version}" \
   --scripts "${BASEDIR}/scripts" \
