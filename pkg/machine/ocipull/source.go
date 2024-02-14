9abc04232088c8c2a9b84d89622f22c43a6561cb
Brent Baude <bbaude@redhat.com>
2024-02-14 12:31:16 -0600

    Fix small bug in ocipull
    This PR fixes a small bug in pulling disk artifacts where the machine os
    was accidently being set to GOOS instead of "linux".  Also removed the
    manifest type verification because it served no purpose.
    
    [NO NEW TESTS NEEDED]
    
    Signed-off-by: Brent Baude <bbaude@redhat.com>

--- pkg/machine/ocipull/source.go
+++ pkg/machine/ocipull/source.go
@@ -5,9 +5,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"strings"
 
-	"github.com/containers/image/v5/docker"
 	"github.com/containers/image/v5/manifest"
 	"github.com/containers/image/v5/oci/layout"
 	"github.com/containers/image/v5/types"
@@ -15,26 +13,6 @@ import (
 	specV1 "github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/sirupsen/logrus"
 )
-
-// readManifestFromImageSource reads the manifest from the specified image
-// source.  Note that the manifest is expected to be an OCI v1 manifest.
-func readManifestFromImageSource(ctx context.Context, src types.ImageSource) (*specV1.Manifest, *digest.Digest, int64, error) {
-	rawData, mimeType, err := src.GetManifest(ctx, nil)
-	if err != nil {
-		return nil, nil, -1, err
-	}
-	if mimeType != specV1.MediaTypeImageManifest {
-		return nil, nil, -1, fmt.Errorf("image %q is of type %q (expected: %q)", strings.TrimPrefix(src.Reference().StringWithinTransport(), "//"), mimeType, specV1.MediaTypeImageManifest)
-	}
-
-	mannyFest := specV1.Manifest{}
-	if err := json.Unmarshal(rawData, &mannyFest); err != nil {
-		return nil, nil, -1, fmt.Errorf("reading manifest: %w", err)
-	}
-
-	manifestDigest := digest.FromBytes(rawData)
-	return &mannyFest, &manifestDigest, int64(len(rawData)), nil
-}
 
 func GetLocalBlob(ctx context.Context, path string) (*types.BlobInfo, error) {
 	ociRef, err := layout.ParseReference(path)
@@ -64,27 +42,6 @@ func GetLocalBlob(ctx context.Context, path string) (*types.BlobInfo, error) {
 	}
 	fmt.Println(blobs[0].Digest.Hex())
 	return &blobs[0], nil
-}
-
-func GetRemoteManifest(ctx context.Context, dest string) (*specV1.Manifest, error) { //nolint:unused
-	ref, err := docker.ParseReference(fmt.Sprintf("//%s", dest))
-	if err != nil {
-		return nil, err
-	}
-
-	imgSrc, err := ref.NewImage(ctx, &types.SystemContext{})
-	if err != nil {
-		return nil, err
-	}
-
-	b, _, err := imgSrc.Manifest(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	remoteManifest := specV1.Manifest{}
-	err = json.Unmarshal(b, &remoteManifest)
-	return &remoteManifest, err
 }
 
 func GetDiskArtifactReference(ctx context.Context, imgSrc types.ImageSource, opts *DiskArtifactOpts) (digest.Digest, error) {
