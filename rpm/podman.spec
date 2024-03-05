containers/podman

commit 3efaffae43cb165092dedf2c8d20a9c8e59194df

New command: podmansh
This commit creates a new command `podmansh` command which can be used by
administrators to provide a confined shell to their users.

The user will only have access to the volumes and capabilities for that
user.

Co-authored-by: Paul Holzinger <pholzing@redhat.com>
Co-authored-by: Daniel Walsh <dwalsh@redhat.com>
Co-authored-by: Petr Lautrbach <lautrbach@redhat.com>
Co-authored-by: Ed Santiago <santiago@redhat.com>

Signed-off-by: Lokesh Mandvekar <lsm5@fedoraproject.org>

rpm/podman.spec
@@ -241,6 +241,19 @@ It is based on the network stack of gVisor. Compared to libslirp,
 gvisor-tap-vsock brings a configurable DNS server and
 dynamic port forwarding.
 
+%package -n %{name}sh
+Summary: Confined login and user shell using %{name}
+Requires: %{name} = %{epoch}:%{version}-%{release}
+Provides: %{name}-shell = %{epoch}:%{version}-%{release}
+Provides: %{name}-%{name}sh = %{epoch}:%{version}-%{release}
+
+%description -n %{name}sh
+%{name}sh provides a confined login and user shell with access to volumes and
+capabilities specified in user quadlets.
+
+It is a symlink to %{_bindir}/%{name} and execs into the `%{name}sh` container
+when `%{_bindir}/%{name}sh is set as a login shell or set as os.Args[0].
+
 %prep
 %autosetup -Sgit -n %{name}-%{version}
 sed -i 's;@@PODMAN@@\;$(BINDIR);@@PODMAN@@\;%{_bindir};' Makefile
@@ -413,6 +426,11 @@ cp -pav test/system %{buildroot}/%{_datadir}/%{name}/test/
 %dir %{_libexecdir}/%{name}
 %{_libexecdir}/%{name}/gvproxy
 %{_libexecdir}/%{name}/gvforwarder
+
+%files -n %{name}sh
+%license LICENSE
+%doc README.md CONTRIBUTING.md install.md transfer.md
+%{_bindir}/%{name}sh
 
 %changelog
 %if %{with changelog}

Makefile
@@ -215,7 +215,7 @@ binaries: podman podman-remote ## Build podman and podman-remote binaries
 else ifneq (, $(findstring $(GOOS),darwin windows))
 binaries: podman-remote ## Build podman-remote (client) only binaries
 else
-binaries: podman podman-remote rootlessport quadlet ## Build podman, podman-remote and rootlessport binaries quadlet
+binaries: podman podman-remote podmansh rootlessport quadlet ## Build podman, podman-remote and rootlessport binaries quadlet
 endif
 
 # Extract text following double-# for targets, as their description for
@@ -407,6 +407,12 @@ bin/rootlessport: $(SOURCES) go.mod go.sum
 
 .PHONY: rootlessport
 rootlessport: bin/rootlessport
+
+# podmansh calls `podman exec` into the `podmansh` container when used as
+# os.Args[0] and is intended to be set as a login shell for users.
+# Run: `man 1 podmansh` for details.
+podmansh: bin/podman
+	if [ ! -f bin/podmansh ]; then ln -s podman bin/podmansh; fi
 
 ###
 ### Secondary binary-build targets
@@ -820,6 +826,7 @@ install.remote:
 install.bin:
 	install ${SELINUXOPT} -d -m 755 $(DESTDIR)$(BINDIR)
 	install ${SELINUXOPT} -m 755 bin/podman $(DESTDIR)$(BINDIR)/podman
+	ln -sfr $(DESTDIR)$(BINDIR)/podman $(DESTDIR)$(BINDIR)/podmansh
 	test -z "${SELINUXOPT}" || chcon --verbose --reference=$(DESTDIR)$(BINDIR)/podman bin/podman
 	install ${SELINUXOPT} -d -m 755 $(DESTDIR)$(LIBEXECPODMAN)
 ifneq ($(shell uname -s),FreeBSD)

cmd/podman/containers/exec.go
@@ -2,10 +2,12 @@ package containers
 
 import (
 	"bufio"
+	"context"
 	"errors"
 	"fmt"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/containers/common/pkg/completion"
 	"github.com/containers/podman/v4/cmd/podman/common"
@@ -84,6 +86,10 @@ func execFlags(cmd *cobra.Command) {
 	flags.StringVarP(&execOpts.WorkDir, workdirFlagName, "w", "", "Working directory inside the container")
 	_ = cmd.RegisterFlagCompletionFunc(workdirFlagName, completion.AutocompleteDefault)
 
+	waitFlagName := "wait"
+	flags.Int32(waitFlagName, 0, "Total seconds to wait for container to start")
+	_ = flags.MarkHidden(waitFlagName)
+
 	if registry.IsRemote() {
 		_ = flags.MarkHidden("preserve-fds")
 	}
@@ -104,7 +110,7 @@ func init() {
 	validate.AddLatestFlag(containerExecCommand, &execOpts.Latest)
 }
 
-func exec(_ *cobra.Command, args []string) error {
+func exec(cmd *cobra.Command, args []string) error {
 	var nameOrID string
 
 	if len(args) == 0 && !execOpts.Latest {
@@ -138,6 +144,16 @@ func exec(_ *cobra.Command, args []string) error {
 		}
 	}
 
+	if cmd.Flags().Changed("wait") {
+		seconds, err := cmd.Flags().GetInt32("wait")
+		if err != nil {
+			return err
+		}
+		if err := execWait(nameOrID, seconds); err != nil {
+			return err
+		}
+	}
+
 	if !execDetach {
 		streams := define.AttachStreams{}
 		streams.OutputStream = os.Stdout
@@ -160,4 +176,38 @@ func exec(_ *cobra.Command, args []string) error {
 	}
 	fmt.Println(id)
 	return nil
+}
+
+func execWait(ctr string, seconds int32) error {
+	maxDuration := time.Duration(seconds) * time.Second
+	interval := 100 * time.Millisecond
+
+	ctx, cancel := context.WithTimeout(registry.Context(), maxDuration)
+	defer cancel()
+
+	cond, err := define.StringToContainerStatus("running")
+	if err != nil {
+		return err
+	}
+	waitOptions.Condition = append(waitOptions.Condition, cond)
+
+	startTime := time.Now()
+	for time.Since(startTime) < maxDuration {
+		_, err = registry.ContainerEngine().ContainerWait(ctx, []string{ctr}, waitOptions)
+		if err == nil {
+			return nil
+		}
+
+		if !errors.Is(err, define.ErrNoSuchCtr) {
+			return err
+		}
+
+		interval *= 2
+		since := time.Since(startTime)
+		if since+interval > maxDuration {
+			interval = maxDuration - since
+		}
+		time.Sleep(interval)
+	}
+	return define.ErrCanceled
 }

cmd/podman/main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 
 	_ "github.com/containers/podman/v4/cmd/podman/completion"
-	_ "github.com/containers/podman/v4/cmd/podman/containers"
 	_ "github.com/containers/podman/v4/cmd/podman/generate"
 	_ "github.com/containers/podman/v4/cmd/podman/healthcheck"
 	_ "github.com/containers/podman/v4/cmd/podman/images"
@@ -27,6 +28,7 @@ import (
 	"github.com/containers/storage/pkg/reexec"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"golang.org/x/term"
 )
 
 func main() {
@@ -34,6 +36,20 @@ func main() {
 		// We were invoked with a different argv[0] indicating that we
 		// had a specific job to do as a subprocess, and it's done.
 		return
+	}
+
+	if filepath.Base(os.Args[0]) == registry.PodmanSh ||
+		(len(os.Args[0]) > 0 && filepath.Base(os.Args[0][1:]) == registry.PodmanSh) {
+		shell := strings.TrimPrefix(os.Args[0], "-")
+		args := []string{shell, "exec", "-i", "--wait", "10"}
+		if term.IsTerminal(0) || term.IsTerminal(1) || term.IsTerminal(2) {
+			args = append(args, "-t")
+		}
+		args = append(args, registry.PodmanSh, "/bin/sh")
+		if len(os.Args) > 1 {
+			args = append(args, os.Args[1:]...)
+		}
+		os.Args = args
 	}
 
 	rootCmd = parseCommands()
@@ -59,7 +75,7 @@ func parseCommands() *cobra.Command {
 				c.Command.RunE = func(cmd *cobra.Command, args []string) error {
 					return fmt.Errorf("cannot use command %q with the %s podman client", cmd.CommandPath(), client)
 				}
-				// turn of flag parsing to make we do not get flag errors
+				// turn off flag parsing to make we do not get flag errors
 				c.Command.DisableFlagParsing = true
 
 				// mark command as hidden so it is not shown in --help

cmd/podman/registry/remote.go
@@ -2,6 +2,8 @@ package registry
 
 import (
 	"os"
+	"path/filepath"
+	"strings"
 	"sync"
 
 	"github.com/containers/podman/v4/pkg/domain/entities"
@@ -15,9 +17,18 @@ var remoteFromCLI = struct {
 	sync  sync.Once
 }{}
 
+const PodmanSh = "podmansh"
+
 // IsRemote returns true if podman was built to run remote or --remote flag given on CLI
 // Use in init() functions as an initialization check
 func IsRemote() bool {
+	// remote conflicts with podmansh in how the `-c` option gets parsed
+	// This is noticeable if a user with shell set to podmansh were to execute
+	// a command using ssh like so:
+	// ssh user@host id
+	if strings.HasSuffix(filepath.Base(os.Args[0]), PodmanSh) {
+		return false
+	}
 	remoteFromCLI.sync.Do(func() {
 		remote := false
 		if _, ok := os.LookupEnv("CONTAINER_HOST"); ok {

test/e2e/exec_test.go
@@ -548,4 +548,28 @@ RUN useradd -u 1000 auser`, fedoraMinimal)
 		session.WaitWithDefaultTimeout()
 		Expect(session.OutputToString()).To(Not(ContainSubstring(secretsString)))
 	})
+
+	It("podman exec --wait 2 seconds on bogus container", func() {
+		SkipIfRemote("not supported for --wait")
+		session := podmanTest.Podman([]string{"exec", "--wait", "2", "1234"})
+		session.WaitWithDefaultTimeout()
+		Expect(session).Should(Exit(125))
+		Expect(session.ErrorToString()).To(Equal("Error: cancelled by user"))
+	})
+
+	It("podman exec --wait 5 seconds for started container", func() {
+		SkipIfRemote("not supported for --wait")
+		ctrName := "waitCtr"
+
+		session := podmanTest.Podman([]string{"exec", "--wait", "5", ctrName, "whoami"})
+
+		session2 := podmanTest.Podman([]string{"run", "-d", "--name", ctrName, ALPINE, "top"})
+		session2.WaitWithDefaultTimeout()
+
+		session.Wait(6)
+
+		Expect(session2).Should(Exit(0))
+		Expect(session).Should(Exit(0))
+		Expect(session.OutputToString()).To(Equal("root"))
+	})
 })

test/system/075-exec.bats
@@ -127,4 +127,18 @@ load helpers
     run_podman rm -t 0 -f $cid
 }
 
+@test "podman exec --wait" {
+    skip_if_remote "test is meaningless over remote"
+
+    # wait on bogus container
+    run_podman 125 exec --wait 5 "bogus_container" echo hello
+    assert "$output" = "Error: cancelled by user"
+
+    run_podman create --name "wait_container" $IMAGE top
+    run_podman 255 exec --wait 5 "wait_container" echo hello
+    assert "$output" = "Error: can only create exec sessions on running containers: container state improper"
+
+    run_podman rm -f wait_container
+}
+
 # vim: filetype=sh

commit 835cfbc05f873e2ac30e9aa03b0cea7e7b9b1ced

[CI:BUILD] rpm: Put the podmansh(1) manual in the podmansh sub-package
... and fix the style.

Currently, the podmansh(1) manual is shipped by the podman package.  It
makes one wonder where the binary is, since they can read the manual.

[NO NEW TESTS NEEDED]

Fixes: 3efaffae43cb1650 ("New command: podmansh")

Signed-off-by: Debarshi Ray <rishi@fedoraproject.org>

rpm/podman.spec
@@ -263,8 +263,8 @@ PODMAN_VERSION=%{version} %{__make} DESTDIR=%{buildroot} PREFIX=%{_prefix} ETCDI
 sed -i 's;%{buildroot};;g' %{buildroot}%{_bindir}/docker
 
 # do not include docker and podman-remote man pages in main package
-for file in `find %{buildroot}%{_mandir}/man[15] -type f | sed "s,%{buildroot},," | grep -v -e remote -e docker`; do
-    echo "$file*" >> podman.file-list
+for file in `find %{buildroot}%{_mandir}/man[15] -type f | sed "s,%{buildroot},," | grep -v -e %{name}sh.1 -e remote -e docker`; do
+    echo "$file*" >> %{name}.file-list
 done
 
 rm -f %{buildroot}%{_mandir}/man5/docker*.5
@@ -319,6 +319,7 @@ cp -pav test/system %{buildroot}/%{_datadir}/%{name}/test/
 
 %files -n %{name}sh
 %{_bindir}/%{name}sh
+%{_mandir}/man1/%{name}sh.1*
 
 %changelog
 %if %{defined autochangelog}