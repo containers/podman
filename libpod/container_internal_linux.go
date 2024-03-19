2967eb9a264cd36dcb3042d4e0b1a38b345f9df8
Paul Holzinger <pholzing@redhat.com>
2024-03-19 12:21:18 +0100

    [v5.0] libpod: restart always reconfigure the netns
    Always teardown the network, trying to reuse the netns has caused
    a significant amount of bugs in this code here. It also never worked
    for containers with user namespaces. So once and for all simplify this
    by never reusing the netns. Originally this was done to have a faster
    restart of containers but with netavark now we are much faster so it
    shouldn't be that noticeable in practice. It also makes more sense to
    reconfigure the netns as it is likely that the container exited due
    some broken network state in which case reusing would just cause more
    harm than good.
    
    The main motivation for this change was the pasta change to use
    --dns-forward by default. As the restarted contianer had no idea what
    nameserver to use as pasta just kept running.
    
    [NO NEW TESTS NEEDED]
    
    Signed-off-by: Paul Holzinger <pholzing@redhat.com>
    Signed-off-by: tomsweeneyredhat <tsweeney@redhat.com>

--- libpod/container_internal_linux.go
+++ libpod/container_internal_linux.go
@@ -413,27 +413,6 @@ func (c *Container) getOCICgroupPath() (string, error) {
 	}
 }
 
-// If the container is rootless, set up the slirp4netns network
-func (c *Container) setupRootlessNetwork() error {
-	// set up slirp4netns again because slirp4netns will die when conmon exits
-	if c.config.NetMode.IsSlirp4netns() {
-		err := c.runtime.setupSlirp4netns(c, c.state.NetNS)
-		if err != nil {
-			return err
-		}
-	}
-
-	// set up rootlesskit port forwarder again since it dies when conmon exits
-	// we use rootlesskit port forwarder only as rootless and when bridge network is used
-	if rootless.IsRootless() && c.config.NetMode.IsBridge() && len(c.config.PortMappings) > 0 {
-		err := c.runtime.setupRootlessPortMappingViaRLK(c, c.state.NetNS, c.state.NetworkStatus)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
 func openDirectory(path string) (fd int, err error) {
 	return unix.Open(path, unix.O_RDONLY|unix.O_PATH, 0)
 }
