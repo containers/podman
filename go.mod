a40cf3195acb6ac5fea5ab4617afb99006a3bed7
Evan Lezar <elezar@nvidia.com>
2024-04-05 17:27:27 +0200

    Bump tags.cncf.io/container-device-interface to v0.7.1
    This includes migrating from cdi.GetRegistry() to cdi.Configure() and
    cdi.GetDefaultCache() as applicable.
    
    Signed-off-by: Evan Lezar <elezar@nvidia.com>

--- go.mod
+++ go.mod
@@ -94,7 +94,7 @@ require (
 	gopkg.in/inf.v0 v0.9.1
 	gopkg.in/yaml.v3 v3.0.1
 	sigs.k8s.io/yaml v1.4.0
-	tags.cncf.io/container-device-interface v0.6.2
+	tags.cncf.io/container-device-interface v0.7.1
 )
 
 require (
@@ -238,7 +238,7 @@ require (
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	tags.cncf.io/container-device-interface/specs-go v0.6.0 // indirect
+	tags.cncf.io/container-device-interface/specs-go v0.7.0 // indirect
 )
 
 replace github.com/opencontainers/runc => github.com/opencontainers/runc v1.1.1-0.20240131200429-02120488a4c0
--- pkg/specgen/generate/config_freebsd.go
+++ pkg/specgen/generate/config_freebsd.go
@@ -18,14 +18,16 @@ import (
 // DevicesFromPath computes a list of devices
 func DevicesFromPath(g *generate.Generator, devicePath string) error {
 	if isCDIDevice(devicePath) {
-		registry := cdi.GetRegistry(
+		registry, err := cdi.NewCache(
 			cdi.WithAutoRefresh(false),
 		)
+		if err != nil {
+			return fmt.Errorf("creating CDI registry: %w", err)
+		}
 		if err := registry.Refresh(); err != nil {
 			logrus.Debugf("The following error was triggered when refreshing the CDI registry: %v", err)
 		}
-		_, err := registry.InjectDevices(g.Config, devicePath)
-		if err != nil {
+		if _, err = registry.InjectDevices(g.Config, devicePath); err != nil {
 			return fmt.Errorf("setting up CDI devices: %w", err)
 		}
 		return nil
