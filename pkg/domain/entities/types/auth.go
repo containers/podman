e8055904e10eaecc5cbba0bd2bbd9725235ec7d6
Paul Holzinger <pholzing@redhat.com>
2024-04-12 14:10:27 +0200

    swagger fix infinitive recursion on some types
    Commit 668d517af9 moved a lot of type definitions and by that also
    copied a bucnh of swagger:model comments, this caused swagger to create
    a incorrect yaml that can no longer be parsed by redoc due
    "Self-referencing circular pointer".
    
    The yaml basically defined the type with a name and the pointed to the
    same name definition again so it caused a infinitive recursion where
    redoc just throws an error but the swagger style ignored the case so it
    seemed like it worked but obviously the type information was not
    working.
    
    Fixes #22351
    
    Signed-off-by: Paul Holzinger <pholzing@redhat.com>

--- pkg/domain/entities/types/auth.go
+++ pkg/domain/entities/types/auth.go
@@ -2,6 +2,7 @@
 package types
 
 // AuthConfig contains authorization information for connecting to a Registry
+// swagger:model
 type AuthConfig struct {
 	Username string `json:"username,omitempty"`
 	Password string `json:"password,omitempty"`
