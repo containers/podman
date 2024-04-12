fb4ddf86cc214941ba04eb93432498c2d44e2ea3
Mario Loriedo <mario.loriedo@gmail.com>
2024-04-12 18:32:12 +0200

    Add Hyper-V option in windows installer
    podman.msi GUI has a radio-button to select WSL or Hyper-V
    
    The checkbox in podman.msi GUI allow the user to specify if
    the machine provider installation (WSL or Hyper-V) should
    be part of podman installation or not.
    
    podman-setup.exe supports 2 new variables: MachineProvider
    (valid values are `wsl` and `hyperv`) and HyperVCheckbox
    (valid values are `0` and `1`)
    
    Installation creates the configuration file
    `99-podman-machine-provider.conf` under folder
    `%APPDATA\containers\containers.conf.d` with the selected
    machine provider
    
    Cirrus CI `win_installer_task` tests the installation with
    both `hyperv` and `wsl` and verifies the configuration.
    Uninstallation is tested too.
    
    Note that podman-setup.exe GUI doesn't allow to choose the
    provider yet. See https://github.com/containers/podman/issues/22492
    
    Signed-off-by: Mario Loriedo <mario.loriedo@gmail.com>

--- contrib/cirrus/win-installer-main.ps1
+++ contrib/cirrus/win-installer-main.ps1
@@ -2,22 +2,59 @@
 
 . $PSScriptRoot\win-lib.ps1
 
-Set-Location "$ENV:CIRRUS_WORKING_DIR\repo\contrib\win-installer"
+if ($Env:CI -eq "true") {
+    $WIN_INST_FOLDER = "$ENV:CIRRUS_WORKING_DIR\repo\contrib\win-installer"
+    $RELEASE_DIR = "$ENV:CIRRUS_WORKING_DIR\repo"
+} else {
+    $WIN_INST_FOLDER = "$PSScriptRoot\..\win-installer"
+    $ENV:WIN_INST_VER = "9.9.9"
+    $RELEASE_DIR = "$PSScriptRoot\..\.."
+    $ENV:CONTAINERS_MACHINE_PROVIDER = "wsl"
+}
+
+$ConfFilePath = "$env:ProgramData\containers\containers.conf.d\99-podman-machine-provider.conf"
+$WindowsPathsToTest = @("C:\Program Files\RedHat\Podman\podman.exe",
+                  "C:\Program Files\RedHat\Podman\win-sshproxy.exe",
+                  "$ConfFilePath",
+                  "HKLM:\SOFTWARE\Red Hat\Podman")
+
+Set-Location $WIN_INST_FOLDER
 
 # Build Installer
 # Note: consumes podman-remote-release-windows_amd64.zip from repo.tbz2
-Run-Command ".\build.ps1 $Env:WIN_INST_VER dev `"$ENV:CIRRUS_WORKING_DIR\repo`""
+Run-Command ".\build.ps1 $Env:WIN_INST_VER dev `"$RELEASE_DIR`""
 
-# Run the installer silently and WSL install option disabled (prevent reboots, wsl requirements)
+# Run the installer silently and WSL/HyperV install options disabled (prevent reboots)
 # We need AllowOldWin=1 for server 2019 (cirrus image), can be dropped after server 2022
-$ret = Start-Process -Wait -PassThru ".\podman-${ENV:WIN_INST_VER}-dev-setup.exe" -ArgumentList "/install /quiet WSLCheckbox=0 AllowOldWin=1 /log inst.log"
+$ret = Start-Process -Wait -PassThru ".\podman-${ENV:WIN_INST_VER}-dev-setup.exe" -ArgumentList "/install /quiet MachineProvider=$ENV:CONTAINERS_MACHINE_PROVIDER WSLCheckbox=0 HyperVCheckbox=0 AllowOldWin=1 /log inst.log"
 if ($ret.ExitCode -ne 0) {
     Write-Host "Install failed, dumping log"
     Get-Content inst.log
     throw "Exit code is $($ret.ExitCode)"
 }
-if (! ((Test-Path -Path "C:\Program Files\RedHat\Podman\podman.exe") -and `
-       (Test-Path -Path "C:\Program Files\RedHat\Podman\win-sshproxy.exe"))) {
-    throw "Expected podman.exe and win-sshproxy.exe, one or both not present after install"
+$WindowsPathsToTest | ForEach-Object {
+    if (! (Test-Path -Path $_) ) {
+        throw "Expected $_ but it's not present after uninstall"
+    }
 }
+$machineProvider = Get-Content $ConfFilePath | Select-Object -Skip 1 | ConvertFrom-StringData | ForEach-Object { $_.provider }
+if ( $machineProvider -ne "`"$ENV:CONTAINERS_MACHINE_PROVIDER`"" ) {
+    throw "Expected `"$ENV:CONTAINERS_MACHINE_PROVIDER`" as default machine provider but got $machineProvider"
+}
+
 Write-Host "Installer verification successful!"
+
+# Run the uninstaller silently to verify that it cleans up properly
+$ret = Start-Process -Wait -PassThru ".\podman-${ENV:WIN_INST_VER}-dev-setup.exe" -ArgumentList "/uninstall /quiet /log uninst.log"
+if ($ret.ExitCode -ne 0) {
+    Write-Host "Uninstall failed, dumping log"
+    Get-Content uninst.log
+    throw "Exit code is $($ret.ExitCode)"
+}
+$WindowsPathsToTest | ForEach-Object {
+    if ( Test-Path -Path $_ ) {
+        throw "Path $_ is still present after uninstall"
+    }
+}
+
+Write-Host "Uninstaller verification successful!"
