79a8c3b4802dd2ae4b21d6e76a09fe9fe7a8be2c
Paul Holzinger <pholzing@redhat.com>
2024-04-18 14:24:38 +0200

    test/compose: remove compose v1 code
    Now that we only test compose v2 remove the special cases from the test
    code to simply the tests.
    
    Signed-off-by: Paul Holzinger <pholzing@redhat.com>
    (cherry picked from commit b1736c472a5f0975901b94285f5227e1cc3d2f90)
    Signed-off-by: Lokesh Mandvekar <lsm5@redhat.com>
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- test/compose/ipam_set_ip/tests.sh
+++ test/compose/ipam_set_ip/tests.sh
@@ -1,8 +1,5 @@
 # -*- bash -*-
 
-ctr_name="ipam_set_ip_test_1"
-if [ "$TEST_FLAVOR" = "compose_v2" ]; then
-    ctr_name="ipam_set_ip-test-1"
-fi
+ctr_name="ipam_set_ip-test-1"
 podman container inspect "$ctr_name" --format '{{ .NetworkSettings.Networks.ipam_set_ip_net1.IPAddress }}'
 like "$output" "10.123.0.253" "$testname : ip address is set"
--- test/compose/uptwice/tests.sh
+++ test/compose/uptwice/tests.sh
@@ -1,4 +1,10 @@
 # -*- bash -*-
 
 sed -i -e 's/10001/10002/' docker-compose.yml
-docker-compose up -d
+output=$(docker-compose up -d 2>&1)
+
+# Horrible output check here but we really want to make sure that there are
+# no unexpected warning/errors and the normal messages are send on stderr as
+# well so we cannot check for an empty stderr.
+expected="Container uptwice-app-1  Recreate${NL}Container uptwice-app-1  Recreated${NL}Container uptwice-app-1  Starting${NL}Container uptwice-app-1  Started"
+is "$output" "$expected" "no error output in compose up (#15580)"
