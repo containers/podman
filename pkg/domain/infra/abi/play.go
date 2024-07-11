bf8c3065f29c768994518abad0d4031d819603bc
Giuseppe Scrivano <gscrivan@redhat.com>
2024-07-11 09:53:00 +0200

    infra: user ns annotation higher precedence
    The pod spec HostUsers boolean only specifies whether a user namespace
    is used or not.  Hene, the podman specific annotation must have a
    higher precedence since it defines how the user namespace is created.
    
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- pkg/domain/infra/abi/play.go
+++ pkg/domain/infra/abi/play.go
@@ -603,11 +603,12 @@ func (ic *ContainerEngine) playKubePod(ctx context.Context, podName string, podY
 	if options.Userns == "" {
 		if v, ok := annotations[define.UserNsAnnotation]; ok {
 			options.Userns = v
+		} else if v, ok := annotations[define.UserNsAnnotation+"/"+podName]; ok {
+			options.Userns = v
+		} else if podYAML.Spec.HostUsers != nil && !*podYAML.Spec.HostUsers {
+			options.Userns = "auto"
 		} else {
 			options.Userns = "host"
-		}
-		if podYAML.Spec.HostUsers != nil && !*podYAML.Spec.HostUsers {
-			options.Userns = "auto"
 		}
 	} else if podYAML.Spec.HostUsers != nil {
 		logrus.Info("overriding the user namespace mode in the pod spec")
