1912dd4bf68a567648aee96d10784fb49eacacd0
Paul Holzinger <pholzing@redhat.com>
2024-08-06 13:13:36 +0200

    test/e2e: work around for pasta issue
    Use network slirp4netns for the registry container to work around a
    pasta regression (#23517). This should be revert once it is fixed in
    pasta and included in our CI images.
    
    Signed-off-by: Paul Holzinger <pholzing@redhat.com>

--- test/e2e/pull_test.go
+++ test/e2e/pull_test.go
@@ -667,7 +667,8 @@ var _ = Describe("Podman pull", func() {
 			}
 			lock := GetPortLock("5012")
 			defer lock.Unlock()
-			session := podmanTest.Podman([]string{"run", "-d", "--name", "registry", "-p", "5012:5000", REGISTRY_IMAGE, "/entrypoint.sh", "/etc/docker/registry/config.yml"})
+			// FIXME: #23517: using network slirp4netns as work around
+			session := podmanTest.Podman([]string{"run", "-d", "--network", "slirp4netns", "--name", "registry", "-p", "5012:5000", REGISTRY_IMAGE, "/entrypoint.sh", "/etc/docker/registry/config.yml"})
 			session.WaitWithDefaultTimeout()
 			Expect(session).Should(ExitCleanly())
 
