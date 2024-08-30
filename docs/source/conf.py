f40eb5f06a007c8005fb1afda4c8516989323920
Paul Holzinger <pholzing@redhat.com>
2024-08-30 11:51:34 +0200

    docs: update read the docs changes
    Read the Docs does some update by October 7, 2024 which will effect our
    builds per[1]. As such apply the recommended changes from there to our
    conf.py file so we do not get broken builds after that date hopefully.
    
    [1] https://about.readthedocs.com/blog/2024/07/addons-by-default/#how-does-it-affect-my-projects
    
    Signed-off-by: Paul Holzinger <pholzing@redhat.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,6 +18,15 @@ import re
 import os
 import subprocess
 
+# Define the canonical URL for our custom docs.podman.io domain configured on Read the Docs
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "")
+
+# Tell Jinja2 templates the build is running on Read the Docs
+if os.environ.get("READTHEDOCS", "") == "True":
+    if "html_context" not in globals():
+        html_context = {}
+    html_context["READTHEDOCS"] = True
+
 # We have to run the preprocessor to create the actual markdown files from .in files.
 # Do it here so the it can work on readthedocs as well.
 path = os.path.join(os.path.abspath(os.path.dirname(
