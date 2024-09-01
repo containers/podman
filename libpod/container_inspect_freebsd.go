60af13ca0f7d11479ffdec3fd4831c974fefa3b7
Doug Rabson <dfr@rabson.org>
2024-09-01 13:42:22 +0100

    libpod: fix HostConfig.Devices output from 'podman inspect' on FreeBSD
    This changes the value emitted for HostConfig.Devices from 'null' to
    '[]'. The 'null' value was preventing ansible's podman module from
    working correctly on FreeBSD.
    
    Signed-off-by: Doug Rabson <dfr@rabson.org>

--- libpod/container_inspect_freebsd.go
+++ libpod/container_inspect_freebsd.go
@@ -15,5 +15,14 @@ func (c *Container) platformInspectContainerHostConfig(ctrSpec *spec.Spec, hostC
 	// UTS namespace mode
 	hostConfig.UTSMode = c.NamespaceMode(spec.UTSNamespace, ctrSpec)
 
+	// Devices
+	// Do not include if privileged - assumed that all devices will be
+	// included.
+	var err error
+	hostConfig.Devices, err = c.GetDevices(hostConfig.Privileged, *ctrSpec, map[string]string{})
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
