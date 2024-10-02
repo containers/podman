5c98c4bc8b6f5a16d68d29f681d31eded35cb620
Paul Holzinger <pholzing@redhat.com>
2024-10-02 15:43:39 +0200

    vendor: update c/common to v0.57.7
    Fixes CVE-2024-9341
    Fixes https://issues.redhat.com/browse/RHEL-60964
    Fixes https://issues.redhat.com/browse/RHEL-60962
    
    Signed-off-by: Paul Holzinger <pholzing@redhat.com>

--- vendor/github.com/containers/common/pkg/subscriptions/subscriptions.go
+++ vendor/github.com/containers/common/pkg/subscriptions/subscriptions.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/containers/common/pkg/umask"
 	"github.com/containers/storage/pkg/idtools"
+	securejoin "github.com/cyphar/filepath-securejoin"
 	rspec "github.com/opencontainers/runtime-spec/specs-go"
 	"github.com/opencontainers/selinux/go-selinux/label"
 	"github.com/sirupsen/logrus"
@@ -345,7 +346,10 @@ func addFIPSModeSubscription(mounts *[]rspec.Mount, containerRunDir, mountPoint,
 
 	srcBackendDir := "/usr/share/crypto-policies/back-ends/FIPS"
 	destDir := "/etc/crypto-policies/back-ends"
-	srcOnHost := filepath.Join(mountPoint, srcBackendDir)
+	srcOnHost, err := securejoin.SecureJoin(mountPoint, srcBackendDir)
+	if err != nil {
+		return fmt.Errorf("resolve %s in the container: %w", srcBackendDir, err)
+	}
 	if _, err := os.Stat(srcOnHost); err != nil {
 		if errors.Is(err, os.ErrNotExist) {
 			return nil
