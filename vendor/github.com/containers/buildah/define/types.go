c1d643e387b25c36f22a6e08c18f79b455d7cc79
David Shea <dshea@redhat.com>
2024-10-25 11:18:07 -0400

    [v4.2.0-rhel] Update buildah for CVE-2024-9675
    Addresses:
    https://issues.redhat.com/browse/RHEL-62376
    https://issues.redhat.com/browse/RHEL-62385
    
    [NO NEW TESTS NEEDED]
    
    Signed-off-by: David Shea <dshea@redhat.com>

--- vendor/github.com/containers/buildah/define/types.go
+++ vendor/github.com/containers/buildah/define/types.go
@@ -30,7 +30,7 @@ const (
 	Package = "buildah"
 	// Version for the Package.  Bump version in contrib/rpm/buildah.spec
 	// too.
-	Version = "1.27.4"
+	Version = "1.27.5"
 
 	// DefaultRuntime if containers.conf fails.
 	DefaultRuntime = "runc"
