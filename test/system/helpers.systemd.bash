0d22d7b74ec49c455c0134660c8fa0218abb048f
Ygal Blum <ygal.blum@gmail.com>
2024-11-21 11:22:08 -0500

    Quadlet - Use = sign when setting the pull arg for build
    Signed-off-by: Ygal Blum <ygal.blum@gmail.com>
    (cherry picked from commit 13affe96d62ef8ee2c2d440e30ef06e7f861e9e8)
    Signed-off-by: Paul Holzinger <pholzing@redhat.com>

--- test/system/helpers.systemd.bash
+++ test/system/helpers.systemd.bash
@@ -89,6 +89,8 @@ quadlet_to_service_name() {
         suffix="-image"
     elif [ "$extension" == "pod" ]; then
         suffix="-pod"
+    elif [ "$extension" == "build" ]; then
+        suffix="-build"
     fi
 
     echo "$filename$suffix.service"
