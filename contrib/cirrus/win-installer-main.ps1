af607d0de41bed86056bbf9250d2b27a8bfaaeb8
Mario Loriedo <mario.loriedo@gmail.com>
2024-12-11 13:23:21 +0100

    Add win installer patch
    Adding a patch to the bundle so that the update from previous version
    (v5.3.1) is a minor update, not a major one. A minor update prevents the
    full uninstallation of v5.3.1 and an unrequested reboot of the machine.
    
    Signed-off-by: Mario Loriedo <mario.loriedo@gmail.com>

--- contrib/cirrus/win-installer-main.ps1
+++ contrib/cirrus/win-installer-main.ps1
@@ -7,16 +7,31 @@ if ($Env:CI -eq "true") {
     $RELEASE_DIR = "$ENV:CIRRUS_WORKING_DIR\repo"
 } else {
     $WIN_INST_FOLDER = "$PSScriptRoot\..\win-installer"
-    $ENV:WIN_INST_VER = "9.9.9"
+    $ENV:WIN_INST_VER = "9.9.8"
     $RELEASE_DIR = "$PSScriptRoot\..\..\contrib\win-installer\current"
     if ($null -eq $ENV:CONTAINERS_MACHINE_PROVIDER) { $ENV:CONTAINERS_MACHINE_PROVIDER = 'wsl' }
 }
 
 Push-Location $WIN_INST_FOLDER
 
-# Build Installer
+# Build and test the windows installer
+
+# Downlaod v5.3.1 installer as `build.ps1` uses it to build the patch
+# (podman.msp). `build.ps1` reads `$env:V531_SETUP_EXE_PATH` to get its path.
+# The v5.3.1 installer is also used to test the "v5.3.1 -> current" version minor
+# update (with patch).
+$env:V531_SETUP_EXE_PATH = Get-Podman-Setup-From-GitHub -version "tags/v5.3.1"
+
+# Downlaod the previous installer to test a major update (without patch)
+# After v5.3.2 release we should download latest instead of v5.3.0 (i.e.
+# `Get-Latest-Podman-Setup-From-GitHub`)
+$env:PREV_SETUP_EXE_PATH = Get-Podman-Setup-From-GitHub -version "tags/v5.3.0"
+
 # Note: consumes podman-remote-release-windows_amd64.zip from repo.tar.zst
 Run-Command ".\build.ps1 $Env:WIN_INST_VER dev `"$RELEASE_DIR`""
+
+# Build a v9.9.9 installer to test an update from current to next version
+Run-Command ".\build.ps1 9.9.9 dev `"$RELEASE_DIR`""
 
 Pop-Location
 
@@ -25,4 +40,7 @@ $command = "$WIN_INST_FOLDER\test-installer.ps1 "
 $command += "-scenario all "
 $command += "-provider $ENV:CONTAINERS_MACHINE_PROVIDER "
 $command += "-setupExePath `"$WIN_INST_FOLDER\podman-$ENV:WIN_INST_VER-dev-setup.exe`""
+$command += "-previousSetupExePath `"$env:PREV_SETUP_EXE_PATH`""
+$command += "-nextSetupExePath `"$WIN_INST_FOLDER\podman-9.9.9-dev-setup.exe`""
+$command += "-v531SetupExePath `"$env:V531_SETUP_EXE_PATH`""
 Run-Command "${command}"
