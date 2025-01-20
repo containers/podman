bdc195d641fabed6764de59f22a601e059e153d7
Paul Holzinger <pholzing@redhat.com>
2025-01-20 18:50:57 +0100

    pkg/machine/e2e: improve podman.exe match
    The regex match would return a horrible error message and is way more
    complicated then it should be. Simply check that .exe is not part of the
    output.
    
    Signed-off-by: Paul Holzinger <pholzing@redhat.com>

--- pkg/machine/e2e/help_test.go
+++ pkg/machine/e2e/help_test.go
@@ -1,9 +1,6 @@
 package e2e_test
 
 import (
-	"regexp"
-	"slices"
-
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	. "github.com/onsi/gomega/gexec"
@@ -16,8 +13,6 @@ var _ = Describe("podman help", func() {
 		Expect(helpSession).Should(Exit(0))
 
 		// Verify `.exe` suffix doesn't present in the usage command string
-		helpMessages := helpSession.outputToStringSlice()
-		usageCmdIndex := slices.IndexFunc(helpMessages, func(helpMessage string) bool { return helpMessage == "Usage:" }) + 1
-		Expect(regexp.MustCompile(`\w\.exe\b`).MatchString(helpMessages[usageCmdIndex])).Should(BeFalse())
+		Expect(helpSession.outputToString()).ToNot(ContainSubstring(".exe"))
 	})
 })
