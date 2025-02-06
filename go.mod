0b9f534a2b45595153c9dbd7f8fdd6a16aede5eb
tomsweeneyredhat <tsweeney@redhat.com>
2025-02-06 18:44:32 -0500

    [v4.4.1-rhel] CVE-2024-11218 Fix
    This fixes CVE-2024-11218 by vendoring in Buidlah v1.29.5 which
    has the actual fixes within it.
    
    Fixes: https://issues.redhat.com/browse/OCPBUGS-44538,
    https://issues.redhat.com/browse/OCPBUGS-44537,
    https://issues.redhat.com/browse/OCPBUGS-44539,
    https://issues.redhat.com/browse/RHEL-67603, https://issues.redhat.com/browse/RHEL-67597
    
    [NO NEW TESTS NEEDED]
    
    Signed-off-by: tomsweeneyredhat <tsweeney@redhat.com>

--- go.mod
+++ go.mod
@@ -11,7 +11,7 @@ require (
 	github.com/container-orchestrated-devices/container-device-interface v0.5.3
 	github.com/containernetworking/cni v1.1.2
 	github.com/containernetworking/plugins v1.2.0
-	github.com/containers/buildah v1.29.4
+	github.com/containers/buildah v1.29.5
 	github.com/containers/common v0.51.4
 	github.com/containers/conmon v2.0.20+incompatible
 	github.com/containers/image/v5 v5.24.3
--- vendor/github.com/containers/buildah/pkg/overlay/overlay.go
+++ vendor/github.com/containers/buildah/pkg/overlay/overlay.go
@@ -11,9 +11,11 @@ import (
 	"errors"
 
 	"github.com/containers/storage/pkg/idtools"
+	"github.com/containers/storage/pkg/mount"
 	"github.com/containers/storage/pkg/system"
 	"github.com/containers/storage/pkg/unshare"
 	"github.com/opencontainers/runtime-spec/specs-go"
+	"github.com/opencontainers/selinux/go-selinux/label"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/sys/unix"
 )
@@ -50,6 +52,12 @@ type Options struct {
 	RootUID int
 	// RootGID is not used yet but keeping it here for legacy reasons.
 	RootGID int
+	// Force overlay mounting and return a bind mount, rather than
+	// attempting to optimize by having the runtime actually mount and
+	// manage the overlay filesystem.
+	ForceMount bool
+	// MountLabel is a label to force for the overlay filesystem.
+	MountLabel string
 }
 
 // TempDir generates an overlay Temp directory in the container content
@@ -187,6 +195,9 @@ func MountWithOptions(contentDir, source, dest string, opts *Options) (mount spe
 		}
 		overlayOptions = fmt.Sprintf("lowerdir=%s,upperdir=%s,workdir=%s,private", escapeColon(source), upperDir, workDir)
 	}
+	if opts.MountLabel != "" {
+		overlayOptions = overlayOptions + "," + label.FormatMountLabel("", opts.MountLabel)
+	}
 
 	mountProgram := findMountProgram(opts.GraphOpts)
 	if mountProgram != "" {
@@ -211,7 +222,25 @@ func MountWithOptions(contentDir, source, dest string, opts *Options) (mount spe
 	mount.Type = "overlay"
 	mount.Options = strings.Split(overlayOptions, ",")
 
+	if opts.ForceMount {
+		if err := mountNatively(overlayOptions, mergeDir); err != nil {
+			return mount, err
+		}
+
+		mount.Source = mergeDir
+		mount.Destination = dest
+		mount.Type = "bind"
+		mount.Options = []string{"bind", "slave"}
+		return mount, nil
+	}
+
 	return mount, nil
+}
+
+// mountNatively mounts an overlay at mergeDir using the kernel's mount()
+// system call.
+func mountNatively(overlayOptions, mergeDir string) error {
+	return mount.Mount("overlay", mergeDir, "overlay", overlayOptions)
 }
 
 // Convert ":" to "\:", the path which will be overlay mounted need to be escaped
