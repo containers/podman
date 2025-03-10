af29bb5b6ed0c779d4f2a89186578d3666849458
Mario Loriedo <mario.loriedo@gmail.com>
2025-03-10 18:11:12 +0100

    Update CI to run Windows unit tests
    Add a new target in winmake.ps1 to run unit tests and use
    use it in a new cirrus task.
    
    Fix machine_windows_test.go to make it work in CI machine.
    
    Add the `!windows` tag on tests files that fail on Windows.
    
    Signed-off-by: Mario Loriedo <mario.loriedo@gmail.com>

--- pkg/machine/machine_windows_test.go
+++ pkg/machine/machine_windows_test.go
@@ -3,14 +3,40 @@
 package machine
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"testing"
 
+	"golang.org/x/sys/windows"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+// shortPathToLongPath converts a Windows short path (C:\PROGRA~1) to its
+// long path equivalent (C:\Program Files). It returns an error if shortPath
+// doesn't exist.
+func shortPathToLongPath(shortPath string) (string, error) {
+	shortPathPtr, err := windows.UTF16PtrFromString(shortPath)
+	if err != nil {
+		return "", err
+	}
+	len, err := windows.GetLongPathName(shortPathPtr, nil, 0)
+	if err != nil {
+		return "", err
+	}
+	if len == 0 {
+		return "", fmt.Errorf("failed to get buffer size for path: %s", shortPath)
+	}
+	longPathPtr := &(make([]uint16, len)[0])
+	_, err = windows.GetLongPathName(shortPathPtr, longPathPtr, len)
+	if err != nil {
+		return "", err
+	}
+	return windows.UTF16PtrToString(longPathPtr), nil
+}
 
 // CreateNewItemWithPowerShell creates a new item using PowerShell.
 // It's an helper to easily create junctions on Windows (as well as other file types).
@@ -23,15 +49,21 @@ import (
 //   - itemType: The type of the item to be created (e.g., "File", "SymbolicLink", "Junction").
 //   - target: The target for the new item, if applicable.
 func CreateNewItemWithPowerShell(t *testing.T, path string, itemType string, target string) {
-	var pwshCmd string
+	var pwshCmd, pwshPath string
+	// Look for Powershell 7 first as it allow Symlink creation for non-admins too
+	pwshPath, err := exec.LookPath("pwsh.exe")
+	if err != nil {
+		// Use Powershell 5 that is always present
+		pwshPath = "powershell.exe"
+	}
 	pwshCmd = "New-Item -Path " + path + " -ItemType " + itemType
 	if target != "" {
 		pwshCmd += " -Target " + target
 	}
-	cmd := exec.Command("pwsh", "-Command", pwshCmd)
+	cmd := exec.Command(pwshPath, "-Command", pwshCmd)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	err := cmd.Run()
+	err = cmd.Run()
 	require.NoError(t, err)
 }
 
@@ -45,13 +77,16 @@ func CreateNewItemWithPowerShell(t *testing.T, path string, itemType string, tar
 // with filepath.EvalSymlink().
 func TestEvalSymlinksOrClean(t *testing.T) {
 	// Create a temporary directory to store the normal file
-	normalFileDir := t.TempDir()
+	normalFileDir, err := shortPathToLongPath(t.TempDir())
+	require.NoError(t, err)
 
 	// Create a temporary directory to store the (hard/sym)link files
-	linkFilesDir := t.TempDir()
+	linkFilesDir, err := shortPathToLongPath(t.TempDir())
+	require.NoError(t, err)
 
 	// Create a temporary directory where the mount point will be created
-	mountPointDir := t.TempDir()
+	mountPointDir, err := shortPathToLongPath(t.TempDir())
+	require.NoError(t, err)
 
 	// Create a normal file
 	normalFile := filepath.Join(normalFileDir, "testFile")
