91113c46ef029a20a20e253bcd6ca6717e2a550e
Kir Kolyshkin <kolyshkin@gmail.com>
2025-03-28 18:23:30 -0700

    test/utils: suppress ST1001 warnings
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- test/utils/utils.go
+++ test/utils/utils.go
@@ -20,9 +20,9 @@ import (
 
 	"github.com/sirupsen/logrus"
 
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
-	. "github.com/onsi/gomega/gexec"
+	. "github.com/onsi/ginkgo/v2"    //nolint:staticcheck // ST1001: should not use dot imports
+	. "github.com/onsi/gomega"       //nolint:staticcheck // ST1001: should not use dot imports
+	. "github.com/onsi/gomega/gexec" //nolint:staticcheck // ST1001: should not use dot imports
 )
 
 type NetworkBackend int
