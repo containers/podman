f18c917ef83e90c6fcd3cb2579cc9e63b881796c
Kir Kolyshkin <kolyshkin@gmail.com>
2025-03-31 13:52:06 -0700

    ci: run golangci-lint on freebsd
    This could have been done by simply running
    
    	GOOS=freebsd ./bin/golangci-lint run [options] ./...
    
    on Linux, but some freebsd code is using cgo (i.e. is linked to C
    libraries), so real freebsd environment is required.
    
    This also fixes the issue of ignoring linter errors for Windows and
    Darwin (exit 0), introduced by commit c9b108d5b38.
    
    Fixes: c9b108d5b38 ("Bump golangci-lint to v2.0.2")
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- hack/golangci-lint.sh
+++ hack/golangci-lint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Run golangci-lint with different sets of build tags.
 set -e
@@ -8,23 +8,29 @@ set -e
 # a very old version, where modern features (like `declare -A`) are
 # absent.
 
-echo "Linting for GOOS=$GOOS"
-# Special case: for Darwin and Windows only "remote" linting is possible and required.
-if [[ "$GOOS" == "windows" || "$GOOS" == "darwin" ]]; then
-  (
-    set -x
-    ./bin/golangci-lint run --build-tags="remote,containers_image_openpgp" "$@"
-  )
-  exit 0
-fi
+declare -a EXTRA_TAGS
 
-# Normal case (Linux): run linter for various sets of build tags.
-TAGS="apparmor,seccomp,selinux"
-for EXTRA_TAGS in "" ",systemd" ",remote"; do
+echo "Linting for GOOS=$GOOS"
+case "$GOOS" in
+  windows|darwin)
+    # For Darwin and Windows, only "remote" linting is possible and required.
+    TAGS="remote,containers_image_openpgp"
+    ;;
+  freebsd)
+    TAGS="containers_image_openpgp"
+    EXTRA_TAGS=(",remote")
+    ;;
+  *)
+    # Assume Linux: run linter for various sets of build tags.
+    TAGS="apparmor,seccomp,selinux"
+    EXTRA_TAGS=(",systemd" ",remote")
+esac
+
+for EXTRA in "" "${EXTRA_TAGS[@]}"; do
+  # Use set -x in a subshell to make it easy for a developer to copy-paste
+  # the command-line to focus or debug a single, specific linting category.
   (
-    # Make it really easy for a developer to copy-paste the command-line
-    # to focus or debug a single, specific linting category.
     set -x
-    ./bin/golangci-lint run --build-tags="${TAGS}${EXTRA_TAGS}" "$@"
+    ./bin/golangci-lint run --build-tags="${TAGS}${EXTRA}" "$@"
   )
 done
