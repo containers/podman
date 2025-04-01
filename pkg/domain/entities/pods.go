containers/podman

commit bbd085ad1e3cf9c5b543c907ad7014ccf8a5cb34

Podman Pod Create --cpus and --cpuset-cpus flags
Added logic and handling for two new Podman pod create Flags.

--cpus specifies the total number of cores on which the pod can execute, this
is a combination of the period and quota for the CPU.

 --cpuset-cpus is a string value which determines of these available cores,
how many we will truly execute on.

Signed-off-by: cdoern <cbdoer23@g.holycross.edu>

pkg/domain/entities/pods.go
@@ -7,6 +7,8 @@ import (
 
 	"github.com/containers/podman/v3/libpod/define"
 	"github.com/containers/podman/v3/pkg/specgen"
+	"github.com/containers/podman/v3/pkg/util"
+	"github.com/opencontainers/runtime-spec/specs-go"
 )
 
 type PodKillOptions struct {
@@ -116,13 +118,35 @@ type PodCreateOptions struct {
 	Name               string
 	Net                *NetOptions
 	Share              []string
+	Cpus               float64
+	CpusetCpus         string
 }
 
 type PodCreateReport struct {
 	Id string //nolint
 }
 
-func (p PodCreateOptions) ToPodSpecGen(s *specgen.PodSpecGenerator) {
+func (p *PodCreateOptions) CPULimits() *specs.LinuxCPU {
+	cpu := &specs.LinuxCPU{}
+	hasLimits := false
+
+	if p.Cpus != 0 {
+		period, quota := util.CoresToPeriodAndQuota(p.Cpus)
+		cpu.Period = &period
+		cpu.Quota = &quota
+		hasLimits = true
+	}
+	if p.CpusetCpus != "" {
+		cpu.Cpus = p.CpusetCpus
+		hasLimits = true
+	}
+	if !hasLimits {
+		return cpu
+	}
+	return cpu
+}
+
+func (p *PodCreateOptions) ToPodSpecGen(s *specgen.PodSpecGenerator) error {
 	// Basic Config
 	s.Name = p.Name
 	s.Hostname = p.Hostname
@@ -156,6 +180,21 @@ func (p PodCreateOptions) ToPodSpecGen(s *specgen.PodSpecGenerator) {
 
 	// Cgroup
 	s.CgroupParent = p.CGroupParent
+
+	// Resource config
+	cpuDat := p.CPULimits()
+	if s.ResourceLimits == nil {
+		s.ResourceLimits = &specs.LinuxResources{}
+		s.ResourceLimits.CPU = &specs.LinuxCPU{}
+	}
+	if cpuDat != nil {
+		s.ResourceLimits.CPU = cpuDat
+		if p.Cpus != 0 {
+			s.CPUPeriod = *cpuDat.Period
+			s.CPUQuota = *cpuDat.Quota
+		}
+	}
+	return nil
 }
 
 type PodPruneOptions struct {

pkg/specgen/podspecgen.go
@@ -2,6 +2,8 @@ package specgen
 
 import (
 	"net"
+
+	spec "github.com/opencontainers/runtime-spec/specs-go"
 )
 
 // PodBasicConfig contains basic configuration options for pods.
@@ -155,6 +157,16 @@ type PodSpecGenerator struct {
 	PodBasicConfig
 	PodNetworkConfig
 	PodCgroupConfig
+	PodResourceConfig
+}
+
+type PodResourceConfig struct {
+	// ResourceLimits contains linux specific CPU data for the pod
+	ResourceLimits *spec.LinuxResources `json:"resource_limits,omitempty"`
+	// CPU period of the cpuset, determined by --cpus
+	CPUPeriod uint64 `json:"cpu_period,omitempty"`
+	// CPU quota of the cpuset, determined by --cpus
+	CPUQuota int64 `json:"cpu_quota,omitempty"`
 }
 
 // NewPodSpecGenerator creates a new pod spec

cmd/podman/containers/create.go
@@ -225,7 +225,6 @@ func createInit(c *cobra.Command) error {
 		}
 		cliVals.Env = env
 	}
-
 	if c.Flag("cgroups").Changed && cliVals.CGroupsMode == "split" && registry.IsRemote() {
 		return errors.Errorf("the option --cgroups=%q is not supported in remote mode", cliVals.CGroupsMode)
 	}
@@ -316,6 +315,8 @@ func createPodIfNecessary(s *specgen.SpecGenerator, netOpts *entities.NetOptions
 		Net:           netOpts,
 		CreateCommand: os.Args,
 		Hostname:      s.ContainerBasicConfig.Hostname,
+		Cpus:          cliVals.CPUS,
+		CpusetCpus:    cliVals.CPUSetCPUs,
 	}
 	// Unset config values we passed to the pod to prevent them being used twice for the container and pod.
 	s.ContainerBasicConfig.Hostname = ""

cmd/podman/pods/create.go
@@ -5,9 +5,13 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"runtime"
+	"sort"
+	"strconv"
 	"strings"
 
 	"github.com/containers/common/pkg/completion"
+	"github.com/containers/common/pkg/sysinfo"
 	"github.com/containers/podman/v3/cmd/podman/common"
 	"github.com/containers/podman/v3/cmd/podman/parse"
 	"github.com/containers/podman/v3/cmd/podman/registry"
@@ -16,6 +20,7 @@ import (
 	"github.com/containers/podman/v3/pkg/errorhandling"
 	"github.com/containers/podman/v3/pkg/specgen"
 	"github.com/containers/podman/v3/pkg/util"
+	"github.com/docker/docker/pkg/parsers"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -54,6 +59,14 @@ func init() {
 	flags.SetInterspersed(false)
 
 	common.DefineNetFlags(createCommand)
+
+	cpusetflagName := "cpuset-cpus"
+	flags.StringVar(&createOptions.CpusetCpus, cpusetflagName, "", "CPUs in which to allow execution")
+	_ = createCommand.RegisterFlagCompletionFunc(cpusetflagName, completion.AutocompleteDefault)
+
+	cpusflagName := "cpus"
+	flags.Float64Var(&createOptions.Cpus, cpusflagName, 0.000, "set amount of CPUs for the pod")
+	_ = createCommand.RegisterFlagCompletionFunc(cpusflagName, completion.AutocompleteDefault)
 
 	cgroupParentflagName := "cgroup-parent"
 	flags.StringVar(&createOptions.CGroupParent, cgroupParentflagName, "", "Set parent cgroup for the pod")
@@ -185,6 +198,46 @@ func create(cmd *cobra.Command, args []string) error {
 		}
 	}
 
+	numCPU := sysinfo.NumCPU()
+	if numCPU == 0 {
+		numCPU = runtime.NumCPU()
+	}
+	if createOptions.Cpus > float64(numCPU) {
+		createOptions.Cpus = float64(numCPU)
+	}
+	copy := createOptions.CpusetCpus
+	cpuSet := createOptions.Cpus
+	if cpuSet == 0 {
+		cpuSet = float64(sysinfo.NumCPU())
+	}
+	ret, err := parsers.ParseUintList(copy)
+	copy = ""
+	if err != nil {
+		errors.Wrapf(err, "could not parse list")
+	}
+	var vals []int
+	for ind, val := range ret {
+		if val {
+			vals = append(vals, ind)
+		}
+	}
+	sort.Ints(vals)
+	for ind, core := range vals {
+		if core > int(cpuSet) {
+			if copy == "" {
+				copy = "0-" + strconv.Itoa(int(cpuSet))
+				createOptions.CpusetCpus = copy
+				break
+			} else {
+				createOptions.CpusetCpus = copy
+				break
+			}
+		} else if ind != 0 {
+			copy += "," + strconv.Itoa(core)
+		} else {
+			copy = "" + strconv.Itoa(core)
+		}
+	}
 	response, err := registry.ContainerEngine().PodCreate(context.Background(), createOptions)
 	if err != nil {
 		return err

libpod/container_validate.go
@@ -131,6 +131,5 @@ func (c *Container) validate() error {
 	if c.config.User == "" && (c.config.Spec.Process.User.UID != 0 || c.config.Spec.Process.User.GID != 0) {
 		return errors.Wrapf(define.ErrInvalidArg, "please set User explicitly via WithUser() instead of in OCI spec directly")
 	}
-
 	return nil
 }

libpod/define/pod_inspect.go
@@ -51,6 +51,12 @@ type InspectPodData struct {
 	// Containers gives a brief summary of all containers in the pod and
 	// their current status.
 	Containers []InspectPodContainerInfo `json:"Containers,omitempty"`
+	// CPUPeriod contains the CPU period of the pod
+	CPUPeriod uint64 `json:"cpu_period,omitempty"`
+	// CPUQuota contains the CPU quota of the pod
+	CPUQuota int64 `json:"cpu_quota,omitempty"`
+	// CPUSetCPUs contains linux specific CPU data for the pod
+	CPUSetCPUs string `json:"cpuset_cpus,omitempty"`
 }
 
 // InspectPodInfraConfig contains the configuration of the pod's infra
@@ -91,6 +97,12 @@ type InspectPodInfraConfig struct {
 	Networks []string
 	// NetworkOptions are additional options for each network
 	NetworkOptions map[string][]string
+	// CPUPeriod contains the CPU period of the pod
+	CPUPeriod uint64 `json:"cpu_period,omitempty"`
+	// CPUQuota contains the CPU quota of the pod
+	CPUQuota int64 `json:"cpu_quota,omitempty"`
+	// CPUSetCPUs contains linux specific CPU data for the container
+	CPUSetCPUs string `json:"cpuset_cpus,omitempty"`
 }
 
 // InspectPodContainerInfo contains information on a container in a pod.

libpod/options.go
@@ -20,6 +20,7 @@ import (
 	"github.com/containers/storage"
 	"github.com/containers/storage/pkg/idtools"
 	"github.com/cri-o/ocicni/pkg/ocicni"
+	"github.com/opencontainers/runtime-spec/specs-go"
 	"github.com/opencontainers/runtime-tools/generate"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
@@ -559,7 +560,6 @@ func WithMaxLogSize(limit int64) CtrCreateOption {
 		if ctr.valid {
 			return define.ErrRuntimeFinalized
 		}
-
 		ctr.config.LogSize = limit
 
 		return nil
@@ -867,7 +867,6 @@ func WithMountNSFrom(nsCtr *Container) CtrCreateOption {
 		if err := checkDependencyContainer(nsCtr, ctr); err != nil {
 			return err
 		}
-
 		ctr.config.MountNsCtr = nsCtr.ID()
 
 		return nil
@@ -2356,6 +2355,45 @@ func WithVolatile() CtrCreateOption {
 		}
 
 		ctr.config.Volatile = true
+		return nil
+	}
+}
+
+// WithPodCPUPAQ takes the given cpu period and quota and inserts them in the proper place.
+func WithPodCPUPAQ(period uint64, quota int64) PodCreateOption {
+	return func(pod *Pod) error {
+		if pod.valid {
+			return define.ErrPodFinalized
+		}
+		if pod.CPUPeriod() != 0 && pod.CPUQuota() != 0 {
+			pod.config.InfraContainer.ResourceLimits.CPU = &specs.LinuxCPU{
+				Period: &period,
+				Quota:  &quota,
+			}
+		} else {
+			pod.config.InfraContainer.ResourceLimits = &specs.LinuxResources{}
+			pod.config.InfraContainer.ResourceLimits.CPU = &specs.LinuxCPU{
+				Period: &period,
+				Quota:  &quota,
+			}
+		}
+		return nil
+	}
+}
+
+// WithPodCPUSetCPUS computes and sets the Cpus linux resource string which determines the amount of cores, from those available,  we are allowed to execute on
+func WithPodCPUSetCPUs(inp string) PodCreateOption {
+	return func(pod *Pod) error {
+		if pod.valid {
+			return define.ErrPodFinalized
+		}
+		if pod.ResourceLim().CPU.Period != nil {
+			pod.config.InfraContainer.ResourceLimits.CPU.Cpus = inp
+		} else {
+			pod.config.InfraContainer.ResourceLimits = &specs.LinuxResources{}
+			pod.config.InfraContainer.ResourceLimits.CPU = &specs.LinuxCPU{}
+			pod.config.InfraContainer.ResourceLimits.CPU.Cpus = inp
+		}
 		return nil
 	}
 }

libpod/pod.go
@@ -1,12 +1,14 @@
 package libpod
 
 import (
+	"context"
 	"net"
 	"time"
 
 	"github.com/containers/podman/v3/libpod/define"
 	"github.com/containers/podman/v3/libpod/lock"
 	"github.com/cri-o/ocicni/pkg/ocicni"
+	"github.com/opencontainers/runtime-spec/specs-go"
 	"github.com/pkg/errors"
 )
 
@@ -91,25 +93,26 @@ type podState struct {
 // Generally speaking, aside from those two exceptions, these options will set
 // the equivalent field in the container's configuration.
 type InfraContainerConfig struct {
-	ConmonPidFile      string               `json:"conmonPidFile"`
-	HasInfraContainer  bool                 `json:"makeInfraContainer"`
-	NoNetwork          bool                 `json:"noNetwork,omitempty"`
-	HostNetwork        bool                 `json:"infraHostNetwork,omitempty"`
-	PortBindings       []ocicni.PortMapping `json:"infraPortBindings"`
-	StaticIP           net.IP               `json:"staticIP,omitempty"`
-	StaticMAC          net.HardwareAddr     `json:"staticMAC,omitempty"`
-	UseImageResolvConf bool                 `json:"useImageResolvConf,omitempty"`
-	DNSServer          []string             `json:"dnsServer,omitempty"`
-	DNSSearch          []string             `json:"dnsSearch,omitempty"`
-	DNSOption          []string             `json:"dnsOption,omitempty"`
-	UseImageHosts      bool                 `json:"useImageHosts,omitempty"`
-	HostAdd            []string             `json:"hostsAdd,omitempty"`
-	Networks           []string             `json:"networks,omitempty"`
-	ExitCommand        []string             `json:"exitCommand,omitempty"`
-	InfraImage         string               `json:"infraImage,omitempty"`
-	InfraCommand       []string             `json:"infraCommand,omitempty"`
-	Slirp4netns        bool                 `json:"slirp4netns,omitempty"`
-	NetworkOptions     map[string][]string  `json:"network_options,omitempty"`
+	ConmonPidFile      string                `json:"conmonPidFile"`
+	HasInfraContainer  bool                  `json:"makeInfraContainer"`
+	NoNetwork          bool                  `json:"noNetwork,omitempty"`
+	HostNetwork        bool                  `json:"infraHostNetwork,omitempty"`
+	PortBindings       []ocicni.PortMapping  `json:"infraPortBindings"`
+	StaticIP           net.IP                `json:"staticIP,omitempty"`
+	StaticMAC          net.HardwareAddr      `json:"staticMAC,omitempty"`
+	UseImageResolvConf bool                  `json:"useImageResolvConf,omitempty"`
+	DNSServer          []string              `json:"dnsServer,omitempty"`
+	DNSSearch          []string              `json:"dnsSearch,omitempty"`
+	DNSOption          []string              `json:"dnsOption,omitempty"`
+	UseImageHosts      bool                  `json:"useImageHosts,omitempty"`
+	HostAdd            []string              `json:"hostsAdd,omitempty"`
+	Networks           []string              `json:"networks,omitempty"`
+	ExitCommand        []string              `json:"exitCommand,omitempty"`
+	InfraImage         string                `json:"infraImage,omitempty"`
+	InfraCommand       []string              `json:"infraCommand,omitempty"`
+	Slirp4netns        bool                  `json:"slirp4netns,omitempty"`
+	NetworkOptions     map[string][]string   `json:"network_options,omitempty"`
+	ResourceLimits     *specs.LinuxResources `json:"resource_limits,omitempty"`
 }
 
 // ID retrieves the pod's ID
@@ -126,6 +129,45 @@ func (p *Pod) Name() string {
 // Namespaces are used to logically separate containers and pods in the state.
 func (p *Pod) Namespace() string {
 	return p.config.Namespace
+}
+
+// ResourceLim returns the cpuset resource limits for the pod
+func (p *Pod) ResourceLim() *specs.LinuxResources {
+	resCopy := &specs.LinuxResources{}
+	if err := JSONDeepCopy(p.config.InfraContainer.ResourceLimits, resCopy); err != nil {
+		return nil
+	}
+	if resCopy != nil && resCopy.CPU != nil {
+		return resCopy
+	}
+	empty := &specs.LinuxResources{
+		CPU: &specs.LinuxCPU{},
+	}
+	return empty
+}
+
+// CPUPeriod returns the pod CPU period
+func (p *Pod) CPUPeriod() uint64 {
+	resCopy := &specs.LinuxResources{}
+	if err := JSONDeepCopy(p.config.InfraContainer.ResourceLimits, resCopy); err != nil {
+		return 0
+	}
+	if resCopy != nil && resCopy.CPU != nil && resCopy.CPU.Period != nil {
+		return *resCopy.CPU.Period
+	}
+	return 0
+}
+
+// CPUQuota returns the pod CPU quota
+func (p *Pod) CPUQuota() int64 {
+	resCopy := &specs.LinuxResources{}
+	if err := JSONDeepCopy(p.config.InfraContainer.ResourceLimits, resCopy); err != nil {
+		return 0
+	}
+	if resCopy != nil && resCopy.CPU != nil && resCopy.CPU.Quota != nil {
+		return *resCopy.CPU.Quota
+	}
+	return 0
 }
 
 // Labels returns the pod's labels
@@ -208,7 +250,31 @@ func (p *Pod) CgroupPath() (string, error) {
 	if err := p.updatePod(); err != nil {
 		return "", err
 	}
+	if p.state.CgroupPath != "" {
+		return p.state.CgroupPath, nil
+	}
+	if !p.HasInfraContainer() {
+		return "", errors.Wrap(define.ErrNoSuchCtr, "pod has no infra container")
+	}
 
+	id := p.state.InfraContainerID
+
+	if id != "" {
+		ctr, err := p.runtime.state.Container(id)
+		if err != nil {
+			return "", errors.Wrapf(err, "could not get infra")
+		}
+		if ctr != nil {
+			ctr.Start(context.Background(), false)
+			cgroupPath, err := ctr.CGroupPath()
+			if err != nil {
+				return "", errors.Wrapf(err, "could not get container cgroup")
+			}
+			p.state.CgroupPath = cgroupPath
+			p.save()
+			return cgroupPath, nil
+		}
+	}
 	return p.state.CgroupPath, nil
 }
 

commit 6c950bdb40f5df2a1925c8d2103713a6e3c3ff84

Remove unused code and data structures
No change in functionality.

I might be missing something here, but it appears to be unfinished and
unused.

Fixes: bbd085ad1e ("Podman Pod Create --cpus and --cpuset-cpus flags")
Fixes: 2d86051893 ("Pod Device-Read-BPS support")
Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

pkg/domain/entities/pods.go
@@ -291,22 +291,16 @@ type PodCreateReport = types.PodCreateReport
 type PodCloneReport = types.PodCloneReport
 
 func (p *PodCreateOptions) CPULimits() *specs.LinuxCPU {
-	cpu := &specs.LinuxCPU{}
-	hasLimits := false
+	cpu := &specs.LinuxCPU{
+		Cpus: p.CpusetCpus,
+	}
 
 	if p.Cpus != 0 {
 		period, quota := util.CoresToPeriodAndQuota(p.Cpus)
 		cpu.Period = &period
 		cpu.Quota = &quota
-		hasLimits = true
 	}
-	if p.CpusetCpus != "" {
-		cpu.Cpus = p.CpusetCpus
-		hasLimits = true
-	}
-	if !hasLimits {
-		return cpu
-	}
+
 	return cpu
 }
 
@@ -380,18 +374,11 @@ func ToPodSpecGen(s specgen.PodSpecGenerator, p *PodCreateOptions) (*specgen.Pod
 	s.CgroupParent = p.CgroupParent
 
 	// Resource config
-	cpuDat := p.CPULimits()
 	if s.ResourceLimits == nil {
 		s.ResourceLimits = &specs.LinuxResources{}
-		s.ResourceLimits.CPU = &specs.LinuxCPU{}
 	}
-	if cpuDat != nil {
-		s.ResourceLimits.CPU = cpuDat
-		if p.Cpus != 0 {
-			s.CPUPeriod = *cpuDat.Period
-			s.CPUQuota = *cpuDat.Quota
-		}
-	}
+	s.ResourceLimits.CPU = p.CPULimits()
+
 	s.Userns = p.Userns
 	sysctl := map[string]string{}
 	if ctl := p.Sysctl; len(ctl) > 0 {

pkg/specgen/podspecgen.go
@@ -248,12 +248,6 @@ type PodSpecGenerator struct {
 type PodResourceConfig struct {
 	// ResourceLimits contains linux specific CPU data for the pod
 	ResourceLimits *spec.LinuxResources `json:"resource_limits,omitempty"`
-	// CPU period of the cpuset, determined by --cpus
-	CPUPeriod uint64 `json:"cpu_period,omitempty"`
-	// CPU quota of the cpuset, determined by --cpus
-	CPUQuota int64 `json:"cpu_quota,omitempty"`
-	// ThrottleReadBpsDevice contains the rate at which the devices in the pod can be read from/accessed
-	ThrottleReadBpsDevice map[string]spec.LinuxThrottleDevice `json:"throttleReadBpsDevice,omitempty"`
 }
 
 type PodSecurityConfig struct {