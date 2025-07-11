ee18133f63f836d06c7ea9d13213b69d291d3bc8
Miloslav Trmač <mitr@redhat.com>
2025-07-11 18:16:34 +0200

    Consolidate the definition and consumption of --sign* CLI options
    There are three copies of them, and already a shared utility, so
    also define the options in a single place.
    
    This will make it easier to add more options, and it reduces the risk
    of incorrectly ordering the parameters to PrepareSigning.
    
    Should not change behavior.
    
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- cmd/podman/common/sign.go
+++ cmd/podman/common/sign.go
@@ -4,30 +4,66 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/containers/common/pkg/completion"
 	"github.com/containers/common/pkg/ssh"
 	"github.com/containers/image/v5/pkg/cli"
 	"github.com/containers/image/v5/pkg/cli/sigstore"
 	"github.com/containers/image/v5/signature/signer"
+	"github.com/containers/podman/v5/cmd/podman/registry"
 	"github.com/containers/podman/v5/pkg/domain/entities"
+	"github.com/spf13/cobra"
 )
 
-// PrepareSigning updates pushOpts.Signers, pushOpts.SignPassphrase and SignSigstorePrivateKeyPassphrase based on a --sign-passphrase-file
-// value signPassphraseFile and a --sign-by-sigsstore value signBySigstoreParamFile, and validates pushOpts.Sign* consistency.
+// SigningCLIOnlyOptions contains signing-related CLI options.
+// Some other options are defined in entities.ImagePushOptions.
+type SigningCLIOnlyOptions struct {
+	signPassphraseFile      string
+	signBySigstoreParamFile string
+}
+
+func DefineSigningFlags(cmd *cobra.Command, cliOpts *SigningCLIOnlyOptions, pushOpts *entities.ImagePushOptions) {
+	flags := cmd.Flags()
+
+	signByFlagName := "sign-by"
+	flags.StringVar(&pushOpts.SignBy, signByFlagName, "", "Add a signature at the destination using the specified key")
+	_ = cmd.RegisterFlagCompletionFunc(signByFlagName, completion.AutocompleteNone)
+
+	signBySigstoreFlagName := "sign-by-sigstore"
+	flags.StringVar(&cliOpts.signBySigstoreParamFile, signBySigstoreFlagName, "", "Sign the image using a sigstore parameter file at `PATH`")
+	_ = cmd.RegisterFlagCompletionFunc(signBySigstoreFlagName, completion.AutocompleteDefault)
+
+	signBySigstorePrivateKeyFlagName := "sign-by-sigstore-private-key"
+	flags.StringVar(&pushOpts.SignBySigstorePrivateKeyFile, signBySigstorePrivateKeyFlagName, "", "Sign the image using a sigstore private key at `PATH`")
+	_ = cmd.RegisterFlagCompletionFunc(signBySigstorePrivateKeyFlagName, completion.AutocompleteDefault)
+
+	signPassphraseFileFlagName := "sign-passphrase-file"
+	flags.StringVar(&cliOpts.signPassphraseFile, signPassphraseFileFlagName, "", "Read a passphrase for signing an image from `PATH`")
+	_ = cmd.RegisterFlagCompletionFunc(signPassphraseFileFlagName, completion.AutocompleteDefault)
+
+	if registry.IsRemote() {
+		_ = flags.MarkHidden(signByFlagName)
+		_ = flags.MarkHidden(signBySigstoreFlagName)
+		_ = flags.MarkHidden(signBySigstorePrivateKeyFlagName)
+		_ = flags.MarkHidden(signPassphraseFileFlagName)
+	}
+}
+
+// PrepareSigning updates pushOpts.Signers, pushOpts.SignPassphrase and SignSigstorePrivateKeyPassphrase based on cliOpts,
+// and validates pushOpts.Sign* consistency.
 // It may interactively prompt for a passphrase if one is required and wasn’t provided otherwise;
 // or it may interactively trigger an OIDC authentication, using standard input/output, or even open a web browser.
 // Returns a cleanup callback on success, which must be called when done.
-func PrepareSigning(pushOpts *entities.ImagePushOptions,
-	signPassphraseFile, signBySigstoreParamFile string) (func(), error) {
+func PrepareSigning(pushOpts *entities.ImagePushOptions, cliOpts *SigningCLIOnlyOptions) (func(), error) {
 	// c/common/libimage.Image does allow creating both simple signing and sigstore signatures simultaneously,
 	// with independent passphrases, but that would make the CLI probably too confusing.
 	// For now, use the passphrase with either, but only one of them.
-	if signPassphraseFile != "" && pushOpts.SignBy != "" && pushOpts.SignBySigstorePrivateKeyFile != "" {
+	if cliOpts.signPassphraseFile != "" && pushOpts.SignBy != "" && pushOpts.SignBySigstorePrivateKeyFile != "" {
 		return nil, fmt.Errorf("only one of --sign-by and sign-by-sigstore-private-key can be used with --sign-passphrase-file")
 	}
 
 	var passphrase string
-	if signPassphraseFile != "" {
-		p, err := cli.ReadPassphraseFile(signPassphraseFile)
+	if cliOpts.signPassphraseFile != "" {
+		p, err := cli.ReadPassphraseFile(cliOpts.signPassphraseFile)
 		if err != nil {
 			return nil, err
 		}
@@ -39,8 +75,8 @@ func PrepareSigning(pushOpts *entities.ImagePushOptions,
 	pushOpts.SignPassphrase = passphrase
 	pushOpts.SignSigstorePrivateKeyPassphrase = []byte(passphrase)
 	cleanup := signingCleanup{}
-	if signBySigstoreParamFile != "" {
-		signer, err := sigstore.NewSignerFromParameterFile(signBySigstoreParamFile, &sigstore.Options{
+	if cliOpts.signBySigstoreParamFile != "" {
+		signer, err := sigstore.NewSignerFromParameterFile(cliOpts.signBySigstoreParamFile, &sigstore.Options{
 			PrivateKeyPassphrasePrompt: cli.ReadPassphraseFile,
 			Stdin:                      os.Stdin,
 			Stdout:                     os.Stdout,
