1f03c86dacfe15cf870a88313b1e37092c6d2281
Paul Holzinger <pholzing@redhat.com>
2025-07-15 15:54:08 +0200

    [v5.4-rhel] podman rm: handle case where conmon was killed
    Adding this commit from #26643 to v5.4-rhel as after PR #27910
    failed QE testing.  The error `ErrConmonDead` was added sometime
    after Podman v5.4-rhel and before v5.6-rhel was released.
    New tests for the issue in #27910 are failing here in 5.4-rhel
    due to that renamed error.
    
    This will hopefully finally
    
    Fixes: https://issues.redhat.com/browse/RHEL-141490
    
    Original PR text for this change from @Luap99
    
    When conmon was killed podman rm -f currently fails but running it again
    then works which doesn't really makes sense. We should properly remove
    the contianer even if conmon is dead.
    
    In fact the code already handles ErrConmonDead as stop error when we
    remove the container but this error was never thrown anywhere. To fix
    this throw ErrConmonDead instead of ErrInternal because that is not an
    intenral error if something else killed conmon.
    
    With this we can correctly cleanup and remove the container. The fact
    that this works on the first try is important for quadlet units as they
    only run the ExecStopPost= command once to remove it.
    
    Fixes: #26640
    
    Signed-off-by: Paul Holzinger <pholzing@redhat.com>
    Signed-off-by: tomsweeneyredhat <tsweeney@redhat.com>

--- libpod/container_internal.go
+++ libpod/container_internal.go
@@ -1518,7 +1518,7 @@ func (c *Container) waitForConmonToExitAndSave() error {
 				logrus.Errorf("Error cleaning up container %s after Conmon exited prematurely: %v", c.ID(), err)
 			}
 
-			return fmt.Errorf("container %s conmon exited prematurely, exit code could not be retrieved: %w", c.ID(), define.ErrInternal)
+			return fmt.Errorf("container %s conmon exited prematurely, exit code could not be retrieved: %w", c.ID(), define.ErrConmonDead)
 		}
 
 		return c.save()
