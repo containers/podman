0530a564a76b167594128d7b3d61d882234358bb
Devashish08 <devashish.cs025@gmail.com>
2025-07-24 12:15:21 +0000

    Fix: Improve error message for empty device modes in API requests
    - Add specific check for empty device modes in ParseDevice function
    - Change error message from 'invalid device mode: ' to 'empty device mode in device specification: <device>'
    - Include full device specification in error message for better context
    - Add test cases for empty device mode scenarios
    - Resolves issue where '/dev/fuse::' provided unhelpful error message
    
    Fixes #26629
    
    Signed-off-by: Devashish08 <devashish.cs025@gmail.com>

--- pkg/specgen/generate/config_common_test.go
+++ pkg/specgen/generate/config_common_test.go
@@ -20,6 +20,7 @@ func TestParseDevice(t *testing.T) {
 		{"/dev/foo:/dev/bar:rw", "/dev/foo", "/dev/bar", "rw"},
 		{"/dev/foo:rw", "/dev/foo", "/dev/foo", "rw"},
 		{"/dev/foo::rw", "/dev/foo", "/dev/foo", "rw"},
+		{"/dev/foo:", "/dev/foo", "/dev/foo", "rwm"},
 	}
 	for _, test := range tests {
 		src, dst, perm, err := ParseDevice(test.device)
@@ -27,5 +28,28 @@ func TestParseDevice(t *testing.T) {
 		assert.Equal(t, src, test.src)
 		assert.Equal(t, dst, test.dst)
 		assert.Equal(t, perm, test.perm)
+	}
+}
+
+func TestParseDeviceErrors(t *testing.T) {
+	errorTests := []struct {
+		device        string
+		expectedError string
+	}{
+		{"/dev/fuse::", "empty device mode in device specification: /dev/fuse::"},
+		{"/dev/fuse:invalid", `invalid device mode "invalid" in device "/dev/fuse:invalid"`},
+		{"/dev/fuse:/path:xyz", `invalid device mode "xyz" in device "/dev/fuse:/path:xyz"`},
+		{"/dev/fuse:/path:rw:extra", `invalid device specification: /dev/fuse:/path:rw:extra`},
+		{"/dev/fuse:/path:rw:extra:more", `invalid device specification: /dev/fuse:/path:rw:extra:more`},
+		{"/dev/fuse:notapath", `invalid device mode "notapath" in device "/dev/fuse:notapath"`},
+		{"/dev/fuse:x", `invalid device mode "x" in device "/dev/fuse:x"`},
+		{"/dev/fuse:rwx", `invalid device mode "rwx" in device "/dev/fuse:rwx"`},
+		{"/dev/fuse:rrw", `invalid device mode "rrw" in device "/dev/fuse:rrw"`},
+	}
+
+	for _, test := range errorTests {
+		_, _, _, err := ParseDevice(test.device)
+		assert.Error(t, err)
+		assert.Contains(t, err.Error(), test.expectedError)
 	}
 }
