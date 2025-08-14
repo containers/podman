a5478d407faa52287fe01c1aa8a916373adfac84
tobwen <1864057+tobwen@users.noreply.github.com>
2025-08-14 22:21:42 +0200

    Add --protected flag to volume create command
    Signed-off-by: tobwen <1864057+tobwen@users.noreply.github.com>

--- cmd/podman/volumes/create.go
+++ cmd/podman/volumes/create.go
@@ -31,11 +31,12 @@ var (
 var (
 	createOpts = entities.VolumeCreateOptions{}
 	opts       = struct {
-		Label  []string
-		Opts   []string
-		Ignore bool
-		UID    int
-		GID    int
+		Label      []string
+		Opts       []string
+		Ignore     bool
+		UID        int
+		GID        int
+		Protected bool
 	}{}
 )
 
@@ -68,6 +69,10 @@ func init() {
 	gidFlagName := "gid"
 	flags.IntVar(&opts.GID, gidFlagName, 0, "Set the GID of the volume owner")
 	_ = createCommand.RegisterFlagCompletionFunc(gidFlagName, completion.AutocompleteNone)
+
+	protectedFlagName := "protected"
+	flags.BoolVar(&opts.Protected, protectedFlagName, false, "Mark volume as protected (excluded from system prune by default)")
+	_ = createCommand.RegisterFlagCompletionFunc(protectedFlagName, completion.AutocompleteNone)
 }
 
 func create(cmd *cobra.Command, args []string) error {
@@ -94,6 +99,7 @@ func create(cmd *cobra.Command, args []string) error {
 	if cmd.Flags().Changed("gid") {
 		createOpts.GID = &opts.GID
 	}
+	createOpts.Protected = opts.Protected
 	response, err := registry.ContainerEngine().VolumeCreate(context.Background(), createOpts)
 	if err != nil {
 		return err
