containers/podman

commit 3e8b2d7d9668a20a4e367ceea15827f240d67060

Fix seccomp profile path on Windows
Call specgen.ConvertWinMountPath() on seccomp profile paths provided via
security-opt parameter.

Fixes https://github.com/containers/podman/issues/26558

Signed-off-by: Mario Loriedo <mario.loriedo@gmail.com>

pkg/specgenutil/specgen.go
@@ -737,8 +737,13 @@ func FillOutSpecGen(s *specgen.SpecGenerator, c *entities.ContainerCreateOptions
 		case "proc-opts":
 			s.ProcOpts = strings.Split(val, ",")
 		case "seccomp":
-			s.SeccompProfilePath = val
-			s.Annotations[define.InspectAnnotationSeccomp] = val
+			convertedPath, err := specgen.ConvertWinMountPath(val)
+			if err != nil {
+				// If the conversion fails, use the original path
+				convertedPath = val
+			}
+			s.SeccompProfilePath = convertedPath
+			s.Annotations[define.InspectAnnotationSeccomp] = convertedPath
 			// this option is for docker compatibility, it is the same as unmask=ALL
 		case "systempaths":
 			if val == "unconfined" {

pkg/specgenutil/specgenutil_windows_test.go
@@ -1,0 +1,53 @@
+//go:build windows
+
+package specgenutil
+
+import (
+	"os"
+	"testing"
+
+	"github.com/containers/podman/v5/pkg/domain/entities"
+	"github.com/containers/podman/v5/pkg/specgen"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSeccompProfilePath(t *testing.T) {
+	cwd, err := os.Getwd()
+	assert.NoError(t, err)
+	cwd_wsl, err := specgen.ConvertWinMountPath(cwd)
+	assert.NoError(t, err)
+
+	tests := []struct {
+		originalPath string
+		expectedPath string
+	}{
+		{`C:\Foo`, "/mnt/c/Foo"},
+		{`C:\Foo`, "/mnt/c/Foo"},
+		{`\\?\C:\Foo`, "/mnt/c/Foo"},
+		{`/c/bar`, "/mnt/c/bar"},
+		{`/c/bar`, "/mnt/c/bar"},
+		{`/mnt/c/bar`, "/mnt/c/bar"},
+		{`/test/this`, "/test/this"},
+		{`c:/bar/something`, "/mnt/c/bar/something"},
+		{`c`, cwd_wsl + "/c"},
+		{`\\computer\loc`, `\\computer\loc`},
+		{`\\.\drive\loc`, "/mnt/wsl/drive/loc"},
+	}
+
+	f := func(secopt string) (*specgen.SpecGenerator, error) {
+		sg := specgen.NewSpecGenerator("nothing", false)
+		err := FillOutSpecGen(sg, &entities.ContainerCreateOptions{
+			SecurityOpt: []string{secopt}}, []string{},
+		)
+		return sg, err
+	}
+
+	for _, test := range tests {
+		t.Run(test.originalPath, func(t *testing.T) {
+			msg := "Checking: " + test.originalPath
+			sg, err := f("seccomp=" + test.originalPath)
+			assert.NoError(t, err, msg)
+			assert.Equal(t, test.expectedPath, sg.SeccompProfilePath, msg)
+		})
+	}
+}

commit 5a2098cf610982b26d2664f3f3b3c68b3a775b0e

windows: do not convert unconfined seccomp path
unconfined is a special value and not a path as such it must not be
converted otherwise --security-opt seccomp=unconfined fails as it tries
to access a file called unconfined.

Fixes: 3e8b2d7d96 ("Fix seccomp profile path on Windows")
Fixes: #26855

Signed-off-by: Paul Holzinger <pholzing@redhat.com>

pkg/specgenutil/specgen.go
@@ -737,10 +737,15 @@ func FillOutSpecGen(s *specgen.SpecGenerator, c *entities.ContainerCreateOptions
 		case "proc-opts":
 			s.ProcOpts = strings.Split(val, ",")
 		case "seccomp":
-			convertedPath, err := specgen.ConvertWinMountPath(val)
-			if err != nil {
-				// If the conversion fails, use the original path
-				convertedPath = val
+			convertedPath := val
+			// Do not try to convert special value "unconfined",
+			// https://github.com/containers/podman/issues/26855
+			if val != "unconfined" {
+				convertedPath, err = specgen.ConvertWinMountPath(val)
+				if err != nil {
+					// If the conversion fails, use the original path
+					convertedPath = val
+				}
 			}
 			s.SeccompProfilePath = convertedPath
 			s.Annotations[define.InspectAnnotationSeccomp] = convertedPath

pkg/machine/e2e/basic_test.go
@@ -44,7 +44,8 @@ var _ = Describe("run basic podman commands", func() {
 		Expect(newImgs).To(Exit(0))
 		Expect(newImgs.outputToStringSlice()).To(HaveLen(1))
 
-		runAlp, err := mb.setCmd(bm.withPodmanCommand([]string{"run", TESTIMAGE, "cat", "/etc/os-release"})).run()
+		// seccomp option as regression test for https://github.com/containers/podman/issues/26855
+		runAlp, err := mb.setCmd(bm.withPodmanCommand([]string{"run", "--security-opt", "seccomp=unconfined", TESTIMAGE, "cat", "/etc/os-release"})).run()
 		Expect(err).ToNot(HaveOccurred())
 		Expect(runAlp).To(Exit(0))
 		Expect(runAlp.outputToString()).To(ContainSubstring("Alpine Linux"))

pkg/specgenutil/specgenutil_windows_test.go
@@ -32,6 +32,7 @@ func TestSeccompProfilePath(t *testing.T) {
 		{`c`, cwd_wsl + "/c"},
 		{`\\computer\loc`, `\\computer\loc`},
 		{`\\.\drive\loc`, "/mnt/wsl/drive/loc"},
+		{"unconfined", "unconfined"},
 	}
 
 	f := func(secopt string) (*specgen.SpecGenerator, error) {