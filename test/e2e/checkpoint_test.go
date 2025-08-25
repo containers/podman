5e5bdb18f0c5b63a59ffe9c3c3998886d5effba1
Tim Zhou <tizhou@redhat.com>
2025-08-25 13:34:43 -0400

    uncomment tests duo to rawhide previous issues
    Signed-off-by: Tim Zhou <tizhou@redhat.com>

--- test/e2e/checkpoint_test.go
+++ test/e2e/checkpoint_test.go
@@ -332,7 +332,6 @@ var _ = Describe("Podman checkpoint", func() {
 	})
 
 	It("podman checkpoint container with established tcp connections", func() {
-		Skip("FIXME: #26289 - Rawhide only issue, skip for now")
 		localRunString := getRunString([]string{REDIS_IMAGE})
 		session := podmanTest.Podman(localRunString)
 		session.WaitWithDefaultTimeout()
@@ -1062,7 +1061,6 @@ var _ = Describe("Podman checkpoint", func() {
 	})
 
 	It("podman checkpoint and restore container with different port mappings", func() {
-		Skip("FIXME: #26289 - Rawhide only issue, skip for now")
 		randomPort, err := utils.GetRandomPort()
 		Expect(err).ShouldNot(HaveOccurred())
 		localRunString := getRunString([]string{"-p", fmt.Sprintf("%d:6379", randomPort), "--rm", REDIS_IMAGE})
@@ -1366,7 +1364,6 @@ var _ = Describe("Podman checkpoint", func() {
 	})
 
 	It("podman checkpoint and restore containers with --print-stats", func() {
-		Skip("FIXME: #26289 - Rawhide only issue, skip for now")
 		session1 := podmanTest.Podman(getRunString([]string{REDIS_IMAGE}))
 		session1.WaitWithDefaultTimeout()
 		Expect(session1).Should(ExitCleanly())
