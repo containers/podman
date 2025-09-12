5ce8602c09512590d6ec4d95fdd66736e16d9798
Jan Rodák <hony.com@seznam.cz>
2025-09-12 14:30:01 +0200

    Add local build API for direct filesystem builds on MacOS and Windows
    Adds /libpod/local/build endpoint, client bindings, and path translation
    utilities to enable container builds from mounted directories to podman machine without tar uploads.
    
    This optimization significantly speeds up build operations when working with remote Podman machines by eliminating redundant file transfers for already-accessible files.
    
    Fixes: https://issues.redhat.com/browse/RUN-3249
    
    Signed-off-by: Jan Rodák <hony.com@seznam.cz>

--- internal/localapi/utils_unsupported.go
+++ internal/localapi/utils_unsupported.go
@@ -12,3 +12,8 @@ func CheckPathOnRunningMachine(ctx context.Context, path string) (*LocalAPIMap, 
 	logrus.Debug("CheckPathOnRunningMachine is not supported")
 	return nil, false
 }
+
+func CheckMultiplePathsOnRunningMachine(ctx context.Context, paths []string) (TranslationLocalAPIMap, bool) {
+	logrus.Debug("CheckMultiplePathsOnRunningMachine is not supported")
+	return nil, false
+}
