42010555e268617c8b5a72fd588b3c47c95b8ead
Christophe Fergeau <cfergeau@redhat.com>
2025-09-18 14:54:52 +0200

    cloud-init: refactor CmdLineCloudInitToConfig
    There should be no functional changes apart from an error being returned
    when passing an unknown cloud-init file, for example `user-data.yaml`
    instead of `user-data`

--- pkg/machine/shim/cloudinit.go
+++ pkg/machine/shim/cloudinit.go
@@ -10,36 +10,46 @@ import (
 	"go.podman.io/podman/v6/pkg/machine/vmconfigs"
 )
 
-func CmdLineCloudInitToConfig(files []string) (vmconfigs.CloudInitConfig, error) {
+func processOneFile(config *vmconfigs.CloudInitConfig, kind string, file string) error {
+	_, err := os.Stat(file)
+	if errors.Is(err, os.ErrNotExist) {
+		return fmt.Errorf("cloud-init: file %s not found: %w", file, err)
+	}
+	if err != nil {
+		return fmt.Errorf("cloud-init: failed to access %s: %w", file, err)
+	}
+
+	switch kind {
+	case "user-data":
+		config.UserData = &define.VMFile{
+			Path: file,
+		}
+	case "meta-data":
+		config.MetaData = &define.VMFile{
+			Path: file,
+		}
+	case "network-config":
+		config.NetworkConfig = &define.VMFile{
+			Path: file,
+		}
+	default:
+		return fmt.Errorf("cloud-init: unexpected configuration file '%s'", kind)
+	}
+	return nil
+}
+func CmdLineCloudInitToConfig(params []string) (vmconfigs.CloudInitConfig, error) {
 	config := vmconfigs.CloudInitConfig{
 		UserData:      nil,
 		MetaData:      nil,
 		NetworkConfig: nil,
 	}
-	for _, file := range files {
-		_, err := os.Stat(file)
-		if errors.Is(err, os.ErrNotExist) {
-			return config, fmt.Errorf("cloud-init: file %s not found: %w", file, err)
-		}
-		if err != nil {
-			return config, fmt.Errorf("cloud-init: failed to access %s: %w", file, err)
-		}
 
-		filename := filepath.Base(file)
-		switch filename {
-		case "user-data":
-			config.UserData = &define.VMFile{
-				Path: file,
-			}
-		case "meta-data":
-			config.MetaData = &define.VMFile{
-				Path: file,
-			}
-		case "network-config":
-			config.NetworkConfig = &define.VMFile{
-				Path: file,
-			}
+	for _, param := range params {
+		kind := filepath.Base(param)
+		if err := processOneFile(&config, kind, param); err != nil {
+			return config, err
 		}
 	}
+
 	return config, nil
 }
