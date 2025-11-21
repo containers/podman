ebd665143ee2344bf44235565a4ba87362c493f1
Lokesh Mandvekar <lsm5@redhat.com>
2025-11-21 14:18:02 -0500

    FreeBSD: solve lint issues
    Signed-off-by: Lokesh Mandvekar <lsm5@redhat.com>

--- cmd/podman/system/service_abi_common.go
+++ cmd/podman/system/service_abi_common.go
@@ -2,9 +2,5 @@
 
 package system
 
-// Currently, we only need servicereaper on Linux to support slirp4netns.
-func maybeStartServiceReaper() {
-}
-
 // There is no cgroup on non linux.
 func maybeMoveToSubCgroup() {}
