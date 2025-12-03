7ce2e00ab140c11a68301f0b161f51984131a858
Paul Holzinger <pholzing@redhat.com>
2025-12-03 19:15:24 +0100

    libpod: simplify resolveWorkDir()
    The code checks for isPathOnVolume and isPathOnMount so we can just use
    the SecureJoin here directly to check for path existance.
    
    Then instead of walking symlinks and trying to guess if they are on a
    mount just assume if it is a link (path is different from the normal
    joined one) then don't error out early and let the OCI runtime deal with
    it. The runtime does produce a less readable error but it still fails
    and we have much less fragile code.
    
    Signed-off-by: Paul Holzinger <pholzing@redhat.com>

--- test/e2e/run_working_dir_test.go
+++ test/e2e/run_working_dir_test.go
@@ -10,6 +10,7 @@ import (
 	. "github.com/containers/podman/v5/test/utils"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/types"
 )
 
 var _ = Describe("Podman run", func() {
@@ -60,11 +61,32 @@ WORKDIR  /etc/foobar`, ALPINE)
 
 	It("podman run on an image with a symlinked workdir", func() {
 		dockerfile := fmt.Sprintf(`FROM %s
-RUN mkdir /A && ln -s /A /B
+RUN mkdir /A && ln -s /A /B && ln -s /vol/test /link
 WORKDIR /B`, ALPINE)
 		podmanTest.BuildImage(dockerfile, "test", "false")
 
 		session := podmanTest.PodmanExitCleanly("run", "test", "pwd")
 		Expect(session.OutputToString()).To(Equal("/A"))
+
+		path := filepath.Join(podmanTest.TempDir, "test")
+		err := os.Mkdir(path, 0o755)
+		Expect(err).ToNot(HaveOccurred())
+
+		session = podmanTest.PodmanExitCleanly("run", "--workdir=/link", "--volume", podmanTest.TempDir+":/vol", "test", "pwd")
+		Expect(session.OutputToString()).To(Equal("/vol/test"))
+
+		// This will fail in the runtime since the target doesn't exists
+		session = podmanTest.Podman([]string{"run", "--workdir=/link", "test", "pwd"})
+		session.WaitWithDefaultTimeout()
+		var matcher types.GomegaMatcher
+		if filepath.Base(podmanTest.OCIRuntime) == "crun" {
+			matcher = ExitWithError(127, "chdir to `/link`: No such file or directory")
+		} else if filepath.Base(podmanTest.OCIRuntime) == "runc" {
+			matcher = ExitWithError(126, "mkdir /link: file exists")
+		} else {
+			// unknown runtime, just check it failed
+			matcher = Not(ExitCleanly())
+		}
+		Expect(session).Should(matcher)
 	})
 })
