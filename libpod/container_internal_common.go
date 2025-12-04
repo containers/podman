containers/podman

commit 637c264e2e8c70db771be74fd5ed4eef95159acb

fix issues found by nilness
The conditions are always true so they can be removed. And in the case
of exportCheckpoint() the scope means addToTarFiles was overwritten and
thus when it looped over it later the slice was always empty.

Signed-off-by: Paul Holzinger <pholzing@redhat.com>

libpod/container_internal_common.go
@@ -901,25 +901,19 @@ func (c *Container) resolveWorkDir() error {
 	if !c.config.CreateWorkingDir {
 		// No need to create it (e.g., `--workdir=/foo`), so let's make sure
 		// the path exists on the container.
-		if err != nil {
-			if os.IsNotExist(err) {
-				// If resolved Workdir path gets marked as a valid symlink,
-				// return nil cause this is valid use-case.
-				if c.isWorkDirSymlink(resolvedWorkdir) {
-					return nil
-				}
-				return fmt.Errorf("workdir %q does not exist on container %s", workdir, c.ID())
+		if errors.Is(err, os.ErrNotExist) {
+			// If resolved Workdir path gets marked as a valid symlink,
+			// return nil cause this is valid use-case.
+			if c.isWorkDirSymlink(resolvedWorkdir) {
+				return nil
 			}
-			// This might be a serious error (e.g., permission), so
-			// we need to return the full error.
-			return fmt.Errorf("detecting workdir %q on container %s: %w", workdir, c.ID(), err)
+			return fmt.Errorf("workdir %q does not exist on container %s", workdir, c.ID())
 		}
-		return nil
+		// This might be a serious error (e.g., permission), so
+		// we need to return the full error.
+		return fmt.Errorf("detecting workdir %q on container %s: %w", workdir, c.ID(), err)
 	}
 	if err := os.MkdirAll(resolvedWorkdir, 0755); err != nil {
-		if os.IsExist(err) {
-			return nil
-		}
 		return fmt.Errorf("creating container %s workdir: %w", c.ID(), err)
 	}
 
@@ -1192,7 +1186,7 @@ func (c *Container) exportCheckpoint(options ContainerCheckpointOptions) error {
 			return fmt.Errorf("exporting root file-system diff for %q: %w", c.ID(), err)
 		}
 
-		addToTarFiles, err := crutils.CRCreateRootFsDiffTar(&rootFsChanges, c.state.Mountpoint, c.bundlePath())
+		addToTarFiles, err = crutils.CRCreateRootFsDiffTar(&rootFsChanges, c.state.Mountpoint, c.bundlePath())
 		if err != nil {
 			return err
 		}

cmd/podman/volumes/prune.go
@@ -64,9 +64,6 @@ func prune(cmd *cobra.Command, args []string) error {
 	if !force {
 		reader := bufio.NewReader(os.Stdin)
 		fmt.Println("WARNING! This will remove all volumes not used by at least one container. The following volumes will be removed:")
-		if err != nil {
-			return err
-		}
 		listOptions.Filter, err = parse.FilterArgumentsIntoFilters(filter)
 		if err != nil {
 			return err

libpod/runtime_pod_common.go
@@ -90,11 +90,7 @@ func (r *Runtime) NewPod(ctx context.Context, p specgen.PodSpecGenerator, option
 			break
 		}
 	}
-	if addPodErr != nil {
-		return nil, fmt.Errorf("adding pod to state: %w", addPodErr)
-	}
-
-	return pod, nil
+	return nil, fmt.Errorf("adding pod to state: %w", addPodErr)
 }
 
 // AddInfra adds the created infra container to the pod state

pkg/domain/infra/abi/images.go
@@ -785,7 +785,7 @@ func (ir *ImageEngine) Scp(ctx context.Context, src, dst string, opts entities.I
 	if err != nil {
 		return nil, err
 	}
-	if (report.LoadReport == nil && err == nil) && (report.Source != nil && report.Dest != nil) { // we need to execute the transfer
+	if report.LoadReport == nil && (report.Source != nil && report.Dest != nil) { // we need to execute the transfer
 		transferOpts := entities.ScpTransferOptions{}
 		transferOpts.ParentFlags = report.ParentFlags
 		_, err := Transfer(ctx, *report.Source, *report.Dest, transferOpts)

pkg/machine/ocipull/source.go
@@ -35,9 +35,6 @@ func GetLocalBlob(ctx context.Context, path string) (*types.BlobInfo, error) {
 		return nil, err
 	}
 	blobs := img.LayerInfos()
-	if err != nil {
-		return nil, err
-	}
 	if len(blobs) != 1 {
 		return nil, errors.New("invalid disk image")
 	}

commit 7b1be7f17723c89a636a65bb6f06456bfb8e5156

libpod: fix workdir MkdirAll() all check
MkdirAll can fail with EEXIST when the path is a symlink and the target
doesn't exist. As such we should ignore the error.

Note there is something fundemantal wrong here with the path access as
it is following the symlink to the host, however it is only for a
stat() so it is not an security issue here.

Fixes: 637c264e2e ("fix issues found by nilness")

Signed-off-by: Paul Holzinger <pholzing@redhat.com>

libpod/container_internal_common.go
@@ -935,6 +935,9 @@ func (c *Container) resolveWorkDir() error {
 		return fmt.Errorf("detecting workdir %q on container %s: %w", workdir, c.ID(), err)
 	}
 	if err := os.MkdirAll(resolvedWorkdir, 0o755); err != nil {
+		if errors.Is(err, fs.ErrExist) {
+			return nil
+		}
 		return fmt.Errorf("creating container %s workdir: %w", c.ID(), err)
 	}
 

test/e2e/run_working_dir_test.go
@@ -56,4 +56,14 @@ WORKDIR  /etc/foobar`, ALPINE)
 		Expect(session).Should(ExitCleanly())
 		Expect(session.OutputToString()).To(Equal("/home/foobar"))
 	})
+
+	It("podman run on an image with a symlinked workdir", func() {
+		dockerfile := fmt.Sprintf(`FROM %s
+RUN mkdir /A && ln -s /A /B
+WORKDIR /B`, ALPINE)
+		podmanTest.BuildImage(dockerfile, "test", "false")
+
+		session := podmanTest.PodmanExitCleanly("run", "test", "pwd")
+		Expect(session.OutputToString()).To(Equal("/A"))
+	})
 })