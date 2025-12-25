9af3346c85f000157a15f703349863f468c0cff9
Lokesh Mandvekar <lsm5@redhat.com>
2025-12-25 14:06:58 -0500

    Remove CNI backend from CLI options and completions
    Remove user-facing CLI options for CNI network backend:
    
    CLI flags:
    - Remove --network-backend global flag
    - Remove flag registration and shell completion for network backend
    
    Shell completions:
    - Remove AutocompleteNetworkBackend() function
    - Remove references to CNI and Netavark type constants
    
    Backward compatibility:
    - Remove --rootless-cni flag alias for podman unshare
    - Remove SetNormalizeFunc that mapped rootless-cni to rootless-netns
    - Update --rootless-netns flag description to mention only netavark
    
    The --network-backend flag was already hidden, and is now completely
    removed. Users can no longer specify CNI as a network backend option.
    The --rootless-cni alias is also removed; users must use --rootless-netns.
    
    Signed-off-by: Lokesh Mandvekar <lsm5@redhat.com>

--- test/e2e/common_test.go
+++ test/e2e/common_test.go
@@ -1324,8 +1324,8 @@ func (p *PodmanTestIntegration) makeOptions(args []string, options PodmanExecOpt
 		eventsType = "none"
 	}
 
-	podmanOptions := strings.Split(fmt.Sprintf("%s--root %s --runroot %s --runtime %s --conmon %s --network-config-dir %s --network-backend %s --cgroup-manager %s --tmpdir %s --events-backend %s",
-		debug, p.Root, p.RunRoot, p.OCIRuntime, p.ConmonBinary, p.NetworkConfigDir, "netavark", p.CgroupManager, p.TmpDir, eventsType), " ")
+	podmanOptions := strings.Split(fmt.Sprintf("%s--root %s --runroot %s --runtime %s --conmon %s --network-config-dir %s --cgroup-manager %s --tmpdir %s --events-backend %s",
+		debug, p.Root, p.RunRoot, p.OCIRuntime, p.ConmonBinary, p.NetworkConfigDir, p.CgroupManager, p.TmpDir, eventsType), " ")
 
 	podmanOptions = append(podmanOptions, strings.Split(p.StorageOptions, " ")...)
 	if !options.NoCache {
