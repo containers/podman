15ec1921fdf4737311f6d442edb54c0cd8906acf
Lokesh Mandvekar <lsm5@redhat.com>
2025-12-25 15:35:27 -0500

    libpod: Add slirp4netns to pasta auto-migration and change default
    Implement automatic one-time migration from slirp4netns to pasta for
    existing containers. When a container using slirp4netns is started, it
    will automatically migrate to pasta with compatible options mapped.
    
    Also change the default rootless network backend from slirp4netns to
    pasta. When default_rootless_network_cmd is set to slirp4netns in
    containers.conf, a deprecation warning is logged and pasta is used
    instead.
    
    Changes:
    - Add migrateSlirp4netnsToPasta() function to auto-migrate containers
    - Integrate migration into prepareToStart() before container init
    - Map compatible options: mtu, enable_ipv6, outbound_addr
    - Warn about unsupported options: cidr, allow_host_loopback
    - Change default from slirp4netns to pasta in container_create.go
    - Add deprecation warning in networking_linux.go for slirp4netns mode
    
    The migration is transparent to users and happens automatically on
    first container start after upgrade.
    
    Note: Migration test cases cannot be included as creating new containers
    with --network=slirp4netns is no longer supported. The migration code
    remains functional for pre-existing containers.
    
    Signed-off-by: Lokesh Mandvekar <lsm5@redhat.com>

--- libpod/networking_linux.go
+++ libpod/networking_linux.go
@@ -30,6 +30,7 @@ func (r *Runtime) configureNetNS(ctr *Container, ctrNS string) (status map[strin
 		}
 	}()
 	if ctr.config.NetMode.IsSlirp4netns() {
+		logrus.Warnf("Container %s still using slirp4netns mode - should have been migrated to pasta", ctr.ID())
 		return nil, r.setupSlirp4netns(ctr, ctrNS)
 	}
 	if ctr.config.NetMode.IsPasta() {
