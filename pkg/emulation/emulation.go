38f6bf07cce8c3c6b9fa29e294ce06fcb1b026bb
Kir Kolyshkin <kolyshkin@gmail.com>
2026-01-15 16:41:17 -0800

    pkg/emulation: remove slice copying
    Copying the slice is not necessary here since registeredBinfmtMisc
    always creates the slice from scratch.
    
    This commit is part of series fixing issues reported by prealloc linter
    from golangci-lint v2.8.0.
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- pkg/emulation/emulation.go
+++ pkg/emulation/emulation.go
@@ -7,12 +7,10 @@ import "github.com/sirupsen/logrus"
 // Registered returns a list of platforms for which we think we have user
 // space emulation available.
 func Registered() []string {
-	var registered []string
-	binfmt, err := registeredBinfmtMisc()
+	registered, err := registeredBinfmtMisc()
 	if err != nil {
 		logrus.Warnf("registeredBinfmtMisc(): %v", err)
 		return nil
 	}
-	registered = append(registered, binfmt...)
 	return registered
 }
