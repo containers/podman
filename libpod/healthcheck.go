containers/podman

commit 88bacfc133cd7a2e388cf6c2f2e3be157d0f6e74

fix: skip execution of probes when initialDelaySeconds is not elapsed
According to the [Kubernetes docs](https://kubernetes.io/docs/tasks/configure-pod-container/configure-liveness-readiness-startup-probes/#configure-probes)
the probes should be executed after the `initialDelaySeconds`. So to be
consistent with the kubernetes specs, skip the execution of the probes until
the `initialDelaySeconds` is elapsed.

Closes #27678

Signed-off-by: Vasileios Anagnostopoulos <anagnwstopoulos@hotmail.com>

libpod/healthcheck.go
@@ -58,6 +58,24 @@ func (c *Container) runHealthCheck(ctx context.Context, isStartup bool) (define.
 		inStartPeriod bool
 	)
 
+	timeStart := time.Now()
+	if c.HealthCheckConfig().StartPeriod > 0 {
+		// there is a start-period we need to honor; we add startPeriod to container start time
+		startPeriodTime := c.state.StartedTime.Add(c.HealthCheckConfig().StartPeriod)
+		if timeStart.Before(startPeriodTime) {
+			// we are still in the start period, flip the inStartPeriod bool
+			inStartPeriod = true
+			logrus.Debugf("healthcheck for %s being run in start-period", c.ID())
+		}
+	}
+
+	// If we are playing a kube yaml then let's honor the start period time for
+	// both failing and succeeding cases to match kube behavior.
+	// So don't run the health check log till the start period is over
+	if _, ok := c.config.Spec.Annotations[define.KubeHealthCheckAnnotation]; ok && inStartPeriod && !isStartup {
+		return define.HealthCheckDefined, "", nil
+	}
+
 	hcCommand := c.HealthCheckConfig().Test
 	if isStartup {
 		logrus.Debugf("Running startup healthcheck for container %s", c.ID())
@@ -96,7 +114,6 @@ func (c *Container) runHealthCheck(ctx context.Context, isStartup bool) (define.
 	hcResult := define.HealthCheckSuccess
 	config := new(ExecConfig)
 	config.Command = newCommand
-	timeStart := time.Now()
 	exitCode, hcErr := c.healthCheckExec(config, c.HealthCheckConfig().Timeout, streams)
 	timeEnd := time.Now()
 	if hcErr != nil {
@@ -144,16 +161,6 @@ func (c *Container) runHealthCheck(ctx context.Context, isStartup bool) (define.
 		hcResult = define.HealthCheckContainerStopped
 	}
 
-	if c.HealthCheckConfig().StartPeriod > 0 {
-		// there is a start-period we need to honor; we add startPeriod to container start time
-		startPeriodTime := c.state.StartedTime.Add(c.HealthCheckConfig().StartPeriod)
-		if timeStart.Before(startPeriodTime) {
-			// we are still in the start period, flip the inStartPeriod bool
-			inStartPeriod = true
-			logrus.Debugf("healthcheck for %s being run in start-period", c.ID())
-		}
-	}
-
 	eventLog := output.String()
 	if c.HealthCheckMaxLogSize() != 0 && len(eventLog) > int(c.HealthCheckMaxLogSize()) {
 		eventLog = eventLog[:c.HealthCheckMaxLogSize()]
@@ -161,7 +168,7 @@ func (c *Container) runHealthCheck(ctx context.Context, isStartup bool) (define.
 
 	hcl := newHealthCheckLog(timeStart, timeEnd, returnCode, eventLog)
 
-	healthCheckResult, err := c.updateHealthCheckLog(hcl, hcResult, inStartPeriod, isStartup)
+	healthCheckResult, err := c.updateHealthCheckLog(hcl, hcResult, inStartPeriod)
 	if err != nil {
 		return hcResult, "", fmt.Errorf("unable to update health check log %s for %s: %w", c.getHealthCheckLogDestination(), c.ID(), err)
 	}
@@ -364,14 +371,7 @@ func (c *Container) isUnhealthy() (bool, error) {
 
 // UpdateHealthCheckLog parses the health check results and writes the log
 // NOTE: The caller must lock the container.
-func (c *Container) updateHealthCheckLog(hcl define.HealthCheckLog, hcResult define.HealthCheckStatus, inStartPeriod, isStartup bool) (define.HealthCheckResults, error) {
-	// If we are playing a kube yaml then let's honor the start period time for
-	// both failing and succeeding cases to match kube behavior.
-	// So don't update the health check log till the start period is over
-	if _, ok := c.config.Spec.Annotations[define.KubeHealthCheckAnnotation]; ok && inStartPeriod && !isStartup {
-		return define.HealthCheckResults{}, nil
-	}
-
+func (c *Container) updateHealthCheckLog(hcl define.HealthCheckLog, hcResult define.HealthCheckStatus, inStartPeriod bool) (define.HealthCheckResults, error) {
 	healthCheck, err := c.readHealthCheckLog()
 	if err != nil {
 		return define.HealthCheckResults{}, err

test/system/700-play.bats
@@ -964,6 +964,56 @@ EOF
     done
 }
 
+# bats test_tags=ci:parallel
+@test "podman kube play healthcheck should wait initialDelaySeconds before executing healthcheck" {
+
+    # GIVEN a container with a liveness exec healthcheck with initialDelaySeconds that is quite long
+    podname="liveness-exec-$(safename)"
+    ctrname="liveness-ctr-$(safename)"
+
+    fname="$PODMAN_TMPDIR/play_kube_$(random_string 6).yaml"
+    cat <<EOF >$fname
+apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+  name: $podname
+spec:
+  containers:
+  - name: $ctrname
+    image: $IMAGE
+    args:
+    - /bin/sh
+    - -c
+    - sleep 100
+    livenessProbe:
+      exec:
+        # /tmp/healthy will exist in healthy container
+        command:
+        - /bin/sh
+        - -c
+        - touch /tmp/healthcheck.log
+      initialDelaySeconds: 100
+      failureThreshold: 1
+      periodSeconds: 1
+EOF
+
+    run_podman kube play $fname
+    ctrName="$podname-$ctrname"
+
+    # WHEN then healthcheck is executed
+    run_podman 1 healthcheck run $ctrName
+
+    # THEN the execution of the liveness probe should be skipped because initialDelaySeconds has not yet elapsed
+    run_podman 1 exec $ctrName test -e /tmp/healthcheck.log
+
+    # The sleep command does not respond to SIGTERM, so we have to use stop here.
+    # This is done to save time in the test suite.
+    run_podman stop -t0 $ctrName
+
+    run_podman kube down $fname
+}
+
 # CANNOT BE PARALLELIZED (YET): buildah#5674, parallel builds fail
 # ...workaround is --layers=false, but there's no way to do that in kube
 @test "podman play --build private registry" {

commit 66efe4c84b5e6ca76929c88dc51c793f753c47ed

Don't fail healthcheck during initialDelaySeconds
A change was introduced in 88bacfc133cd7a2e388cf6c2f2e3be157d0f6e74 that
skips execution of the healthcheck command during the startup period
when the K8s `initialDelaySeconds` is defined for a liveness probe. A
`HealthCheckDefined` is returned instead. This means that `podman
healthcheck run` always exits with a non-zero code during that time and
the transient systemd unit that triggers the healthcheck fails.

Return a success instead so that we don't see failures before the actual
healthcheck is run.

Signed-off-by: Patrick Wicki <patrick.wicki@siemens.com>

libpod/healthcheck.go
@@ -73,7 +73,7 @@ func (c *Container) runHealthCheck(ctx context.Context, isStartup bool) (define.
 	// both failing and succeeding cases to match kube behavior.
 	// So don't run the health check log till the start period is over
 	if _, ok := c.config.Spec.Annotations[define.KubeHealthCheckAnnotation]; ok && inStartPeriod && !isStartup {
-		return define.HealthCheckDefined, "", nil
+		return define.HealthCheckSuccess, "", nil
 	}
 
 	hcCommand := c.HealthCheckConfig().Test