c69b76a2d4547780b4f373028a72d8b3369b216a
Christophe Fergeau <cfergeau@redhat.com>
2026-01-23 10:31:28 +0100

    move GenerateUserData to os specific impl
    to support user-mode networking in hyperv we need to customize the
    cloud-init config. This patch just move GenerateUserData outside of the
    default cloudinit impl so it can be customized for hyperv in following
    commit.
    
    Signed-off-by: lstocchi <lstocchi@redhat.com>

--- pkg/machine/cloudinit/cloudinit.go
+++ pkg/machine/cloudinit/cloudinit.go
@@ -6,12 +6,10 @@ import (
 	"os"
 	"path/filepath"
 
-	"go.podman.io/podman/v6/pkg/machine"
 	"go.podman.io/podman/v6/pkg/machine/define"
 	"go.podman.io/podman/v6/pkg/machine/vmconfigs"
 	"github.com/kdomanski/iso9660"
 	"github.com/sirupsen/logrus"
-	"gopkg.in/yaml.v3"
 )
 
 type User struct {
@@ -24,36 +22,6 @@ type User struct {
 
 type UserData struct {
 	Users []User `yaml:"users"`
-}
-
-func GenerateUserData(mc *vmconfigs.MachineConfig) ([]byte, error) {
-	sshKey, err := machine.GetSSHKeys(mc.SSH.IdentityPath)
-	if err != nil {
-		return nil, err
-	}
-
-	userData := UserData{
-		Users: []User{
-			User{
-				Name:    mc.SSH.RemoteUsername,
-				Sudo:    "ALL=(ALL) NOPASSWD:ALL",
-				Shell:   "/bin/bash",
-				Groups:  []string{"users"},
-				SSHKeys: []string{sshKey},
-			},
-		},
-	}
-
-	yamlBytes, err := yaml.Marshal(&userData)
-	if err != nil {
-		logrus.Errorf("Error marshaling to YAML: %v", err)
-		return nil, err
-	}
-
-	headerLine := "#cloud-config\n"
-	yamlBytes = append([]byte(headerLine), yamlBytes...)
-
-	return yamlBytes, nil
 }
 
 func GenerateUserDataFile(mc *vmconfigs.MachineConfig) (string, error) {
---
+++ pkg/machine/cloudinit/cloudinit_unix.go
@@ -0,0 +1,40 @@
+//go:build !windows
+
+package cloudinit
+
+import (
+	"github.com/sirupsen/logrus"
+	"go.podman.io/podman/v6/pkg/machine"
+	"go.podman.io/podman/v6/pkg/machine/vmconfigs"
+	"gopkg.in/yaml.v3"
+)
+
+func GenerateUserData(mc *vmconfigs.MachineConfig) ([]byte, error) {
+	sshKey, err := machine.GetSSHKeys(mc.SSH.IdentityPath)
+	if err != nil {
+		return nil, err
+	}
+
+	userData := UserData{
+		Users: []User{
+			User{
+				Name:    mc.SSH.RemoteUsername,
+				Sudo:    "ALL=(ALL) NOPASSWD:ALL",
+				Shell:   "/bin/bash",
+				Groups:  []string{"users"},
+				SSHKeys: []string{sshKey},
+			},
+		},
+	}
+
+	yamlBytes, err := yaml.Marshal(&userData)
+	if err != nil {
+		logrus.Errorf("Error marshaling to YAML: %v", err)
+		return nil, err
+	}
+
+	headerLine := "#cloud-config\n"
+	yamlBytes = append([]byte(headerLine), yamlBytes...)
+
+	return yamlBytes, nil
+}
