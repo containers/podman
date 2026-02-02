402a3bf39a336bb531522e487a4febd17b0bd395
Lokesh Mandvekar <lsm5@redhat.com>
2026-02-02 19:03:38 +0530

    test: disable all checkpoint tests
    Skip all checkpoint tests in both system and integration test suites
    for both root and rootless modes.
    
    Files modified:
    - test/system/520-checkpoint.bats: Added skip in setup()
    - test/e2e/checkpoint_test.go: Added Skip() in BeforeEach()
    - test/e2e/checkpoint_image_test.go: Added Skip() in BeforeEach()
    
    These tests are being disabled as checkpoint functionality is not
    being tested in this environment.
    
    Signed-off-by: Lokesh Mandvekar <lsm5@redhat.com>
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- test/e2e/checkpoint_test.go
+++ test/e2e/checkpoint_test.go
@@ -56,6 +56,7 @@ var _ = Describe("Podman checkpoint", func() {
 	)
 
 	BeforeEach(func() {
+		Skip("checkpoint tests disabled")
 		SkipIfRootless("checkpoint not supported in rootless mode")
 		tempdir, err = CreateTempDirInTempDir()
 		Expect(err).To(BeNil())
