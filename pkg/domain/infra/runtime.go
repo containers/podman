0e7c57e6e93a364025143291d891f053815e03c2
Miloslav Trmač <mitr@redhat.com>
2026-02-13 01:29:42 +0100

    Implement --tls-details for connecting to the remote server
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- pkg/domain/infra/runtime.go
+++ pkg/domain/infra/runtime.go
@@ -5,16 +5,25 @@ import (
 
 	"github.com/containers/podman/v6/pkg/bindings"
 	"github.com/containers/podman/v6/pkg/domain/entities"
+	"go.podman.io/image/v5/pkg/cli/basetls/tlsdetails"
 )
 
 // For the meaning of "WithoutLock", compare runtime_tunnel.go:newConnection()
 func newConnectionWithoutLock(ctx context.Context, facts *entities.PodmanConfig) (context.Context, error) {
+	// Doing this here means we typically parse the file twice, once when called by NewContainerEngine
+	// and once when called by NewImageEngine.
+	// Alternatively, we could have the top-level caller parse the file, and pass us a baseTLSConfig.
+	baseTLSConfig, err := tlsdetails.BaseTLSFromOptionalFile(facts.TLSDetailsFile)
+	if err != nil {
+		return nil, err
+	}
 	return bindings.NewConnectionWithOptions(ctx, bindings.Options{
-		URI:         facts.URI,
-		Identity:    facts.Identity,
-		TLSCertFile: facts.TLSCertFile,
-		TLSKeyFile:  facts.TLSKeyFile,
-		TLSCAFile:   facts.TLSCAFile,
-		Machine:     facts.MachineMode,
+		URI:           facts.URI,
+		Identity:      facts.Identity,
+		TLSCertFile:   facts.TLSCertFile,
+		TLSKeyFile:    facts.TLSKeyFile,
+		TLSCAFile:     facts.TLSCAFile,
+		BaseTLSConfig: baseTLSConfig.TLSConfig(),
+		Machine:       facts.MachineMode,
 	})
 }
