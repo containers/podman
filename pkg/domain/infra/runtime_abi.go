8ebc14d0501139ff0fe3219944929e047173d0ed
Miloslav Trmač <mitr@redhat.com>
2026-02-13 01:25:40 +0100

    Consolidate the call to bindings.NewConnectionWithOptions
    ... to centralize the conversion from entities.PodmanConfig
    to bindings.Options, we will add more code there.
    
    Should not change behavior.
    
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- pkg/domain/infra/runtime_abi.go
+++ pkg/domain/infra/runtime_abi.go
@@ -6,7 +6,6 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/containers/podman/v6/pkg/bindings"
 	"github.com/containers/podman/v6/pkg/domain/entities"
 	"github.com/containers/podman/v6/pkg/domain/infra/tunnel"
 )
@@ -18,14 +17,7 @@ func NewContainerEngine(facts *entities.PodmanConfig) (entities.ContainerEngine,
 		r, err := NewLibpodRuntime(facts.FlagSet, facts)
 		return r, err
 	case entities.TunnelMode:
-		ctx, err := bindings.NewConnectionWithOptions(context.Background(), bindings.Options{
-			URI:         facts.URI,
-			Identity:    facts.Identity,
-			TLSCertFile: facts.TLSCertFile,
-			TLSKeyFile:  facts.TLSKeyFile,
-			TLSCAFile:   facts.TLSCAFile,
-			Machine:     facts.MachineMode,
-		})
+		ctx, err := newConnectionWithoutLock(context.Background(), facts)
 		return &tunnel.ContainerEngine{ClientCtx: ctx}, err
 	}
 	return nil, fmt.Errorf("runtime mode '%v' is not supported", facts.EngineMode)
@@ -39,14 +31,7 @@ func NewImageEngine(facts *entities.PodmanConfig) (entities.ImageEngine, error) 
 		return r, err
 	case entities.TunnelMode:
 		// TODO: look at me!
-		ctx, err := bindings.NewConnectionWithOptions(context.Background(), bindings.Options{
-			URI:         facts.URI,
-			Identity:    facts.Identity,
-			TLSCertFile: facts.TLSCertFile,
-			TLSKeyFile:  facts.TLSKeyFile,
-			TLSCAFile:   facts.TLSCAFile,
-			Machine:     facts.MachineMode,
-		})
+		ctx, err := newConnectionWithoutLock(context.Background(), facts)
 		if err != nil {
 			return nil, fmt.Errorf("%w: %s", err, facts.URI)
 		}
